b3754c82bda3a7e8ce84ddd0baebf78b358c2691
Harald Fernengel <harald.fernengel@here.com>
2017-04-27 21:17:28 +0200

    VRY-88: Remove duplicate export
    Change-Id: I891685a62f2a8bd636dd62ebad9da2ba977fdc9f

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 export * from './lib/Decoder';
-export * from './lib/TileDataSource';
 export * from './lib/DataProvider';
 export * from './lib/HypeDataProvider';
 export * from './lib/AbstractTileDataSource';
