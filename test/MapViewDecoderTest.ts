1a275f429a08645de1588707bd1824657d8bc419
Harald Fernengel <harald.fernengel@here.com>
2017-08-25 15:48:04 +0200

    Remove broken test
    Change-Id: I591467f2aa55f0c6d742889ac21d88198f16ab50

--- test/MapViewDecoderTest.ts
+++ test/MapViewDecoderTest.ts
@@ -15,36 +15,6 @@ import { assert } from "chai";
 import * as decoder from '../index';
 
 describe("MapViewDecoder", function() {
-    it("postMessage", function(done) {
-
-        if (typeof Blob === "undefined") {
-            done();
-            return; // requires Blob support doesn't work in nodejs
-        }
-
-        assert.isDefined(decoder);
-
-        /** ### AMD
-
-        const script = `define({
-            testFunction: (message) => {
-                const result = message.data.value + 1;
-                self.postMessage({ type: 'test', value: result });
-            }
-        });`;
-
-        const blob = new Blob([script], {type: 'application/javascript'});
-        const url = URL.createObjectURL(blob);
-
-        const pool = new decoder.Decoder(1);
-        pool.addEventListener('test', (message: any) => {
-            assert.strictEqual(message.data.value, 43);
-            done();
-        });
-        pool.registerWorkerFunction('test', url, 'testFunction').then(() => {
-            pool.postMessage({ type: 'test', value: 42 });
-        }).catch((err) => done(err));
-
-         */
+    it("isOk", function() {
     });
 });
