81e7e61c3fe437cc916b9750a8e020c648840cb2
Stefan Dachwitz <stefan.dachwitz@here.com>
2018-06-13 18:30:54 +0200

    HARP-1962: Add missing documentation to mapview-decoder
    Change-Id: I699e19791197af30c6ef6d6028ff9240c986d8c4

--- lib/HypeDataProvider.ts
+++ lib/HypeDataProvider.ts
@@ -1,35 +1,55 @@
 /*
- * Copyright (C) 2017 HERE Global B.V. and its affiliate(s).
- * All rights reserved.
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
  *
- * This software and other materials contain proprietary information
- * controlled by HERE and are protected by applicable copyright legislation.
- * Any use and utilization of this software and other materials and
- * disclosure to any third parties is conditional upon having a separate
- * agreement with HERE for the access, use, utilization or disclosure of this
- * software. In the absence of such agreement, the use of the software is not
- * allowed.
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
  */
-
 import { CancellationToken } from "@here/fetch";
 import { TileKey } from "@here/geoutils";
-import { DataStoreClientParameters, HRN } from "@here/hype";
+import { DataStoreClientParameters } from "@here/hype";
 import { Catalog1Client, Catalog1Layer } from "@here/hype/lib/v1/Catalog1Client";
 import { DataStore1Client } from "@here/hype/lib/v1/DataStore1Client";
 import { Catalog2Client, Catalog2Layer } from "@here/hype/lib/v2/Catalog2Client";
 import { DataStore2Client } from "@here/hype/lib/v2/DataStore2Client";
+
 import { DataProvider } from "./DataProvider";
 
+/**
+ * Specify the options of the Hype datasource.
+ */
 export interface HypeDataProviderOptions {
+    /**
+     * Specify which layer to use.
+     */
     layer: string;
+
+    /**
+     * Optional parameter of the proxy URL, override the URL specified in the Hype data layer.
+     */
     proxyDataUrl?: string;
+
+    /**
+     * Specify which version of CatalogClient should be used.
+     */
     catalogVersion?: number;
 }
 
+/**
+ * The `HypeDataProvider` is a [[DataProvider]] which retrieves Hype tiles in any of the specified
+ * versions of the Hype DataStore.
+ */
 export class HypeDataProvider extends DataProvider {
     private m_layer?: Catalog1Layer | Catalog2Layer;
     private m_catalogClient?: Catalog1Client | Catalog2Client;
 
+    /**
+     * Construct a new `HypeDataProvider` with the specified options.
+     *
+     * @param m_options Specify options like layer, URL and version.
+     */
     constructor(private readonly m_options: HypeDataProviderOptions & DataStoreClientParameters) {
         super();
     }
@@ -41,7 +61,8 @@ export class HypeDataProvider extends DataProvider {
     /**
      * Returns the underlying catalog client.
      *
-     * **Note**: The data provider must be connected before this method can be called.
+     * **Note**: The data provider must be connected before this method can be called, so make sure
+     * that `ready()` returns `true` before calling it.
      *
      * @returns The catalog client this data provider uses.
      */
--- lib/TileLoader.ts
+++ lib/TileLoader.ts
@@ -1,53 +1,94 @@
 /*
- * Copyright (C) 2018 HERE Global B.V. and its affiliate(s).
- * All rights reserved.
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
  *
- * This software and other materials contain proprietary information
- * controlled by HERE and are protected by applicable copyright legislation.
- * Any use and utilization of this software and other materials and
- * disclosure to any third parties is conditional upon having a separate
- * agreement with HERE for the access, use, utilization or disclosure of this
- * software. In the absence of such agreement, the use of the software is not
- * allowed.
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
  */
-
 import { DecodedTile, ITileDecoder, TileInfo } from "@here/datasource-protocol";
 import { CancellationException, CancellationToken } from "@here/fetch";
 import { TileKey } from "@here/geoutils";
-import { DataSource, Tile, TileLoaderState } from "@here/mapview";
+import { DataSource, TileLoaderState } from "@here/mapview";
 import { LoggerManager } from "@here/utils";
 
 import { DataProvider } from "./DataProvider";
 
-const logger = LoggerManager.instance.create('TileLoader');
+const logger = LoggerManager.instance.create("TileLoader");
 
+/**
+ * The `TileLoader` manages the different states of loading and decoding for a [[Tile]]. Used by the
+ * [[TiledataSource]].
+ */
 export class TileLoader {
-
+    /**
+     * Current state of `TileLoader`.
+     */
     state: TileLoaderState = TileLoaderState.Initialized;
+
+    /**
+     * Error object if loading or decoding failed.
+     */
     error?: Error;
+
+    /**
+     * The binary tile data.
+     */
     payload?: ArrayBufferLike;
+
+    /**
+     * The result of decoding the `payload`: The [[DecodedTile]].
+     */
     decodedTile?: DecodedTile;
 
+    /**
+     * The [[CancellationToken]] notifying the [[DataProvider]] to cancel loading.
+     */
     protected loadCancellationToken = new CancellationToken();
+
+    /**
+     * The [[CancellationToken]] notifying the [[ITileDecoder]] to cancel decoding.
+     */
     protected decodeCancellationToken?: CancellationToken;
+
+    /**
+     * The promise which is resolved when loading and decoding have finished.
+     */
     protected donePromise?: Promise<TileLoaderState>;
+
+    /**
+     * The internal function that is called when loading and decoding have finished.
+     */
     protected resolveDonePromise?: (state: TileLoaderState) => void;
 
+    /**
+     * Set up loading of a single [[Tile]].
+     *
+     * @param dataSource The [[DataSource]] the tile belongs to.
+     * @param tileKey The quadtree address of a [[Tile]].
+     * @param dataProvider The [[DataProvider]] that retrieves the binary tile data.
+     * @param tileDecoder The [[ITileDecoder]] that decodes the binary tile to a [[DecodeTile]].
+     */
     constructor(
         protected dataSource: DataSource,
         protected tileKey: TileKey,
         protected dataProvider: DataProvider,
         protected tileDecoder: ITileDecoder
-    ) {
-    }
+    ) {}
 
+    /**
+     * Start loading and/or proceed through the various states of loading of this tile.
+     *
+     * @returns A promise which resolves the [[TileLoaderState]].
+     */
     loadAndDecode(): Promise<TileLoaderState> {
         switch (this.state) {
             case TileLoaderState.Loading:
                 return this.donePromise!;
 
             case TileLoaderState.Disposed:
-                logger.warn('cannot load in disposed state');
+                logger.warn("cannot load in disposed state");
                 return Promise.resolve(TileLoaderState.Disposed);
 
             case TileLoaderState.Ready:
@@ -68,18 +109,28 @@ export class TileLoader {
         }
     }
 
-    waitSettled() {
+    /**
+     * Return the current state in form of a promise. Caller can then wait for the promise to be
+     * resolved.
+     *
+     * @returns A promise which resolves the current [[TileLoaderState]].
+     */
+    waitSettled(): Promise<TileLoaderState> {
         if (!this.donePromise) {
             return Promise.resolve(this.state);
         }
         return this.donePromise;
     }
 
+    /**
+     * Cancel loading of the [[Tile]]. Cancellation token is notified, an internal state is cleaned
+     * up.
+     */
     cancel() {
         switch (this.state) {
             case TileLoaderState.Disposed:
                 // prevent accidental escape from disposed state
-                logger.warn('attempting to cancel disposed TileLoader');
+                logger.warn("attempting to cancel disposed TileLoader");
                 return;
 
             case TileLoaderState.Loading:
@@ -98,6 +149,9 @@ export class TileLoader {
         this.onDone(TileLoaderState.Canceled);
     }
 
+    /**
+     * Cancel loading and remove all references.
+     */
     dispose() {
         if (this.state !== TileLoaderState.Disposed) {
             this.cancel();
@@ -105,7 +159,10 @@ export class TileLoader {
         }
     }
 
-    isFinished() {
+    /**
+     * Return `true` if [[Tile]] is still loading, `false` otherwise.
+     */
+    isFinished(): boolean {
         return (
             this.state === TileLoaderState.Ready ||
             this.state === TileLoaderState.Canceled ||
@@ -113,8 +170,10 @@ export class TileLoader {
         );
     }
 
+    /**
+     * Depending on state: if not loaded yet, make sure it is loading.
+     */
     protected ensureLoadingStarted() {
-
         switch (this.state) {
             case TileLoaderState.Ready:
             case TileLoaderState.Initialized:
@@ -125,7 +184,7 @@ export class TileLoader {
             case TileLoaderState.Loading:
             case TileLoaderState.Loaded:
                 // we may reuse already started loading promise
-                logger.info('reusing already started load operation');
+                logger.info("reusing already started load operation");
                 return;
 
             case TileLoaderState.Decoding:
@@ -134,15 +193,19 @@ export class TileLoader {
                 return;
 
             case TileLoaderState.Disposed:
-                logger.warn('cannot load in disposed state');
+                logger.warn("cannot load in disposed state");
                 return;
         }
     }
 
+    /**
+     * Start loading. Only call if loading did not start yet.
+     */
     protected doStartLoad() {
         const myLoadCancellationToken = this.loadCancellationToken;
-        this.dataProvider.getTile(this.tileKey, this.loadCancellationToken)
-            .then((payload) => {
+        this.dataProvider
+            .getTile(this.tileKey, this.loadCancellationToken)
+            .then(payload => {
                 if (myLoadCancellationToken.isCancelled) {
                     // safety belt if getTile doesn't really support cancellation tokens
                     throw new CancellationException();
@@ -156,7 +219,7 @@ export class TileLoader {
                 this.onError(error);
             });
 
-        if (!this.donePromise) {
+        if (this.donePromise === undefined) {
             this.donePromise = new Promise<TileLoaderState>((resolve, reject) => {
                 this.resolveDonePromise = resolve;
             });
@@ -164,6 +227,11 @@ export class TileLoader {
         this.state = TileLoaderState.Loading;
     }
 
+    /**
+     * Called when binary data has been loaded. The loading state is now progressing to decoding.
+     *
+     * @param payload Binary data in form of[[ArrayBufferLike]].
+     */
     protected onLoaded(payload: ArrayBufferLike) {
         this.state = TileLoaderState.Loaded;
         this.payload = payload;
@@ -177,10 +245,13 @@ export class TileLoader {
         this.startDecodeTile();
     }
 
+    /**
+     * Start decoding the payload.
+     */
     protected startDecodeTile() {
         const payload = this.payload;
         if (payload === undefined) {
-            logger.error('TileLoader#startDecodeTile: Cannot decode without payload');
+            logger.error("TileLoader#startDecodeTile: Cannot decode without payload");
             return;
         }
 
@@ -193,12 +264,8 @@ export class TileLoader {
         this.decodeCancellationToken = myCancellationPoint;
 
         const dataSource = this.dataSource;
-        this.tileDecoder.decodeTile(
-            payload,
-            this.tileKey,
-            dataSource.name,
-            dataSource.projection
-        )
+        this.tileDecoder
+            .decodeTile(payload, this.tileKey, dataSource.name, dataSource.projection)
             .then(decodedTile => {
                 if (myCancellationPoint.isCancelled) {
                     // our flow is cancelled, silently return
@@ -216,14 +283,20 @@ export class TileLoader {
             });
     }
 
+    /**
+     * Called when the decoding is finished, and the [[DecodedTile]] has been created.
+     *
+     * @param decodedTile The [[DecodedTile]].
+     */
     protected onDecoded(decodedTile: DecodedTile) {
-
         this.decodedTile = decodedTile;
         this.onDone(TileLoaderState.Ready);
     }
 
+    /**
+     * Cancel the decoding process.
+     */
     protected cancelDecoding() {
-
         if (this.decodeCancellationToken !== undefined) {
             // we should cancel any decodes already in progress!
             this.decodeCancellationToken.cancel();
@@ -231,6 +304,11 @@ export class TileLoader {
         }
     }
 
+    /**
+     * Called when loading and decoding has finished successfully. Resolves loading promise.
+     *
+     * @param doneState The latest state of loading.
+     */
     protected onDone(doneState: TileLoaderState) {
         if (this.resolveDonePromise) {
             this.resolveDonePromise(doneState);
@@ -240,6 +318,11 @@ export class TileLoader {
         this.state = doneState;
     }
 
+    /**
+     * Called when loading or decoding has finished with an error.
+     *
+     * @param error Error object describing the failing.
+     */
     protected onError(error: Error) {
         const dataSource = this.dataSource;
         logger.error(
@@ -253,14 +336,17 @@ export class TileLoader {
     }
 }
 
+/**
+ * Subclass of [[TileLoader]] which is used by [[TileDataSource]] to load the [[TileInfo]] meta
+ * data, not the tile data itself.
+ */
 export class TileInfoLoader extends TileLoader {
-
     tileInfo?: TileInfo;
 
     protected startDecodeTile() {
         const payload = this.payload;
         if (payload === undefined) {
-            logger.error('TileInfoLoader#startDecodeTile: Cannot decode without payload');
+            logger.error("TileInfoLoader#startDecodeTile: Cannot decode without payload");
             return;
         }
 
@@ -273,12 +359,8 @@ export class TileInfoLoader extends TileLoader {
         this.decodeCancellationToken = myCancellationPoint;
 
         const dataSource = this.dataSource;
-        this.tileDecoder.getTileInfo(
-            payload,
-            this.tileKey,
-            dataSource.name,
-            dataSource.projection
-        )
+        this.tileDecoder
+            .getTileInfo(payload, this.tileKey, dataSource.name, dataSource.projection)
             .then(tileInfo => {
                 if (myCancellationPoint.isCancelled) {
                     // our flow is cancelled, silently return
