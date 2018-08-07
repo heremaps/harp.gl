5ff9fd83797aca19f9a334243db043c7b245c97b
Harald Fernengel <harald.fernengel@here.com>
2018-08-07 11:17:43 +0200

    Monorepo specific changes

---
+++ scripts/doc-snippets.ts
@@ -0,0 +1,77 @@
+import * as fs from "fs";
+import * as glob from "glob";
+import * as path from "path";
+
+/*
+Simple script that extracts all code snippets from examples
+*/
+
+// counts the leading spaces in a string
+function countLeadingSpaces(line: string): number {
+    let result = 0;
+    for (const char of line) {
+        if (char !== " ") {
+            return result;
+        }
+        ++result;
+    }
+    return result;
+}
+
+// remove empty lines from top and bottom of snippet
+function chop(lines: string[]) {
+    while (lines[0] === "") {
+        lines.shift();
+    }
+    while (lines[-1] === "") {
+        lines.pop();
+    }
+}
+
+function reindented(spaces: number, lines: string[]): string[] {
+    if (spaces === 0) {
+        return lines;
+    }
+
+    const prefix = " ".repeat(spaces);
+    return lines.map(line => (line.startsWith(prefix) ? line.substring(spaces) : line));
+}
+
+// tslint:disable-next-line:no-var-requires
+const mkpath = require("mkpath");
+
+const sdkDir = path.resolve(__dirname, "..");
+const outDir = path.resolve(sdkDir, "dist/doc");
+mkpath.sync(outDir);
+
+const sourceFiles = glob.sync(sdkDir + "/@here/harp-examples/**/*.{ts,tsx,html}");
+
+const snippetRegex = /snippet:(\S+).*$([\s\S]*)^.*end:\1/gm;
+
+for (const sourceFile of sourceFiles) {
+    const contents = fs.readFileSync(sourceFile, { encoding: "utf8" });
+
+    let match;
+    // tslint:disable-next-line:no-conditional-assignment
+    while ((match = snippetRegex.exec(contents)) !== null) {
+        const fileName = match[1];
+        const snippet = match[2];
+
+        const lines = snippet.split("\n");
+        chop(lines);
+
+        if (lines.length === 0) {
+            // tslint:disable-next-line:no-console
+            console.error("ERROR: snippet", snippet, "in", fileName, "too short");
+            continue;
+        }
+
+        // reindent the snippet
+        const leadingSpaces = countLeadingSpaces(lines[0]);
+        const result = reindented(leadingSpaces, lines).join("\n");
+
+        fs.writeFileSync(path.resolve(outDir, fileName), result, { encoding: "utf8" });
+        // tslint:disable-next-line:no-console
+        console.log("generated", fileName, path.resolve(outDir, fileName));
+    }
+}
---
+++ webpack.examples.config.js
@@ -0,0 +1,17 @@
+const exampleConfig = require("./@here/harp-examples/webpack.config");
+const path = require("path");
+const fs = require("fs");
+
+const outputPath = path.join(__dirname, "dist/harp-examples");
+
+function patchConfig(config) {
+    config.resolve.modules = [__dirname, "node_modules"];
+    config.output.path = outputPath;
+
+    // make sure all source files are compiled using the same tsconfig.json
+    config.module.rules[0].options.configFile = path.join(__dirname, "tsconfig.json");
+
+    return config;
+}
+
+module.exports = exampleConfig.map(config => patchConfig(config));
