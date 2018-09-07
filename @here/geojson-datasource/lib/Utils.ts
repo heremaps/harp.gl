a19eb09077e3203b2183dc880b3ad8431da71ba2
Harald Fernengel <harald.fernengel@here.com>
2018-09-07 13:23:34 +0200

    Initial import of geojson-datasource

---
+++ @here/geojson-datasource/lib/Utils.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+
+/**
+ * Converts a base64 encoded string to a BLOB.
+ *
+ * @param b64Data Base64 encoded PNG image of texture
+ * @param sliceSize Side size of the texture.
+ */
+export function b64toBlob(b64Data: string, sliceSize = 512): Blob {
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        const byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays);
+    return blob;
+}
