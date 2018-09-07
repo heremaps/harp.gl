bf38d353dd21fca2e8f2b87cbb78537cf820f518
Harald Fernengel <harald.fernengel@here.com>
2018-09-07 13:23:36 +0200

    Initial import of mapview-decoder

---
+++ @here/mapview-decoder/lib/ThemedTileDecoder.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+import {
+    DecodedTile,
+    ITileDecoder,
+    OptionsMap,
+    Theme,
+    ThemeEvaluator,
+    TileInfo
+} from "@here/datasource-protocol";
+import { Projection, TileKey } from "@here/geoutils";
+
+/**
+ * `ThemedTileDecoder` implements an [[ITileDecoder]] which uses a [[Theme]] to apply styles to the
+ * objects displayed in the map.
+ *
+ * By default, decoders are executed in web workers (using [[TileDecoderService]]) for performance
+ * reasons.
+ */
+export abstract class ThemedTileDecoder implements ITileDecoder {
+    languages?: string[];
+    private m_theme?: Theme;
+    private m_themeEvaluators: Map<string, ThemeEvaluator> = new Map();
+    abstract connect(): Promise<void>;
+
+    dispose() {
+        // implemented in subclasses
+    }
+
+    decodeTile(
+        data: ArrayBufferLike,
+        tileKey: TileKey,
+        dataSourceName: string,
+        projection: Projection
+    ): Promise<DecodedTile> {
+        const themeEvaluator = this.getThemeEvalator(dataSourceName);
+        if (themeEvaluator === undefined) {
+            return Promise.reject(new Error("no theme loaded"));
+        }
+
+        return this.decodeThemedTile(data, tileKey, themeEvaluator, projection);
+    }
+
+    // tslint:disable:no-unused-variable
+    getTileInfo(
+        data: ArrayBufferLike,
+        tileKey: TileKey,
+        dataSourceName: string,
+        projection: Projection
+    ): Promise<TileInfo | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    // tslint:disable:no-unused-variable
+    configure(theme?: Theme | undefined, languages?: string[],
+        options?: OptionsMap | undefined): void {
+        if (theme !== undefined) {
+            this.m_theme = theme;
+            this.m_themeEvaluators.clear();
+        }
+        if (languages !== undefined) {
+            this.languages = languages;
+        }
+    }
+
+    /**
+     * Create a [[DecodedTile]] from binary tile data and a theme description in form of a
+     * [[ThemeEvaluator]].
+     *
+     * @param data Binary data in form of [[ArrayBufferLike]], or any object.
+     * @param tileKey Quadtree address of tile.
+     * @param themeEvaluator Processor of [[Theme]], identifies styling techniques applicable to
+     *      individual objects.
+     * @param projection Projection used by the individual data sources.
+     */
+    abstract decodeThemedTile(
+        data: ArrayBufferLike | {},
+        tileKey: TileKey,
+        themeEvaluator: ThemeEvaluator,
+        projection: Projection
+    ): Promise<DecodedTile>;
+
+    /**
+     * Create and deliver an individual [[ThemeEvaluator]] for every [[DataSource]] this
+     * `ThemedTileDecoder` is connected to.
+     *
+     * @param dataSourceName Name of [[DataSource]]
+     */
+    protected getThemeEvalator(dataSourceName: string): ThemeEvaluator | undefined {
+        if (this.m_theme === undefined) {
+            return undefined;
+        }
+        let themeEvaluator = this.m_themeEvaluators.get(dataSourceName);
+        if (themeEvaluator === undefined) {
+            themeEvaluator = new ThemeEvaluator(this.m_theme, dataSourceName);
+            this.m_themeEvaluators.set(dataSourceName, themeEvaluator);
+        }
+        return themeEvaluator;
+    }
+}
---
+++ @here/mapview-decoder/lib/TileDecoderService.ts
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+import {
+    ConfigurationMessage,
+    DecodeTileRequest,
+    getProjection,
+    isConfigurationMessage,
+    isDecodeTileRequest,
+    isStandardTexturedTechnique,
+    isTextureBuffer,
+    isTileInfoRequest,
+    ITileDecoder,
+    TileInfoRequest
+} from "@here/datasource-protocol";
+import { TileKey } from "@here/geoutils";
+import { LoggerManager } from "@here/utils";
+
+import { WorkerService, WorkerServiceResponse } from "./WorkerService";
+
+const logger = LoggerManager.instance.create("TileDecoderService");
+
+/**
+ * An extension to [[WorkerService]], the `TileDecoderService` implements an asynchronous
+ * (message based) service to decode tile content in web workers. The `TileDecoderService` itself
+ * lives in the web worker, and communicates with messages by means of a [[ConcurrentWorkerSet]]
+ * with the application.
+ *
+ * The `TileDecoderService` handles a [[DecodeTileRequest]], which contains a tile and its freshly
+ * loaded binary data, decodes the content with the [[ITileDecoder]] that the service is configured
+ * to use, and sends the data back in form of a [[WorkerServiceResponse]].
+ */
+export class TileDecoderService extends WorkerService {
+    /**
+     * Start a [[TileDecoderService]] with a given decoder.
+     *
+     * @param serviceId Service id. Must be unique.
+     * @param decoder   [[TileDecoder]] instance.
+     */
+    static start(serviceId: string, decoder: ITileDecoder) {
+        return new TileDecoderService(serviceId, decoder);
+    }
+
+    /**
+     * Set up the `TileDecoderService`. The name of the service must be unique
+     *
+     * @param serviceId Service id. Must be unique.
+     * @param m_decoder Decoder to handle the decoding and info requests.
+     */
+    constructor(readonly serviceId: string, private readonly m_decoder: ITileDecoder) {
+        super(serviceId);
+        this.m_decoder.connect();
+    }
+
+    /**
+     * Handle incoming request messages. Identifies message type and processes the request.
+     *
+     * @param request Message that is either a DecodeTileRequest or a TileInfoRequest.
+     * @returns A promise which resolves to a [[WorkerServiceResponse]].
+     */
+    protected handleRequest(request: any): Promise<WorkerServiceResponse> {
+        if (isDecodeTileRequest(request)) {
+            return new Promise<WorkerServiceResponse>(resolve => {
+                resolve(this.handleDecodeTileRequest(request));
+            });
+        } else if (isTileInfoRequest(request)) {
+            return new Promise<WorkerServiceResponse>(resolve => {
+                resolve(this.handleTileInfoRequest(request));
+            });
+        } else {
+            return super.handleRequest(request);
+        }
+    }
+
+    /**
+     * Handle incoming configuration message. Configuration message is passed on to decoder.
+     *
+     * @param request Message of type [[ConfigurationMessage]].
+     */
+    protected handleMessage(message: any) {
+        if (isConfigurationMessage(message)) {
+            this.handleConfigurationMessage(message);
+        } else {
+            logger.error(`[${this.serviceId}]: invalid message ${message.type}`);
+        }
+    }
+
+    private handleDecodeTileRequest(request: DecodeTileRequest): Promise<WorkerServiceResponse> {
+        const tileKey = TileKey.fromMortonCode(request.tileKey);
+        const projection = getProjection(request.projection);
+
+        return this.m_decoder
+            .decodeTile(request.data, tileKey, request.dataSourceName, projection)
+            .then(decodedTile => {
+                const transferList: ArrayBuffer[] = [];
+                decodedTile.geometries.forEach(geom => {
+                    geom.vertexAttributes.forEach(attr => {
+                        if (attr.buffer instanceof ArrayBuffer) {
+                            transferList.push(attr.buffer);
+                        }
+                    });
+
+                    if (geom.index && geom.index.buffer instanceof ArrayBuffer) {
+                        transferList.push(geom.index.buffer);
+                    }
+                });
+
+                decodedTile.techniques.forEach(technique => {
+                    if (isStandardTexturedTechnique(technique)) {
+                        if (isTextureBuffer(technique.texture)) {
+                            if (technique.texture.buffer instanceof ArrayBuffer) {
+                                transferList.push(technique.texture.buffer);
+                            }
+                        }
+                    }
+                });
+
+                return {
+                    response: decodedTile,
+                    transferList
+                };
+            });
+    }
+
+    private handleTileInfoRequest(request: TileInfoRequest): Promise<WorkerServiceResponse> {
+        const tileKey = TileKey.fromMortonCode(request.tileKey);
+        const projection = getProjection(request.projection);
+
+        return this.m_decoder
+            .getTileInfo(request.data, tileKey, request.dataSourceName, projection)
+            .then(tileInfo => {
+                const transferList: ArrayBuffer[] =
+                    tileInfo !== undefined && tileInfo.transferList !== undefined
+                        ? tileInfo.transferList
+                        : [];
+                return {
+                    response: tileInfo,
+                    transferList
+                };
+            });
+    }
+
+    private handleConfigurationMessage(message: ConfigurationMessage) {
+        this.m_decoder.configure(message.theme, message.languages, message.options);
+    }
+}
