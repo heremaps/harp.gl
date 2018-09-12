437a26c77e39ee58937195ea92fb85a6c9a98ae5
Harald Fernengel <harald.fernengel@here.com>
2018-09-12 10:54:13 +0200

    Initial import of materials

---
+++ @here/materials/index.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+
+export * from "./lib/CopyShader";
+export * from "./lib/DashedLineMaterial";
+export * from "./lib/EdgeMaterial";
+export * from "./lib/GlyphCopyMaterial";
+export * from "./lib/HighPrecisionLineMaterial";
+export * from "./lib/HighPrecisionPointMaterial";
+export * from "./lib/IconMaterial";
+export * from "./lib/SolidLineMaterial";
+export * from "./lib/MSAAMaterial";
+export * from "./lib/TextMaterial";
