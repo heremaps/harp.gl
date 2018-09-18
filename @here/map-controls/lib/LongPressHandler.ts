52c87096d46a9bb8c9b3167070b6327e4234dfcf
Harald Fernengel <harald.fernengel@here.com>
2018-09-18 10:53:58 +0200

    Initial import of map-controls

---
+++ @here/map-controls/lib/LongPressHandler.ts
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+
+/**
+ * Class that can be used to track long presses on an HTML Element. A long press is a press that
+ * lasts a minimum duration (see the [[timeout]] member) while the mouse is not moved more than a
+ * certain threshold (see the [[moveThreshold]] member).
+ */
+export class LongPressHandler {
+    /**
+     * How long to wait (in ms) until a press is considered a long press.
+     */
+    timeout: number = 500;
+
+    /**
+     * If the cursor moves more than the given number of pixels, it is not a long-press, but a pan.
+     */
+    moveThreshold: number = 5;
+
+    /**
+     * Button id that should be handled by this event.
+     */
+    buttonId: number = 0;
+
+    private m_mouseDownEvent?: MouseEvent = undefined;
+    private m_timerId?: number = undefined;
+    private m_moveHandlerRegistered: boolean = false;
+    private readonly m_boundMouseMoveHandler: any;
+    private readonly m_boundMouseDownHandler: any;
+    private readonly m_boundMouseUpHandler: any;
+
+    /**
+     * Default constructor.
+     *
+     * @param element The HTML element to track.
+     * @param onLongPress The callback to call when a long press occurred.
+     */
+    constructor(readonly element: HTMLElement, public onLongPress: (event: MouseEvent) => void) {
+        // workaround - need to bind 'this' for our dynamic mouse move handler
+        this.m_boundMouseMoveHandler = this.onMouseMove.bind(this);
+        this.m_boundMouseDownHandler = this.onMousedown.bind(this);
+        this.m_boundMouseUpHandler = this.onMouseup.bind(this);
+
+        this.element.addEventListener("mousedown", this.m_boundMouseDownHandler);
+        this.element.addEventListener("mouseup", this.m_boundMouseUpHandler);
+    }
+
+    /**
+     * Removes all events listeners. No more events will be sent.
+     */
+    dispose() {
+        this.cancel();
+        this.element.removeEventListener("mousedown", this.m_boundMouseDownHandler);
+        this.element.removeEventListener("mouseup", this.m_boundMouseUpHandler);
+    }
+
+    private onMousedown(event: MouseEvent) {
+        if (event.button !== this.buttonId) {
+            return;
+        }
+        this.cancelTimer();
+
+        this.m_mouseDownEvent = event;
+        this.m_timerId = setTimeout(() => this.onTimeout(), this.timeout) as any;
+        this.addMouseMoveHandler();
+    }
+
+    private onMouseup(event: MouseEvent) {
+        if (event.button !== this.buttonId) {
+            return;
+        }
+        this.cancel();
+    }
+
+    private onMouseMove(event: MouseEvent) {
+        if (this.m_mouseDownEvent === undefined) {
+            return; // Must not happen
+        }
+
+        const manhattanLength =
+            Math.abs(event.clientX - this.m_mouseDownEvent.clientX) +
+            Math.abs(event.clientY - this.m_mouseDownEvent.clientY);
+
+        if (manhattanLength >= this.moveThreshold) {
+            this.cancel();
+        }
+    }
+
+    private cancel() {
+        this.m_mouseDownEvent = undefined;
+        this.cancelTimer();
+        this.removeMouseMoveHandler();
+    }
+
+    private cancelTimer() {
+        if (this.m_timerId === undefined) {
+            return;
+        }
+
+        clearTimeout(this.m_timerId);
+        this.m_timerId = undefined;
+    }
+
+    private addMouseMoveHandler() {
+        if (this.m_moveHandlerRegistered) {
+            return;
+        }
+
+        this.element.addEventListener("mousemove", this.m_boundMouseMoveHandler);
+        this.m_moveHandlerRegistered = true;
+    }
+
+    private removeMouseMoveHandler() {
+        if (!this.m_moveHandlerRegistered) {
+            return;
+        }
+
+        this.element.removeEventListener("mousemove", this.m_boundMouseMoveHandler);
+        this.m_moveHandlerRegistered = false;
+    }
+
+    private onTimeout() {
+        const event = this.m_mouseDownEvent;
+
+        this.m_timerId = undefined;
+        this.cancel();
+
+        if (event !== undefined) {
+            this.onLongPress(event);
+        }
+    }
+}
---
+++ @here/map-controls/test/MapControlsTest.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
+ *
+ * This software and other materials contain proprietary information controlled by HERE and are
+ * protected by applicable copyright legislation. Any use and utilization of this software and other
+ * materials and disclosure to any third parties is conditional upon having a separate agreement
+ * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
+ * agreement, the use of the software is not allowed.
+ */
+
+import { MapView } from "@here/mapview";
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { MapControls } from "../lib/MapControls";
+
+describe("MapControls", () => {
+    let sandbox: sinon.SinonSandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe("on object creation", () => {
+        let addEventListener: () => {};
+        let mapView: MapView;
+        let mapControls: MapControls;
+        let camera: THREE.Camera;
+        let domElement: any;
+        let maxZoom: number;
+        let minZoom: number;
+        let minCameraHeight: number;
+
+        beforeEach(() => {
+            addEventListener = sandbox.stub();
+            camera = {} as any;
+            domElement = { addEventListener } as any;
+            maxZoom = 10;
+            minZoom = 5;
+            minCameraHeight = 100;
+
+            mapView = sandbox.createStubInstance(MapView) as any;
+            sandbox.stub(mapView, "renderer").get(() => ({ domElement }));
+            sandbox.stub(mapView, "camera").get(() => camera);
+            sandbox.stub(mapView, "maxZoomLevel").get(() => maxZoom);
+            sandbox.stub(mapView, "minZoomLevel").get(() => minZoom);
+            sandbox.stub(mapView, "minCameraHeight").get(() => minCameraHeight);
+
+            mapControls = new MapControls(mapView);
+        });
+
+        it("initializes camera property using value from constructor param", () => {
+            expect(mapControls.camera).to.be.equals(camera);
+        });
+
+        it("initializes domElement property using value from constructor param", () => {
+            expect(mapControls.domElement).to.be.equals(domElement);
+        });
+
+        it("initializes minZoomLevel property using value from constructor param", () => {
+            expect(mapControls.minZoomLevel).to.be.equals(minZoom);
+        });
+
+        it("initializes maxZoomLevel property using value from constructor param", () => {
+            expect(mapControls.maxZoomLevel).to.be.equals(maxZoom);
+        });
+
+        it("initializes minCameraHeight property using value from constructor param", () => {
+            expect(mapControls.minCameraHeight).to.be.equals(minCameraHeight);
+        });
+    });
+
+    it("correctly updates mapView on mouse move", () => {
+        const updateStub = sandbox.stub();
+        //@ts-ignore
+        const mapControls = new MapControls({
+            renderer: { domElement: { addEventListener: sandbox.stub() } },
+            update: updateStub
+        });
+        sandbox.stub(mapControls, "dispatchEvent");
+
+        expect(updateStub.callCount).to.be.equal(0);
+        mapControls.mouseMove({ preventDefault: sandbox.stub(), stopPropagation: sandbox.stub() });
+        expect(updateStub.callCount).to.be.equal(1);
+    });
+
+    it("correctly updates mapView on touch move", () => {
+        const updateStub = sandbox.stub();
+        //@ts-ignore
+        const mapControls = new MapControls({
+            renderer: { domElement: { addEventListener: sandbox.stub() } },
+            update: updateStub
+        });
+        mapControls.m_touchState.touches = { length: 5 };
+        sandbox.stub(mapControls, "updateTouches");
+        sandbox.stub(mapControls, "updateTouchState");
+        sandbox.stub(mapControls, "dispatchEvent");
+
+        expect(updateStub.callCount).to.be.equal(0);
+        mapControls.touchMove({
+            touches: [],
+            preventDefault: sandbox.stub(),
+            stopPropagation: sandbox.stub()
+        });
+        expect(updateStub.callCount).to.be.equal(1);
+    });
+});
