2413957168ca1bfd0bc8acc0584d7180d78d9893
Harald Fernengel <harald.fernengel@here.com>
2018-09-18 10:53:59 +0200

    Initial import of verity-examples

---
+++ @here/verity-examples/decoder/decoder.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017-2018 HERE Global B.V. and its affiliate(s).
+ * All rights reserved.
+ *
+ * This software and other materials contain proprietary information
+ * controlled by HERE and are protected by applicable copyright legislation.
+ * Any use and utilization of this software and other materials and
+ * disclosure to any third parties is conditional upon having a separate
+ * agreement with HERE for the access, use, utilization or disclosure of this
+ * software. In the absence of such agreement, the use of the software is not
+ * allowed.
+ */
+
+declare let self: Worker & {
+    importScripts(..._scripts: string[]): void;
+};
+
+self.importScripts("three.min.js");
+
+import GeoJsonWorkerClient from "@here/geojson-datasource/lib/GeoJsonDecoder";
+import OmvWorkerClient from "@here/omv-datasource/lib/OmvDecoder";
+
+/* tslint:disable:no-unused-expression */
+new OmvWorkerClient();
+new OmvWorkerClient("omv-tile-decoder-1");
+new OmvWorkerClient("omv-tile-decoder-2");
+new OmvWorkerClient("omv-tile-decoder-3");
+new GeoJsonWorkerClient();
