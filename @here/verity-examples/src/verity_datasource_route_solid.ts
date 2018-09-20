6bbfc5dc6a0556eabdc6cd8140b435d7c53298e8
Harald Fernengel <harald.fernengel@here.com>
2018-09-20 14:37:18 +0200

    Initial import of verity-examples

---
+++ @here/verity-examples/src/verity_datasource_route_solid.ts
@@ -0,0 +1,271 @@
+/*
+ * Copyright (C) 2017-2018 HERE Global B.V. and its affiliate(s).
+ * All rights reserved.
+ *
+ * This software and other materials contain proprietary information
+ * controlled by HERE and are protected by applicable copyright legislation.
+ * Any use and utilization of this software and other materials and
+ * disclosure to any third parties is conditional upon having a separate
+ * agreement with HERE for the access, use, utilization or disclosure of this
+ * software. In the absence of such agreement, the use of the software is not
+ * allowed.
+ */
+import { GeoCoordinates, hereTilingScheme, TileKey, TilingScheme } from "@here/geoutils";
+import { Lines } from "@here/lines";
+import { MapControls } from "@here/map-controls";
+import { DataSource, MapView, Tile } from "@here/mapview";
+import { SolidLineMaterial } from "@here/materials";
+import { APIFormat, OmvDataSource } from "@here/omv-datasource";
+import * as routing from "@here/routing";
+import { LoggerManager } from "@here/utils";
+import * as THREE from "three";
+import { bearerTokenProvider } from "./common";
+
+const logger = LoggerManager.instance.create("verity_datasource_route_solid");
+
+/**
+ * This example illustrates how to show a route on a map. It uses solid lines to render the route
+ * line.
+ *
+ * The first step is to instantiate a new [[RoutingDataSource]] object. Then reading a pre-defined
+ * JSON containg the route (it mimics the one returned from HERE routing API). Next the
+ * [[RoutingDataSource.setRoute]] method is used to add that route to the data source. Last step is
+ * adding the newly created datasource to the map view.
+ *
+ * ```typescript
+ * [[include:vislib_datasource_route_solid_1.ts]]
+ * ```
+ *
+ * As a result the route is being drawn on the map. Additionally there is a 'Switch route' button
+ * added, which enables changing between two exemplary routes.
+ *
+ */
+export namespace RouteDataSourceExampleSolidLines {
+    /**
+     * Asynchronously loads and validates route from URL objects.
+     */
+    async function loadRouteAsync(url: string): Promise<routing.CalculateRouteResponse> {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`RouteLoader#loadAsync: cannot load a route: ${response.statusText}`);
+        }
+        const route = (await response.json()) as routing.CalculateRouteResponse | null;
+        if (route === null) {
+            throw new Error("RouteLoader#loadAsync: the loaded resource is not a valid JSON.");
+        }
+        return route;
+    }
+
+    /**
+     * Adds a button to enable switching between two exemplary routes.
+     */
+    document.body.innerHTML += `
+    <style>
+        #switch-route-btn {
+            display: inline-block;
+            background-color: rgba(255,255,255,0.5);
+            margin: 20px 0 0 20px;
+            padding: 10px; cursor: pointer;
+            user-select: none;
+        }
+    </style>
+    <div id="switch-route-btn">Toggle Route</div>
+`;
+
+    // creates a new MapView for the HTMLCanvasElement of the given id
+    export function initializeMapView(id: string): MapView {
+        const canvas = document.getElementById(id) as HTMLCanvasElement;
+
+        const sampleMapView = new MapView({
+            canvas,
+            theme: "resources/day.json"
+        });
+
+        sampleMapView.geoCenter = new GeoCoordinates(52.5160501, 13.3769104, 0);
+        sampleMapView.camera.rotateX(THREE.Math.degToRad(0));
+
+        // resize the mapView to maximum
+        sampleMapView.resize(window.innerWidth, window.innerHeight);
+
+        // react on resize events
+        window.addEventListener("resize", () => {
+            sampleMapView.resize(window.innerWidth, window.innerHeight);
+        });
+
+        return sampleMapView;
+    }
+
+    /**
+     * Get a precomputed demo route
+     * There are two precomputed exemplary routes
+     */
+    export function setupDemoRoute(
+        routeNumber = 0,
+        mapView: MapView,
+        routingDataSource: RoutingDataSource,
+        controls: MapControls
+    ) {
+        let url;
+        switch (routeNumber) {
+            case 0: {
+                mapView.geoCenter = new GeoCoordinates(52.5160501, 13.3769104, 0);
+                mapView.camera.position.set(0, 0, 135570);
+                controls.zoomOnTargetPosition(0, 0, 14);
+                url = "./resources/route1.json";
+                break;
+            }
+            case 1: {
+                mapView.geoCenter = new GeoCoordinates(52.5177491, 13.3768845, 0);
+                mapView.camera.position.set(0, 0, 6500);
+                controls.zoomOnTargetPosition(0, 0, 14);
+                url = "./resources/route.json";
+                break;
+            }
+            default: {
+                mapView.camera.position.set(0, 0, 300);
+                url = "./resources/route.json";
+                break;
+            }
+        }
+
+        loadRouteAsync(url)
+            .then((route: routing.CalculateRouteResponse) => {
+                routingDataSource.setRoute(route);
+                logger.log("loadRouteData: successfully loaded route");
+            })
+            .catch(error => {
+                logger.log("loadRouteData: failed to load route", error);
+            });
+    }
+
+    export class RoutingDataSource extends DataSource {
+        private tile: Tile | undefined;
+        private readonly line: THREE.Mesh;
+        private route?: routing.Route;
+        private shouldRecreateGeometry = false;
+
+        constructor(name = "route") {
+            super(name);
+
+            const material = new SolidLineMaterial({
+                color: "#800000",
+                lineWidth: 6
+            });
+
+            this.line = new THREE.Mesh(new THREE.Geometry(), material);
+            this.line.renderOrder = 1000;
+        }
+
+        // tslint:disable-next-line:no-unused-variable
+        shouldRender(zoomLevel: number, tileKey: TileKey) {
+            return tileKey.mortonCode() === 1;
+        }
+
+        getTilingScheme(): TilingScheme {
+            return hereTilingScheme;
+        }
+
+        getTile(tileKey: TileKey): Tile | undefined {
+            if (tileKey.mortonCode() !== 1) {
+                return undefined;
+            }
+            return this.getRootTile();
+        }
+
+        disposeGeometry() {
+            if (this.tile === undefined) {
+                return;
+            }
+            this.line.geometry.dispose();
+            this.line.geometry = new THREE.Geometry();
+        }
+
+        createGeometry(tile: Tile) {
+            if (this.route && this.route.shape !== undefined) {
+                const lines = new Lines();
+                const line = new Array<number>();
+
+                for (let i = 0; i < this.route.shape.length; i += 3) {
+                    const point = new GeoCoordinates(
+                        this.route.shape[i],
+                        this.route.shape[i + 1],
+                        this.route.shape[i + 2]
+                    );
+                    const vertex = this.projection.projectPoint(point, new THREE.Vector3());
+                    vertex.sub(tile.center);
+                    // (<THREE.Geometry>this.m_line.geometry).vertices.push(vertex);
+                    line.push(vertex.x, vertex.y);
+                }
+
+                lines.add(line);
+
+                this.line.geometry = lines.createGeometry();
+            }
+
+            this.requestUpdate();
+        }
+
+        setRoute(response?: routing.CalculateRouteResponse) {
+            this.shouldRecreateGeometry = true;
+
+            if (
+                response === undefined ||
+                response.route === undefined ||
+                response.route.length === 0
+            ) {
+                this.requestUpdate();
+                return;
+            }
+            // predefined (hardcoded) route is used
+            this.route = response.route[0];
+        }
+
+        private getRootTile(): Tile {
+            if (this.tile === undefined) {
+                this.tile = new Tile(this, TileKey.fromRowColumnLevel(0, 0, 0));
+                this.tile.objects.push(this.line);
+            }
+
+            if (this.shouldRecreateGeometry) {
+                this.disposeGeometry();
+                this.createGeometry(this.tile);
+            }
+
+            return this.tile;
+        }
+    }
+
+    function initSwitchRouteButton(mapView: MapView, routingDataSource: RoutingDataSource) {
+        const el = document.getElementById("switch-route-btn");
+
+        if (el === null) {
+            return;
+        }
+
+        let routeNumber = 0;
+        el.onclick = () => {
+            routeNumber = (routeNumber + 1) % 2;
+            setupDemoRoute(routeNumber, mapView, routingDataSource, mapControls);
+        };
+    }
+
+    const newMapView = initializeMapView("mapCanvas");
+    // instantiate the default map controls, allowing the user to pan around freely
+    const mapControls = new MapControls(newMapView);
+
+    // snippet:vislib_datasource_route_solid_1.ts
+    const newRoutingDataSource = new RoutingDataSource();
+    setupDemoRoute(0, newMapView, newRoutingDataSource, mapControls);
+    newMapView.addDataSource(newRoutingDataSource);
+    // end:vislib_datasource_route_solid_1.ts
+
+    initSwitchRouteButton(newMapView, newRoutingDataSource);
+
+    const omvDataSource = new OmvDataSource({
+        baseUrl: "https://vector.cit.data.here.com/1.0/base/mc",
+        apiFormat: APIFormat.HereV1,
+        authenticationCode: bearerTokenProvider
+    });
+
+    newMapView.addDataSource(omvDataSource);
+}
---
+++ @here/verity-examples/src/verity_multiview_syncview.ts
@@ -0,0 +1,198 @@
+/*
+ * Copyright (C) 2017-2018 HERE Global B.V. and its affiliate(s).
+ * All rights reserved.
+ *
+ * This software and other materials contain proprietary information
+ * controlled by HERE and are protected by applicable copyright legislation.
+ * Any use and utilization of this software and other materials and
+ * disclosure to any third parties is conditional upon having a separate
+ * agreement with HERE for the access, use, utilization or disclosure of this
+ * software. In the absence of such agreement, the use of the software is not
+ * allowed.
+ */
+
+import { GeoCoordinates } from "@here/geoutils";
+import { MapControls } from "@here/map-controls";
+import { ConcurrentDecoderFacade, MapView } from "@here/mapview";
+import { APIFormat, OmvDataSource } from "@here/omv-datasource";
+import { WebTileDataSource } from "@here/webtile-datasource";
+
+import { appCode, appId } from "../config";
+import { bearerTokenProvider } from "./common";
+
+/**
+ * A simple example showing two MapViews with different themes or even datasources.
+ *
+ * Create two views with their own MapView and a MaControl
+ * as WebTileDataSourceOptions:
+ * ```typescript
+ * [[include:vislib_syncview_1.ts]]
+ * ```
+ *
+ * After adding the two MapViews with their own datasources, add event handler to sync the two
+ * MapViews:
+ * ```typescript
+ * [[include:vislib_syncview_2.ts]]
+ * ```
+ */
+
+export namespace SyncViewExample {
+    // inject HTML code to page to show second map cancas and position them
+    document.body.innerHTML += `
+
+<style>
+
+    #mapCanvas {
+        position: absolute;
+        border: 0px;
+        left: 0px;
+        width: 100%;
+        height: 50%;
+        top: 0%;
+        overflow: hidden;
+        z-index: -1
+    }
+    #mapCanvas2 {
+        position: absolute;
+        border: 0px;
+        left: 0px;
+        width: 100%;
+        height: 50%;
+        top: 50%;
+        overflow: hidden;
+        z-index: -1
+    }
+
+</style>
+
+<canvas id="mapCanvas2"></canvas>
+`;
+
+    const defaultTheme = "./resources/reducedDay.json";
+
+    // if true, the lower MapView will show the WebTile datasource content
+    const SHOW_WEBTILES = false;
+
+    /**
+     * A pair of MapView and MapController.
+     */
+    export interface ViewControlPair {
+        mapView: MapView;
+        mapControls: MapControls;
+    }
+
+    /**
+     * Creates the pair of MapView and MapControllers required to sync the two views.
+     *
+     * @param id ID of HTML canvas element
+     * @param theme URL of theme to load
+     * @param decoderUrl URL of decoder bundle
+     */
+    export function initializeMapView(
+        id: string,
+        theme?: string,
+        decoderUrl?: string
+    ): ViewControlPair {
+        const canvas = document.getElementById(id) as HTMLCanvasElement;
+
+        const sampleMapView = new MapView({
+            canvas,
+            theme: theme !== undefined ? theme : defaultTheme,
+            decoderUrl
+        });
+
+        // let the camera float over the map, looking straight down
+        sampleMapView.camera.position.set(0, 0, 800);
+        // center the camera somewhere around Berlin geo locations
+        sampleMapView.geoCenter = new GeoCoordinates(52.518611, 13.376111, 0);
+
+        // instantiate the default map controls, allowing the user to pan around freely.
+        const mapControls = new MapControls(sampleMapView);
+
+        // resize the mapView to maximum
+        sampleMapView.resize(window.innerWidth, window.innerHeight / 2);
+
+        // react on resize events
+        window.addEventListener("resize", () => {
+            sampleMapView.resize(window.innerWidth, window.innerHeight / 2);
+        });
+
+        return { mapView: sampleMapView, mapControls };
+    }
+
+    // create two MapViews, each with their own theme file
+    // snippet:vislib_syncview_1.ts
+    const view1 = initializeMapView("mapCanvas", "./resources/theme.json", "./decoder.bundle.js");
+    const view2 = initializeMapView(
+        "mapCanvas2",
+        "./resources/reducedDay.json",
+        "./decoder.bundle.js"
+    );
+    // end:vislib_syncview_1.ts
+
+    const omvDataSource = new OmvDataSource({
+        baseUrl: "https://vector.cit.data.here.com/1.0/base/mc",
+        apiFormat: APIFormat.HereV1,
+        maxZoomLevel: 17,
+        authenticationCode: bearerTokenProvider,
+        decoder: ConcurrentDecoderFacade.getTileDecoder("omv-tile-decoder-1")
+    });
+
+    view1.mapView.addDataSource(omvDataSource);
+
+    if (SHOW_WEBTILES) {
+        const webTileDataSource = new WebTileDataSource({
+            appId,
+            appCode
+        });
+
+        view2.mapView.addDataSource(webTileDataSource);
+    } else {
+        const omvDataSource2 = new OmvDataSource({
+            baseUrl: "https://vector.cit.data.here.com/1.0/base/mc",
+            apiFormat: APIFormat.HereV1,
+            maxZoomLevel: 17,
+            decoder: ConcurrentDecoderFacade.getTileDecoder("omv-tile-decoder-2"),
+            authenticationCode: bearerTokenProvider
+        });
+
+        view2.mapView.addDataSource(omvDataSource2);
+    }
+
+    /**
+     * A function that copies the position and orientation of one MapView/MapControl
+     * @param srcView Source with MapView with current location and MapControl with current camera
+     *                  position and orientation
+     * @param destView Destination MapView synced to current location; MapControl synced to current
+     *                  position and orientation
+     */
+    // snippet:vislib_syncview_2.ts
+    export const syncMapViews = (srcView: ViewControlPair, destView: ViewControlPair) => {
+        const ypr = srcView.mapControls.yawPitchRoll;
+        destView.mapControls.setRotation(ypr.yaw, ypr.pitch);
+
+        destView.mapView.worldCenter.copy(srcView.mapView.worldCenter);
+
+        destView.mapControls.cameraHeight = srcView.mapControls.cameraHeight;
+
+        // force update on changed MapView
+        destView.mapView.update();
+    };
+
+    // sync camera of view2 if view1 changes.
+    view1.mapControls.addEventListener(
+        "update",
+        (): void => {
+            syncMapViews(view1, view2);
+        }
+    );
+
+    // sync camera of view1 if view2 changes.
+    view2.mapControls.addEventListener(
+        "update",
+        (): void => {
+            syncMapViews(view2, view1);
+        }
+    );
+    // end:vislib_syncview_2.ts
+}
