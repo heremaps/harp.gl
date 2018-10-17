65e8cc4956a43fe4f9016fcddb7aee6f29f67869
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2018-10-17 13:54:35 +0200

    @here/fetch supports file:// protocol and relative paths in Node.js env.

--- @here/fetch/index.ts
+++ @here/fetch/index.ts
@@ -6,6 +6,8 @@
 
 // tslint:disable-next-line:no-var-requires
 const node_fetch = require("node-fetch");
+import * as fs from "fs";
+import { URL } from "url";
 
 declare const global: any;
 
@@ -21,7 +23,7 @@ class AbortController {
 }
 
 if (global.fetch === undefined) {
-    global.fetch = node_fetch;
+    global.fetch = fetchWithFileSupport;
     global.Response = node_fetch.Response;
     global.Headers = node_fetch.Headers;
     global.Request = node_fetch.Request;
@@ -30,3 +32,76 @@ if (global.fetch === undefined) {
 }
 
 export type FetchFunction = typeof fetch;
+
+/**
+ * `fetch` with local file system support.
+ *
+ * WHATWG `fetch` compliant decorator over `node-fetch` that
+ *
+ * * supports `file:` protocol
+ * * treats relative URIs as relative to `process.cwd()`
+ *
+ * `@here/fetch` exposes this function as `global.fetch` Node.js environment, so it mimics
+ * _browser_ `fetch` that supports relative URL's which are resolved against baseUrl. It also
+ * allows to APIs whom use `fetch` internally, by passing them `file://` resources without need to
+ * stub `global.fetch`.
+ *
+ * *Use only for testing purposes*.
+ *
+ * Example:
+ * ```
+ *     const fetchStub = sandbox.stub(global as any, "fetch");
+ *     fetchStub.callsFake(fetchWithFileSupport);
+ *     await someApi.loadSomethingWithFetch("./test/resources/x.json");
+ *     assert.equal(fetchStub, 1);
+ * ```
+ *
+ * @see [fetch documentation](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+ *
+ * @hidden
+ */
+export function fetchWithFileSupport(input: RequestInfo, init?: RequestInit): Promise<Response> {
+    const url = typeof input === "object" ? input.url : input;
+    const parentUrl = `file://${process.cwd()}/`;
+    const actualUrl = new URL(url, parentUrl);
+    if (actualUrl.protocol === "file:") {
+        return new Promise<Response>((resolve, reject) => {
+            fs.readFile(actualUrl, (error, buffer) => {
+                if (error) {
+                    reject(new Error(`failed to read file ${actualUrl}: ${error}`));
+                }
+
+                const response = {
+                    headers: new Headers(),
+                    ok: true,
+                    status: 200,
+                    statusText: "OK",
+                    type: "basic",
+                    url: actualUrl.toString(),
+                    size: buffer.byteLength,
+                    timeout: 0,
+                    body: null as any,
+                    bodyUsed: false,
+                    buffer() {
+                        return Promise.resolve(buffer);
+                    },
+                    arrayBuffer() {
+                        return Promise.resolve(buffer.buffer as ArrayBuffer);
+                    },
+                    json() {
+                        return Promise.resolve(JSON.parse(buffer.toString("utf-8")));
+                    },
+                    text() {
+                        return Promise.resolve(buffer.toString("utf-8"));
+                    },
+                    clone() {
+                        return Object.assign({}, this);
+                    }
+                };
+                resolve((response as any) as Response);
+            });
+        });
+    } else {
+        return node_fetch(url, init);
+    }
+}
