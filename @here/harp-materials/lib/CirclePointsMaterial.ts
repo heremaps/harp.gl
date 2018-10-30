48c211960039a22db473da989a97af5c44ba5c97
Harald Fernengel <harald.fernengel@here.com>
2018-10-30 14:25:34 +0100

    Remove obsolete license

--- @here/harp-materials/lib/CirclePointsMaterial.ts
+++ @here/harp-materials/lib/CirclePointsMaterial.ts
@@ -4,16 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * Copyright (C) 2018 HERE Global B.V. and its affiliate(s). All rights reserved.
- *
- * This software and other materials contain proprietary information controlled by HERE and are
- * protected by applicable copyright legislation. Any use and utilization of this software and other
- * materials and disclosure to any third parties is conditional upon having a separate agreement
- * with HERE for the access, use, utilization or disclosure of this software. In the absence of such
- * agreement, the use of the software is not allowed.
- */
-
 import { Color, ShaderMaterial, Uniform } from "three";
 
 const vertexShader: string = `
