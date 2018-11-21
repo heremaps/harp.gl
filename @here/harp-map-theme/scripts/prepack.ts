5a75f360a212b7f5d24fdd3b2ef346a8068834a0
Harald Fernengel <harald.fernengel@here.com>
2018-11-21 13:58:35 +0100

    HARP-3806: Include fonts in harp-map-theme

---
+++ @here/harp-map-theme/scripts/prepack.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ncp } from 'ncp';
+import * as path from 'path';
+
+// tslint:disable:no-console
+
+function onCopyError(err: Error) {
+    if (err === null) {
+        return;
+    }
+
+    console.error("Unable to copy files:", err);
+    process.exitCode = 1;
+}
+
+const fontDir = path.dirname(require.resolve("@here/harp-font-resources/package.json"));
+
+ncp(path.join(fontDir, "resources", "fonts"),
+    path.join(__dirname, "..", "resources", "fonts"),
+    onCopyError);
