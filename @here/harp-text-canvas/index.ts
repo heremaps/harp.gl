15588e33497daafe456420208fbbaec8d35a3955
Harald Fernengel <harald.fernengel@here.com>
2018-12-04 18:45:05 +0100

    Fix tests

--- @here/harp-text-canvas/index.ts
+++ @here/harp-text-canvas/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export * from "./lib/rendering/FontCatalog";
 export * from "./lib/rendering/TextStyle";
 export * from "./lib/TextCanvas";
