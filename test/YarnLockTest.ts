47fe0e37460ac4e7538581f9d7dbda70d9d49a73
Harald Fernengel <harald.fernengel@here.com>
2018-12-18 23:54:11 +0100

    Unit test preventing private URLs in yarn.lock

---
+++ test/YarnLockTest.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2017-2018 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as fs from "fs";
+import * as path from "path";
+
+import { assert } from "chai";
+
+// tslint:disable:only-arrow-functions
+
+describe("YarnLockFile", function() {
+    it("Contains only public paths", function() {
+        const yarnLock = fs.readFileSync(path.join(__dirname, "..", "yarn.lock"));
+        assert.isFalse(yarnLock.includes("in.here.com"));
+    });
+});
