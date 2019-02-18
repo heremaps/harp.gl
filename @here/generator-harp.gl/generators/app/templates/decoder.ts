79ed2e0eeee9fed3f964f0a1d96f07a758f76414
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2019-02-18 14:59:47 +0100

    HARP-3791: Yeoman generator for harp.gl
    Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

---
+++ @here/generator-harp.gl/generators/app/templates/decoder.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+declare let self: Worker & {
+    importScripts(..._scripts: string[]): void;
+};
+
+self.importScripts("three.min.js");
+
+import { OmvTileDecoderService } from "@here/harp-omv-datasource/index-worker";
+
+OmvTileDecoderService.start();
---
+++ @here/generator-harp.gl/test/test.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const fsExtra = require("fs.extra");
+const path = require("path");
+const yeomanTestHelpers = require("yeoman-test");
+const {
+    assert
+} = require("chai");
+const {
+    spawnSync
+} = require('child_process');
+
+/*
+
+    Note, in certain environments, custom .npmrc may be needed to run these tests, use like this:
+
+    USE_NPMRC=~/.npmrc npx mocha
+
+*/
+describe("harp.gl:app", function () {
+
+    this.timeout(300000);
+
+    let helperDirectory;
+
+    before(async function () {
+        helperDirectory = await yeomanTestHelpers.run(path.join(__dirname, '../generators/app'))
+            .inTmpDir(function (dir) {
+                if (process.env.USE_NPMRC) {
+                    const targetPath = path.join(dir, ".npmrc");
+                    fs.copyFileSync(process.env.USE_NPMRC, targetPath);
+                }
+            })
+            .withPrompts({
+                'appname': 'test_name'
+            });
+
+        assert.isString(helperDirectory);
+    });
+
+    it("webpack", async function () {
+        const installStatus = spawnSync("npm", ["install"], {
+            cwd: helperDirectory
+        });
+        assert.strictEqual(installStatus.status, 0, installStatus.output);
+
+        const buildStatus = spawnSync("npm", ["run", "build"], {
+            cwd: helperDirectory
+        });
+        assert.strictEqual(buildStatus.status, 0, buildStatus.output);
+    });
+
+    after(function () {
+        if (process.env.KEEP_TEMP_DIR) {
+            console.log("Keeping", helperDirectory);
+        } else {
+            fsExtra.rmrfSync(helperDirectory);
+        }
+    });
+});
