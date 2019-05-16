10885024018e131a48fdcb60d3c0840d46659dd7
Ignacio Julve <41909512+musculman@users.noreply.github.com>
2019-05-16 09:43:56 +0200

    HARP-5577 Bundle deploy scripts in a file and reorder the deploy steps
    Signed-off-by: Ignacio Julve <41909512+musculman@users.noreply.github.com>

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+#
+# Simple script that bundles the publishing of packages
+# to be run from Travis
+#
+
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
+npx lerna publish -y from-git
