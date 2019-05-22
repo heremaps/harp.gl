30519e1666d30d302acf1fa90620fa7e3b50a8a4
Nino Kettlitz <1396039+ninok@users.noreply.github.com>
2019-05-22 08:55:13 +0200

    HARP-5896: Added dancing dude example
    Signed-off-by: Nino Kettlitz <1396039+ninok@users.noreply.github.com>

--- @here/harp-examples/src/threejs_add-simple-object.ts
+++ @here/harp-examples/src/threejs_add-simple-object.ts
@@ -4,17 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { GeoCoordinates } from "@here/harp-geoutils";
-import { MapControls, MapControlsUI } from "@here/harp-map-controls";
-import {
-    CopyrightElementHandler,
-    CopyrightInfo,
-    MapView,
-    MapViewEventNames
-} from "@here/harp-mapview";
-import { APIFormat, OmvDataSource } from "@here/harp-omv-datasource";
+import { MapView, MapViewEventNames } from "@here/harp-mapview";
 import * as THREE from "three";
-import { accessToken } from "../config";
+import { HelloWorldExample } from "./hello";
 
 /**
  * This example builds on top of the [[HelloWorldExample]], so please consult that first for any
@@ -38,14 +30,19 @@ import { accessToken } from "../config";
  * [[include:harp_gl_threejs_add_simple_object_2.ts]]
  * ```
  *
- * In harp.gl, we position elements [relative to
- * center](http://help.agi.com/AGIComponents/html/BlogPrecisionsPrecisions.htm), which helps to
- * ensure accuracy.
+ * For ensuring enough precision close to the camera harp.gl uses [relative to
+ * center](http://help.agi.com/AGIComponents/html/BlogPrecisionsPrecisions.htm).
  *
- * The drawback of this is that we need to update the position of the cube each frame. This is
- * fortunately easy to do and is shown here:
+ * Therefore we have to adjust the position of the object each frame. We do this
+ * by adding an event listener to the map view that is called whenever the map is
+ * rendered:
  * ```typescript
  * [[include:harp_gl_threejs_add_simple_object_3.ts]]
+ * ```
+ * In the callback we just subtract the [[MapView.worldCenter]] from the desired
+ * world position of the object:
+ * ```typescript
+ * [[include:harp_gl_threejs_add_simple-object_rtc.ts]]
  * ```
  *
  * Finally, in order to see the cube rendered on the map, we need to force an update.
@@ -69,39 +66,13 @@ export namespace ThreejsAddSimpleObject {
     // end:harp_gl_threejs_add_simple_object_0.ts
 
     // Create a new MapView for the HTMLCanvasElement of the given id.
-    function initializeMapView(id: string): MapView {
-        const canvas = document.getElementById(id) as HTMLCanvasElement;
-        const map = new MapView({
-            canvas,
-            theme: "resources/berlin_tilezen_base.json"
-        });
-
-        CopyrightElementHandler.install("copyrightNotice", map);
-
-        // Center the camera on Manhattan, New York City.
-        map.setCameraGeolocationAndZoom(new GeoCoordinates(40.6935, -74.009), 16.9);
-
-        // Instantiate the default map controls, allowing the user to pan around freely.
-        const mapControls = new MapControls(map);
-        mapControls.maxPitchAngle = 50;
-        mapControls.setRotation(6.3, 50);
-
-        // Add an UI.
-        const ui = new MapControlsUI(mapControls);
-        canvas.parentElement!.appendChild(ui.domElement);
-
-        // Resize the mapView to maximum.
-        map.resize(window.innerWidth, window.innerHeight);
-
-        // React on resize events.
-        window.addEventListener("resize", () => {
-            map.resize(window.innerWidth, window.innerHeight);
-        });
+    function addMouseEventListener(mapView: MapView) {
+        const canvas = mapView.canvas;
 
         canvas.addEventListener("mousedown", event => {
             // snippet:harp_gl_threejs_add_simple_object_1.ts
             // Get the position of the mouse in world space.
-            const worldPositionAtMouse = map.getWorldPositionAt(event.pageX, event.pageY);
+            const worldPositionAtMouse = mapView.getWorldPositionAt(event.pageX, event.pageY);
             if (worldPositionAtMouse === null) {
                 return;
             }
@@ -109,26 +80,29 @@ export namespace ThreejsAddSimpleObject {
 
             // snippet:harp_gl_threejs_add_simple_object_2.ts
             const cube = createPinkCube();
-            map.scene.add(cube);
+            mapView.scene.add(cube);
             // end:harp_gl_threejs_add_simple_object_2.ts
 
-            // snippet:harp_gl_threejs_add_simple_object_3.ts
-            // Add a callback to execute before the items are rendered.
-            map.addEventListener(MapViewEventNames.Render, () => {
+            const onRender = () => {
                 // Set the cube position relative to the world center. Note, we don't subtract the
                 // [[worldCenter]] from the worldMousePosition, because we need to keep the cubes
                 // world position untouched.
-                cube.position.copy(worldPositionAtMouse).sub(map.worldCenter);
-            });
+
+                // snippet:harp_gl_threejs_add_simple-object_rtc.ts
+                cube.position.copy(worldPositionAtMouse).sub(mapView.worldCenter);
+                // end:harp_gl_threejs_add_simple-object_rtc.ts
+            };
+
+            // snippet:harp_gl_threejs_add_simple_object_3.ts
+            // Add a callback to execute before the items are rendered.
+            mapView.addEventListener(MapViewEventNames.Render, onRender);
             // end:harp_gl_threejs_add_simple_object_3.ts
 
             // snippet:harp_gl_threejs_add_simple_object_4.ts
             // Force the scene to be rerendered once the cube is added to the scene.
-            map.update();
+            mapView.update();
             // end:harp_gl_threejs_add_simple_object_4.ts
         });
-
-        return map;
     }
 
     const message = document.createElement("div");
@@ -140,24 +114,5 @@ export namespace ThreejsAddSimpleObject {
     message.style.right = "10px";
     document.body.appendChild(message);
 
-    const mapView = initializeMapView("mapCanvas");
-
-    const hereCopyrightInfo: CopyrightInfo = {
-        id: "here.com",
-        year: new Date().getFullYear(),
-        label: "HERE",
-        link: "https://legal.here.com/terms"
-    };
-    const copyrights: CopyrightInfo[] = [hereCopyrightInfo];
-
-    const omvDataSource = new OmvDataSource({
-        baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
-        apiFormat: APIFormat.XYZOMV,
-        styleSetName: "tilezen",
-        maxZoomLevel: 17,
-        authenticationCode: accessToken,
-        copyrightInfo: copyrights
-    });
-
-    mapView.addDataSource(omvDataSource);
+    addMouseEventListener(HelloWorldExample.mapView);
 }
