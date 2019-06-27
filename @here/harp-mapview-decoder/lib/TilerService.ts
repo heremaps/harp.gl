heremaps/harp.gl

commit b994dd4f13627fc5c0d9846291b024549a606f56

HARP-6161 Added geoJsonTiler for easier writing ibct tests (without web worksers)
Signed-off-by: Arkadiusz Spiewak <ext-arkadiusz.spiewak@here.com>

@here/harp-mapview-decoder/lib/TilerService.ts
@@ -4,16 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { WorkerTilerProtocol } from "@here/harp-datasource-protocol";
+import { ITiler, WorkerTilerProtocol } from "@here/harp-datasource-protocol";
 import { TileKey } from "@here/harp-geoutils";
-import { LoggerManager } from "@here/harp-utils";
+import { GeoJsonTiler } from "./GeoJsonTiler";
 import { WorkerService, WorkerServiceResponse } from "./WorkerService";
-
-declare const require: any;
-// tslint:disable-next-line:no-var-requires
-const geojsonvt = require("geojson-vt");
-
-const logger = LoggerManager.instance.create("TilerService");
 
 /**
  * An extension to [[WorkerService]], the `TilerService` implements an asynchronous (message based)
@@ -34,8 +28,7 @@ export class TilerService extends WorkerService {
     static start(serviceId: string) {
         return new TilerService(serviceId);
     }
-
-    private m_tileIndexMap: Map<string, any>;
+    tiler: ITiler = new GeoJsonTiler();
 
     /**
      * Set up the `TilerService`. The name of the service must be unique
@@ -44,7 +37,6 @@ export class TilerService extends WorkerService {
      */
     constructor(readonly serviceId: string) {
         super(serviceId);
-        this.m_tileIndexMap = new Map();
     }
 
     /**
@@ -68,57 +60,28 @@ export class TilerService extends WorkerService {
     private async handleTileRequest(
         request: WorkerTilerProtocol.TileRequest
     ): Promise<WorkerServiceResponse> {
-        const tileIndex = this.m_tileIndexMap.get(request.index);
-        if (tileIndex === undefined) {
-            return { response: {} };
-        }
-
         const tileKey = TileKey.fromMortonCode(request.tileKey);
-        const tile = tileIndex.getTile(tileKey.level, tileKey.column, tileKey.row);
+        const tile = this.tiler.getTile(request.index, tileKey);
+
         return { response: tile || {} };
     }
 
     private async handleRegisterIndexRequest(
         message: WorkerTilerProtocol.RegisterIndexRequest
     ): Promise<WorkerServiceResponse> {
-        const tileIndex = this.m_tileIndexMap.get(message.id);
-        if (tileIndex === undefined) {
-            await this.writeIndex(message);
-        }
+        const input = typeof message.input === "string" ? new URL(message.input) : message.input;
+        await this.tiler.registerIndex(message.id, input);
+
         return { response: {} };
     }
 
     private async handleUpdateIndexRequest(
         message: WorkerTilerProtocol.UpdateIndexRequest
     ): Promise<WorkerServiceResponse> {
-        return this.writeIndex(message);
-    }
+        const input = typeof message.input === "string" ? new URL(message.input) : message.input;
 
-    private async writeIndex(
-        message: WorkerTilerProtocol.RegisterIndexRequest | WorkerTilerProtocol.UpdateIndexRequest
-    ): Promise<WorkerServiceResponse> {
-        let json = message.input;
-        if (typeof message.input === "string") {
-            const response = await fetch(message.input);
-            if (!response.ok) {
-                logger.error(`${message.input} Status Text:  ${response.statusText}`);
-                return { response: {} };
-            }
-            json = await response.json();
-        }
-        const tileIndex = geojsonvt.default(json, {
-            maxZoom: 20, // max zoom to preserve detail on
-            indexMaxZoom: 5, // max zoom in the tile index
-            indexMaxPoints: 100000, // max number of points per tile in the tile index
-            tolerance: 3, // simplification tolerance (higher means simpler)
-            extent: 4096, // tile extent
-            buffer: 0, // tile buffer on each side
-            lineMetrics: false, // whether to calculate line metrics
-            promoteId: null, // name of a feature property to be promoted to feature.id
-            generateId: false, // whether to generate feature ids. Cannot be used with promoteId
-            debug: 0 // logging level (0, 1 or 2)
-        });
-        this.m_tileIndexMap.set(message.id, tileIndex);
+        this.tiler.updateIndex(message.id, input);
+
         return { response: {} };
     }
 }

@here/harp-features-datasource/lib/FeaturesDataSource.ts
@@ -33,7 +33,7 @@ export class FeaturesDataSource extends OmvDataSource {
      */
     constructor(workerTilerUrl?: string) {
         super({
-            dataProvider: new GeoJsonDataProvider(NAME, DEFAULT_GEOJSON, workerTilerUrl)
+            dataProvider: new GeoJsonDataProvider(NAME, DEFAULT_GEOJSON, { workerTilerUrl })
         });
         this.addTileBackground = false;
     }

@here/harp-geojson-datasource/lib/GeoJsonDataProvider.ts
@@ -10,6 +10,21 @@ import { TileKey } from "@here/harp-geoutils";
 import { ConcurrentTilerFacade } from "@here/harp-mapview";
 import { DataProvider } from "@here/harp-mapview-decoder";
 
+export interface GeoJsonDataProviderOptions {
+    /**
+     * Worker script hosting [[Tiler Service]]
+     * @default `./decoder.bundle.ts`
+     */
+    workerTilerUrl?: string;
+
+    /**
+     * Custom tiler instance.
+     * If not provided, [[GeoJsonDataProvider]] will obtain [[WorkerBasedTiler]]
+     * from [[ConcurrentTilerFacade]].
+     */
+    tiler?: ITiler;
+}
+
 /**
  * GeoJson [[DataProvider]]. Automatically handles tiling and simplification of static GeoJson.
  */
@@ -22,11 +37,17 @@ export class GeoJsonDataProvider implements DataProvider {
      *
      * @param name Name to be used to reference this `DataProvider`
      * @param input URL of the GeoJSON, or a GeoJSON.
-     *
+     * @param options Optional
      * @returns New `GeoJsonDataProvider`.
      */
-    constructor(readonly name: string, public input: URL | GeoJson, workerTilerUrl?: string) {
-        this.m_tiler = ConcurrentTilerFacade.getTiler("omv-tiler", workerTilerUrl);
+    constructor(
+        readonly name: string,
+        public input: URL | GeoJson,
+        options?: GeoJsonDataProviderOptions
+    ) {
+        this.m_tiler =
+            (options && options.tiler) ||
+            ConcurrentTilerFacade.getTiler("omv-tiler", options && options.workerTilerUrl);
     }
 
     async connect(): Promise<void> {

@here/harp-mapview-decoder/index-worker.ts
@@ -9,3 +9,4 @@ export * from "./lib/TileDecoderService";
 export * from "./lib/TilerService";
 export * from "./lib/WorkerService";
 export * from "./lib/WorkerServiceManager";
+export * from "./lib/GeoJsonTiler";

@here/harp-mapview-decoder/lib/GeoJsonTiler.ts
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { GeoJson, ITiler } from "@here/harp-datasource-protocol";
+import { TileKey } from "@here/harp-geoutils";
+
+// tslint:disable-next-line:no-var-requires
+const geojsonvt = require("geojson-vt");
+
+interface GeoJsonVtIndex {
+    getTile(level: number, column: number, row: number): any;
+}
+
+export class GeoJsonTiler implements ITiler {
+    indexes: Map<string, GeoJsonVtIndex>;
+
+    constructor() {
+        this.indexes = new Map();
+    }
+
+    dispose() {
+        /* */
+    }
+
+    async connect(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    async registerIndex(indexId: string, input: URL | GeoJson): Promise<void> {
+        if (this.indexes.has(indexId)) {
+            return;
+        }
+        return this.updateIndex(indexId, input);
+    }
+
+    async updateIndex(indexId: string, input: URL | GeoJson): Promise<void> {
+        if (input instanceof URL) {
+            const response = await fetch(input.href);
+            if (!response.ok) {
+                throw new Error(
+                    `GeoJsonTiler: Unable to fetch ${input.href}: ${response.statusText}`
+                );
+            }
+            input = await response.json();
+        } else {
+            input = input as GeoJson;
+        }
+
+        this.indexes.set(
+            indexId,
+            geojsonvt.default(input, {
+                maxZoom: 20, // max zoom to preserve detail on
+                indexMaxZoom: 5, // max zoom in the tile index
+                indexMaxPoints: 100000, // max number of points per tile in the tile index
+                tolerance: 3, // simplification tolerance (higher means simpler)
+                extent: 4096, // tile extent
+                buffer: 0, // tile buffer on each side
+                lineMetrics: false, // whether to calculate line metrics
+                promoteId: null, // name of a feature property to be promoted to feature.id
+                generateId: false, // whether to generate feature ids. Cannot be used with promoteId
+                debug: 0 // logging level (0, 1 or 2)
+            })
+        );
+    }
+
+    async getTile(indexId: string, tileKey: TileKey): Promise<{}> {
+        const index = this.indexes.get(indexId);
+        if (index === undefined) {
+            throw new Error("Tile not found");
+        }
+        return index.getTile(tileKey.level, tileKey.column, tileKey.row);
+    }
+}

commit 49f32c8859098238bac139fbc8172dcc9ca457ca

Fix GeoJsonDataProvider caused by improper async usage in TilerService.
Fixes

   Error: DataCloneError: Failed to execute 'postMessage' on
      'DedicatedWorkerGlobalScope': #<Promise> could not be cloned

error cause introduced in #526.

Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

@here/harp-mapview-decoder/lib/TilerService.ts
@@ -61,7 +61,7 @@ export class TilerService extends WorkerService {
         request: WorkerTilerProtocol.TileRequest
     ): Promise<WorkerServiceResponse> {
         const tileKey = TileKey.fromMortonCode(request.tileKey);
-        const tile = this.tiler.getTile(request.index, tileKey);
+        const tile = await this.tiler.getTile(request.index, tileKey);
 
         return { response: tile || {} };
     }