c30df2230883544ec7757cb92226a69cad819904
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2019-07-15 10:44:49 +0200

    Expr: Fix semantics of length()
    Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

---
+++ @here/harp-datasource-protocol/test/ExprTest.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// tslint:disable:only-arrow-functions
+//    Mocha discourages using arrow functions, see https://mochajs.org/#arrow-functions
+
+import { assert } from "chai";
+import { Env, MapEnv, Parser, Value, ValueMap } from "../lib/Expr";
+
+function evaluate(expr: string, env?: Env | ValueMap): Value {
+    if (typeof env === "object" && !(env instanceof Env)) {
+        env = new MapEnv(env);
+    }
+    return new Parser(expr).parse().evaluate(env || new Env());
+}
+
+describe("Expr", function() {
+    describe("language features", function() {
+        it("supports length function", function() {
+            assert.equal(evaluate("length('foo')"), 3);
+            assert.equal(evaluate("length('')"), 0);
+
+            const env = { str: "bar", n: 3 };
+            assert.equal(evaluate("length(str)", env), 3);
+            assert.equal(evaluate("length(n)", env), undefined);
+            assert.equal(evaluate("length(1 == 1)"), undefined);
+
+            assert.throws(() => evaluate("length()"), "Syntax error");
+        });
+        it("supports has function", function() {
+            assert.equal(evaluate("has(foo)"), false);
+
+            const env = { str: "bar", n: 3 };
+            assert.equal(evaluate("has(str)", env), true);
+            assert.equal(evaluate("has(n)", env), true);
+            assert.equal(evaluate("has(foo)", env), false);
+        });
+    });
+});
