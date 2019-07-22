91312d5bfb3c72caa2ba45a52fa91514e2fc2617
Adrien Coffre <Astrak@users.noreply.github.com>
2019-07-22 15:36:39 +0200

    HARP-6118: Implement whole GeoJson management in FeaturesDataSource. (#583)
    Signed-off-by: Adrien Coffre <ext-adrien.coffre@here.com>

---
+++ @here/harp-examples/src/geojson-viewer.ts
@@ -0,0 +1,299 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { StyleSet } from "@here/harp-datasource-protocol";
+import { FeaturesDataSource } from "@here/harp-features-datasource";
+import { GeoCoordinates } from "@here/harp-geoutils";
+import { MapControls, MapControlsUI } from "@here/harp-map-controls";
+import { CopyrightElementHandler, CopyrightInfo, MapView } from "@here/harp-mapview";
+import { APIFormat, OmvDataSource } from "@here/harp-omv-datasource";
+import { accessToken } from "../config";
+
+/**
+ * In this example we avail ourselves of the [[FeaturesDataSource]] and its `setFromGeoJson` method
+ * to read GeoJSON from a file or from a textarea in the page. A default style is applied for all
+ * possible geometry types.
+ */
+export namespace GeoJsonExample {
+    const editorWidth = "550px";
+    const map = createBaseMap();
+
+    setUpFilePicker();
+    setUpEditor();
+
+    const featuresDataSource = new FeaturesDataSource();
+    map.addDataSource(featuresDataSource).then(() => {
+        featuresDataSource.setStyleSet(getStyleSet());
+    });
+
+    function setUpEditor() {
+        const editorInput = document.querySelector("#editor textarea") as HTMLTextAreaElement;
+        editorInput.placeholder = `{
+    type: "FeatureCollection",
+    features:[
+
+    ]
+}`;
+        const updateButton = document.querySelector("#editor button") as HTMLButtonElement;
+        updateButton.addEventListener("click", () => {
+            const geojson = JSON.parse(
+                (document.querySelector("#editor textarea") as HTMLTextAreaElement).value
+            );
+            featuresDataSource.setFromGeojson(geojson);
+        });
+    }
+
+    function getStyleSet(): StyleSet {
+        return [
+            {
+                when: "$geometryType == 'polygon'",
+                technique: "fill",
+                renderOrder: 10000,
+                attr: {
+                    color: "#7cf",
+                    transparent: true,
+                    opacity: 0.8,
+                    lineWidth: 1,
+                    lineColor: "#003344"
+                }
+            },
+            {
+                when: "$geometryType == 'polygon'",
+                technique: "solid-line",
+                renderOrder: 10001,
+                attr: {
+                    color: "#8df",
+                    metricUnit: "Pixel",
+                    lineWidth: 5
+                }
+            },
+            {
+                when: "$geometryType == 'point'",
+                technique: "circles",
+                renderOrder: 10002,
+                attr: {
+                    size: 10,
+                    color: "5ad"
+                }
+            },
+            {
+                when: "$geometryType == 'line'",
+                technique: "solid-line",
+                renderOrder: 10000,
+                attr: {
+                    color: "#8df",
+                    metricUnit: "Pixel",
+                    lineWidth: 5
+                }
+            }
+        ];
+    }
+
+    function processGeoJsonFile(file: File) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            (document.querySelector(
+                "#editor textarea"
+            ) as HTMLTextAreaElement).value = reader.result as string;
+            featuresDataSource.setFromGeojson(JSON.parse(reader.result as string));
+            map.update();
+        };
+        if (file.type === "application/geo+json" || file.type === "application/json") {
+            reader.readAsText(file);
+        }
+    }
+
+    function setUpFilePicker() {
+        const input = document.getElementById("input") as HTMLInputElement;
+        input.addEventListener("change", e => {
+            const file = input.files![0];
+            processGeoJsonFile(file);
+        });
+
+        const overlay = document.getElementById("drag-overlay") as HTMLDivElement;
+        window.addEventListener("dragover", e => {
+            e.preventDefault();
+            overlay.style.display = "block";
+        });
+        const removeOverlay = (e: any) => {
+            e.preventDefault();
+            overlay.style.display = "none";
+        };
+        overlay.addEventListener("dragleave", removeOverlay);
+        overlay.addEventListener("dragend", removeOverlay);
+        overlay.addEventListener("dragexit", removeOverlay);
+        overlay.addEventListener(
+            "drop",
+            e => {
+                removeOverlay(e);
+                const file = e.dataTransfer!.files[0];
+                processGeoJsonFile(file);
+            },
+            false
+        );
+    }
+
+    function createBaseMap(): MapView {
+        document.body.innerHTML += getExampleHTML();
+
+        const canvas = document.getElementById("mapCanvas") as HTMLCanvasElement;
+        const mapView = new MapView({
+            canvas,
+            theme: "resources/berlin_tilezen_night_reduced.json"
+        });
+        mapView.renderLabels = false;
+        mapView.setCameraGeolocationAndZoom(new GeoCoordinates(30, 0), 2);
+
+        CopyrightElementHandler.install("copyrightNotice", mapView);
+
+        const controls = new MapControls(mapView);
+        const ui = new MapControlsUI(controls);
+        const editorWidthNumber = Math.min(parseInt(editorWidth, undefined), innerWidth * 0.4);
+        ui.domElement.style.right = editorWidthNumber + 10 + "px";
+        canvas.parentElement!.appendChild(ui.domElement);
+
+        window.addEventListener("resize", () => {
+            const width =
+                innerWidth <= 450
+                    ? 0
+                    : Math.min(parseInt(editorWidth, undefined), innerWidth * 0.4);
+            canvas.className = "full";
+            ui.domElement.style.right = width + 10 + "px";
+            mapView.resize(innerWidth - width, innerHeight);
+        });
+
+        const hereCopyrightInfo: CopyrightInfo = {
+            id: "here.com",
+            year: new Date().getFullYear(),
+            label: "HERE",
+            link: "https://legal.here.com/terms"
+        };
+        const copyrights: CopyrightInfo[] = [hereCopyrightInfo];
+
+        const baseMap = new OmvDataSource({
+            baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
+            apiFormat: APIFormat.XYZOMV,
+            styleSetName: "tilezen",
+            maxZoomLevel: 17,
+            authenticationCode: accessToken,
+            copyrightInfo: copyrights
+        });
+        mapView.addDataSource(baseMap);
+
+        return mapView;
+    }
+
+    function getExampleHTML(): string {
+        // tslint:disable:max-line-length
+        return `
+            <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600&amp;display=swap" rel="stylesheet">
+            <style>
+                :root{
+                    --editor-width:${editorWidth};
+                }
+                #mapCanvas{
+                    top:0;
+                    width:calc(100% - var(--editor-width));
+                    min-width: 60%;
+                }
+                #drag-overlay{
+                    position:absolute;
+                    width:calc(100% - var(--editor-width));
+                    min-width: 60%;
+                    height: 100%;
+                    background: rgba(0,0,0,0.5);
+                    display:none
+                }
+                #drag-dashes{
+                    --border-thickness: 9px;
+                    --margin:70px;
+                    position:absolute;
+                    width:calc(100% - var(--margin) * 2 - var(--border-thickness) * 2);
+                    height:calc(100% - var(--margin) * 2 - var(--border-thickness) * 2);
+                    top:var(--margin);
+                    left:var(--margin);
+                    border: dashed var(--border-thickness) #fff;
+                    border-radius: 25px;
+                }
+                #editor{
+                    font-family: 'Fira Sans', sans-serif;
+                    width:var(--editor-width);
+                    max-width:40%;
+                    right:0;
+                    top:0;
+                    position:absolute;
+                    height:100%;
+                    padding:5px;
+                    background:hsla(215, 15%, 65%, 1);
+                    box-sizing: border-box;
+                }
+                button{
+                    font-family: 'Fira Sans', sans-serif;
+                    background: hsla(215, 15%, 17%, 1);
+                    height: 21px;
+                    border: 0;
+                    color: #d2d7de;
+                    text-transform: uppercase;
+                    cursor: pointer;
+                }
+                #browse{
+                    display:block;
+                    position:absolute;
+                    right: 5px;
+                    top: 5px;
+                }
+                #editor textarea{
+                    font-family: 'Fira Sans', sans-serif;
+                    position:absolute;
+                    resize: none;
+                    width:calc(100% - 10px);
+                    height:calc(100% - 36px);
+                    padding:0;
+                    top:31px;
+                    right:5px;
+                    border:0
+                }
+                #info{
+                    color: #fff;
+                    min-width: 60%;
+                    width: calc(100% - var(--editor-width));
+                    text-align: center;
+                    font-family: monospace;
+                    left: 50%;
+                    bottom: 10px;
+                    font-size: 15px;
+                    position: absolute;
+                    margin-left: -50%;
+                }
+                #copyrightNotice{
+                    left:0;
+                    background:#888;
+                    right:inherit;
+                }
+                @media all and (max-width: 450px) {
+                    #editor{
+                        display:none;
+                    }
+                    #mapCanvas, #info, #drag-overlay{
+                        width: 100%;
+                    }
+                }
+            </style>
+            <div id=editor>
+                <button>Update</button>
+                <textarea></textarea>
+            </div>
+
+            <input type="file" id="input" style="display: none;" />
+            <button id=browse onclick="document.getElementById('input').click();">Browse a file</button>
+
+            <div id="drag-overlay">
+                <div id="drag-dashes"></div>
+            </div>
+            <p id=info>Drag and drop a GeoJSON or browse a local one.</p>
+        `;
+    }
+}
--- @here/harp-features-datasource/lib/FeaturesDataSource.ts
+++ @here/harp-features-datasource/lib/FeaturesDataSource.ts
@@ -4,13 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Feature, FeatureCollection, FeatureGeometry } from "@here/harp-datasource-protocol";
+import {
+    Feature,
+    FeatureCollection,
+    FeatureGeometry,
+    GeometryCollection
+} from "@here/harp-datasource-protocol";
 import { GeoJsonDataProvider } from "@here/harp-geojson-datasource";
 import { OmvDataSource } from "@here/harp-omv-datasource";
 import { MapViewFeature } from "./Features";
 
 const NAME = "user-features-datasource";
-
 const DEFAULT_GEOJSON: FeatureCollection = {
     type: "FeatureCollection",
     features: []
@@ -20,10 +24,7 @@ const DEFAULT_GEOJSON: FeatureCollection = {
  * [[DataSource]] implementation to use for the addition of custom features.
  */
 export class FeaturesDataSource extends OmvDataSource {
-    private m_featureCollection: FeatureCollection = {
-        type: "FeatureCollection",
-        features: []
-    };
+    private m_featureCollection: FeatureCollection = this.emptyGeojson();
 
     /**
      * Builds a `FeaturesDataSource`.
@@ -36,6 +37,35 @@ export class FeaturesDataSource extends OmvDataSource {
             dataProvider: new GeoJsonDataProvider(NAME, DEFAULT_GEOJSON, { workerTilerUrl })
         });
         this.addTileBackground = false;
+    }
+
+    /**
+     * This method allows to directly add a GeoJSON without using [[MapViewFeature]] instances. It
+     * also overwrites existing features in this data source. To add a GeoJSON without overwriting
+     * the data source, one should loop through it to create [[MapViewFeature]] and add them with
+     * the `add` method.
+     *
+     * @param geojson A javascript object matching the GeoJSON specification.
+     */
+    setFromGeojson(geojson: FeatureCollection | GeometryCollection | Feature) {
+        if (geojson.type === "FeatureCollection") {
+            this.m_featureCollection = geojson;
+        } else if (geojson.type === "Feature") {
+            this.m_featureCollection = this.emptyGeojson();
+            this.m_featureCollection.features.push(geojson);
+        } else if (geojson.type === "GeometryCollection") {
+            this.m_featureCollection = this.emptyGeojson();
+            for (const geometry of geojson.geometries) {
+                this.m_featureCollection.features.push({
+                    type: "Feature",
+                    geometry
+                });
+            }
+        } else {
+            throw new TypeError("The provided object is not a valid GeoJSON object.");
+        }
+        this.update();
+        return this;
     }
 
     /**
@@ -68,10 +98,7 @@ export class FeaturesDataSource extends OmvDataSource {
      * Removes all the custom features in this `FeaturesDataSource`.
      */
     clear() {
-        this.m_featureCollection = {
-            type: "FeatureCollection",
-            features: []
-        };
+        this.m_featureCollection = this.emptyGeojson();
         this.update();
     }
 
@@ -116,5 +143,12 @@ export class FeaturesDataSource extends OmvDataSource {
         (this.dataProvider() as GeoJsonDataProvider).updateInput(this.m_featureCollection);
         this.mapView.markTilesDirty(this);
         this.mapView.clearTileCache(this.name);
+    }
+
+    private emptyGeojson(): FeatureCollection {
+        return {
+            features: [],
+            type: "FeatureCollection"
+        };
     }
 }
