a231d1c7b251566554bfe82c739976e662ec61e2
Arkadiusz Spiewak <ext-arkadiusz.spiewak@here.com>
2019-09-10 16:55:44 +0200

    Test commit for triggering update reference images
    Signed-off-by: Arkadiusz Spiewak <ext-arkadiusz.spiewak@here.com>

--- @here/harp-geojson-datasource/lib/GeoJsonDataProvider.ts
+++ @here/harp-geojson-datasource/lib/GeoJsonDataProvider.ts
@@ -87,6 +87,7 @@ export class GeoJsonDataProvider implements DataProvider {
     }
 
     async getTile(tileKey: TileKey): Promise<{}> {
-        return this.m_tiler.getTile(this.name, tileKey);
+        return {};
+        // return this.m_tiler.getTile(this.name, tileKey);
     }
 }
