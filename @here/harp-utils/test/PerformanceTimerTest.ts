5d04b6e5937d9a711b8a571e2301771ead7f26c5
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2019-09-13 23:06:34 +0200

    Support for test execution on windows.
    * windows requires different workflow, as it's run in CMD, not in posix shell
    * `node-fetch` on windows, needs proper `file://` URL
    * increase `LicenseHeaderCheck` as `glob` is slower on windows
    
    Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

--- @here/harp-utils/test/PerformanceTimerTest.ts
+++ @here/harp-utils/test/PerformanceTimerTest.ts
@@ -7,19 +7,20 @@
 // tslint:disable:only-arrow-functions
 //    Mocha discourages using arrow functions, see https://mochajs.org/#arrow-functions
 
+import { willEventually } from "@here/harp-test-utils";
 import { assert } from "chai";
 import { PerformanceTimer } from "../lib/PerformanceTimer";
 
 describe("PerformanceTimer", function() {
-    it("#now", function() {
+    it("#now", async function() {
         const t0 = PerformanceTimer.now();
         assert.isNumber(t0);
         assert.isAbove(t0, 0);
 
-        setTimeout(() => {
+        await willEventually(() => {
             const t1 = PerformanceTimer.now();
             assert.isNumber(t1);
             assert.isAbove(t1, t0);
-        }, 2);
+        });
     });
 });
