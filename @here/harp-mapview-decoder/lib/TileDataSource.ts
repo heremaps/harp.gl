heremaps/harp.gl

commit 291efd0565eb61ae2052cd315391564971e17e24

HARP-7103: Resolve themes 'ref' expressions in decoder.
* DataSource.setTheme revived as main theming interface for
  datasources because decoder based datasources require
  `theme.definitions` too
* Definitions are communicated from MapView to decoder
* StyleSetEvaluator now resolves reference using Expr.fromJSON
* ThemeLoader doesn't resolve references by default
* (minor) BackgroundDataSource reacts to theme change

Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

@here/harp-mapview-decoder/lib/TileDataSource.ts
@@ -4,7 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ITileDecoder, StyleSet, TileInfo } from "@here/harp-datasource-protocol";
+import {
+    Definitions,
+    ITileDecoder,
+    StyleSet,
+    Theme,
+    TileInfo
+} from "@here/harp-datasource-protocol";
 import { TileKey, TilingScheme } from "@here/harp-geoutils";
 import {
     ConcurrentDecoderFacade,
@@ -183,15 +189,22 @@ export class TileDataSource<TileType extends Tile> extends DataSource {
         await Promise.all([this.m_options.dataProvider.connect(), this.m_decoder.connect()]);
         this.m_isReady = true;
 
-        this.m_decoder.configure(undefined, undefined, {
+        this.m_decoder.configure(undefined, undefined, undefined, {
             storageLevelOffset: this.m_options.storageLevelOffset
         });
     }
 
-    setStyleSet(styleSet?: StyleSet, languages?: string[]): void {
-        this.m_tileLoaderCache.clear();
-        this.m_decoder.configure(styleSet, languages);
+    setStyleSet(styleSet?: StyleSet, definitions?: Definitions, languages?: string[]): void {
+        this.m_decoder.configure(styleSet, definitions, languages);
         this.mapView.markTilesDirty(this);
+    }
+
+    setTheme(theme: Theme, languages?: string[]): void {
+        const styleSet =
+            (this.styleSetName !== undefined && theme.styles && theme.styles[this.styleSetName]) ||
+            [];
+
+        this.setStyleSet(styleSet, theme.definitions, languages);
     }
 
     clearCache() {

@here/harp-mapview/lib/DataSource.ts
@@ -3,7 +3,7 @@
  * Licensed under Apache 2.0, see full license in LICENSE
  * SPDX-License-Identifier: Apache-2.0
  */
-import { StyleSet } from "@here/harp-datasource-protocol";
+import { Definitions, StyleSet, Theme } from "@here/harp-datasource-protocol";
 import { Projection, TileKey, TilingScheme } from "@here/harp-geoutils";
 import { assert } from "@here/harp-utils";
 import * as THREE from "three";
@@ -112,17 +112,13 @@ export abstract class DataSource extends THREE.EventDispatcher {
 
     /**
      * Sets the name of the [[StyleSet]] to use for the decoding. If this [[DataSource]] is already
-     * attached to a [[MapView]], this setter then looks for a [[StyleSet]] with this name and
-     * applies it.
+     * attached to a [[MapView]], this setter then reapplies [[StyleSet]] with this name found in
+     * [[MapView]]s theme.
      */
     set styleSetName(styleSetName: string | undefined) {
         this.m_styleSetName = styleSetName;
-        if (
-            this.m_mapView !== undefined &&
-            styleSetName !== undefined &&
-            this.m_mapView.theme.styles !== undefined
-        ) {
-            this.setStyleSet(this.m_mapView.theme.styles[styleSetName]);
+        if (this.m_mapView !== undefined && styleSetName !== undefined) {
+            this.setTheme(this.m_mapView.theme);
         }
     }
 
@@ -209,13 +205,26 @@ export abstract class DataSource extends THREE.EventDispatcher {
     /**
      * Invoked by [[MapView]] to notify when the [[Theme]] has been changed.
      *
-     * If `DataSource` depends on a theme, it must update its tiles' geometry.
+     * If `DataSource` depends on a `styleSet` or `languages`, it must update its tiles' geometry.
+     *
+     * @deprecated, Use [[setTheme]].
      *
      * @param styleSet The new theme that [[MapView]] uses.
      * @param languages An optional list of languages for the `DataSource`.
      */
     // tslint:disable-next-line:no-unused-variable
-    setStyleSet(styleSet?: StyleSet, languages?: string[]): void {
+    setStyleSet(styleSet?: StyleSet, definitions?: Definitions, languages?: string[]): void {
+        // to be overwritten by subclasses
+    }
+
+    /**
+     * Invoked by [[MapView]] to apply new [[Theme]].
+     *
+     * If `DataSource` depends on a `styleSet` or `languages`, it must update its tiles' geometry.
+     *
+     * @param languages
+     */
+    setTheme(theme: Theme, languages?: string[]): void {
         // to be overwritten by subclasses
     }
 

@here/harp-datasource-protocol/lib/Expr.ts
@@ -62,7 +62,7 @@ export abstract class Expr {
      *
      * If `definitions` are defined, then references (`['ref', name]`) are resolved.
      *
-     * Paass `definitionExprCache` to reuse `Expr` instances created from definitions across
+     * Pass `definitionExprCache` to reuse `Expr` instances created from definitions across
      * many `fromJSON` calls.
      *
      * @param node expression in JSON format to parse
@@ -83,10 +83,10 @@ export abstract class Expr {
                   }
                 : undefined;
 
-        return Expr.fromJSONInt(node, referenceResolverState);
+        return Expr.parseNode(node, referenceResolverState);
     }
 
-    private static fromJSONInt(
+    private static parseNode(
         node: unknown,
         referenceResolverState: ReferenceResolverState | undefined
     ): Expr {
@@ -130,7 +130,7 @@ export abstract class Expr {
 
             case "has":
                 if (node[2] !== undefined) {
-                    return Expr.makeCallExpr(op, node);
+                    return Expr.makeCallExpr(op, node, referenceResolverState);
                 }
                 if (typeof node[1] !== "string") {
                     throw new Error(`expected the name of an attribute`);
@@ -150,10 +150,7 @@ export abstract class Expr {
                         throw new Error("expected an array of constant values");
                     }
                 });
-                return new ContainsExpr(
-                    this.fromJSONInt(node[1], referenceResolverState),
-                    elements
-                );
+                return new ContainsExpr(this.parseNode(node[1], referenceResolverState), elements);
 
             case "literal":
                 if (typeof node[1] !== "object") {
@@ -168,7 +165,7 @@ export abstract class Expr {
                 if (!(node.length % 2)) {
                     throw new Error("fallback is missing in 'match' expression");
                 }
-                const value = this.fromJSONInt(node[1], referenceResolverState);
+                const value = this.parseNode(node[1], referenceResolverState);
                 const conditions: Array<[MatchLabel, Expr]> = [];
                 for (let i = 2; i < node.length - 1; i += 2) {
                     const label = node[i];
@@ -181,10 +178,10 @@ export abstract class Expr {
                     ) {
                         throw new Error(`parse error ${JSON.stringify(label)}`);
                     }
-                    const expr = this.fromJSONInt(node[i + 1], referenceResolverState);
+                    const expr = this.parseNode(node[i + 1], referenceResolverState);
                     conditions.push([label, expr]);
                 }
-                const fallback = this.fromJSONInt(node[node.length - 1], referenceResolverState);
+                const fallback = this.parseNode(node[node.length - 1], referenceResolverState);
                 return new MatchExpr(value, conditions, fallback);
             }
 
@@ -197,14 +194,11 @@ export abstract class Expr {
                 }
                 const branches: Array<[Expr, Expr]> = [];
                 for (let i = 1; i < node.length - 1; i += 2) {
-                    const condition = this.fromJSONInt(node[i], referenceResolverState);
-                    const expr = this.fromJSONInt(node[i + 1], referenceResolverState);
+                    const condition = this.parseNode(node[i], referenceResolverState);
+                    const expr = this.parseNode(node[i + 1], referenceResolverState);
                     branches.push([condition, expr]);
                 }
-                const caseFallback = this.fromJSONInt(
-                    node[node.length - 1],
-                    referenceResolverState
-                );
+                const caseFallback = this.parseNode(node[node.length - 1], referenceResolverState);
                 return new CaseExpr(branches, caseFallback);
             }
 
@@ -220,7 +214,7 @@ export abstract class Expr {
     ): Expr {
         return new CallExpr(
             op,
-            node.slice(1).map(childExpr => this.fromJSONInt(childExpr, referenceResolverState))
+            node.slice(1).map(childExpr => this.parseNode(childExpr, referenceResolverState))
         );
     }
 
@@ -250,13 +244,12 @@ export abstract class Expr {
             definitionEntry = definitionEntry.value;
         }
         let result: Expr;
-
         if (isValueDefinition(definitionEntry)) {
             result = Expr.fromJSON(definitionEntry.value);
         } else if (isJsonExpr(definitionEntry)) {
             referenceResolverState.lockedNames.add(name);
             try {
-                result = Expr.fromJSONInt(definitionEntry, referenceResolverState);
+                result = Expr.parseNode(definitionEntry, referenceResolverState);
             } finally {
                 referenceResolverState.lockedNames.delete(name);
             }
@@ -400,12 +393,17 @@ export class VarExpr extends Expr {
     }
 }
 
+export abstract class LiteralExpr extends Expr {
+    abstract get value(): Value;
+}
+
 /**
  * Null literal expression.
  * @hidden
  */
 export class NullLiteralExpr extends Expr {
     static instance = new NullLiteralExpr();
+    readonly value = null;
 
     protected constructor() {
         super();
@@ -420,7 +418,7 @@ export class NullLiteralExpr extends Expr {
  * Boolean literal expression.
  * @hidden
  */
-export class BooleanLiteralExpr extends Expr {
+export class BooleanLiteralExpr extends LiteralExpr {
     constructor(readonly value: boolean) {
         super();
     }
@@ -434,7 +432,7 @@ export class BooleanLiteralExpr extends Expr {
  * Number literal expression.
  * @hidden
  */
-export class NumberLiteralExpr extends Expr {
+export class NumberLiteralExpr extends LiteralExpr {
     constructor(readonly value: number) {
         super();
     }
@@ -448,7 +446,7 @@ export class NumberLiteralExpr extends Expr {
  * String literal expression.
  * @hidden
  */
-export class StringLiteralExpr extends Expr {
+export class StringLiteralExpr extends LiteralExpr {
     constructor(readonly value: string) {
         super();
     }
@@ -462,7 +460,7 @@ export class StringLiteralExpr extends Expr {
  * Object literal expression.
  * @hidden
  */
-export class ObjectLiteralExpr extends Expr {
+export class ObjectLiteralExpr extends LiteralExpr {
     constructor(readonly value: object) {
         super();
     }

@here/harp-datasource-protocol/lib/ITileDecoder.ts
@@ -6,7 +6,7 @@
 import { Projection, TileKey } from "@here/harp-geoutils";
 
 import { DecodedTile } from "./DecodedTile";
-import { StyleSet } from "./Theme";
+import { Definitions, StyleSet } from "./Theme";
 import { TileInfo } from "./TileInfo";
 import { OptionsMap, RequestController } from "./WorkerDecoderProtocol";
 
@@ -58,10 +58,16 @@ export interface ITileDecoder {
      * Non-existing (`undefined`) options (including styleSet) are not changed.
      *
      * @param styleSet optional, new style set.
+     * @param definitions optional, definitions used to resolve references in `styleSet`
      * @param languages optional, language list
      * @param options optional, new options - shape is specific for each decoder
      */
-    configure(styleSet?: StyleSet, languages?: string[], options?: OptionsMap): void;
+    configure(
+        styleSet?: StyleSet,
+        definitions?: Definitions,
+        languages?: string[],
+        options?: OptionsMap
+    ): void;
 
     /**
      * Free all resources associated with this decoder.

@here/harp-datasource-protocol/lib/StyleSetEvaluator.ts
@@ -17,6 +17,7 @@ import {
     HasAttributeExpr,
     isJsonExpr,
     JsonExpr,
+    LiteralExpr,
     MatchExpr,
     NullLiteralExpr,
     NumberLiteralExpr,
@@ -33,7 +34,16 @@ import {
 import { InterpolatedProperty } from "./InterpolatedPropertyDefs";
 import { AttrScope, mergeTechniqueDescriptor, TechniquePropNames } from "./TechniqueDescriptor";
 import { IndexedTechnique, Technique, techniqueDescriptors } from "./Techniques";
-import { LineStyle, Style, StyleDeclaration, StyleSelector, StyleSet } from "./Theme";
+import {
+    Definitions,
+    isActualSelectorDefinition,
+    isJsonExprReference,
+    LineStyle,
+    Style,
+    StyleDeclaration,
+    StyleSelector,
+    StyleSet
+} from "./Theme";
 
 const logger = LoggerManager.instance.create("StyleSetEvaluator");
 
@@ -245,26 +255,51 @@ export class StyleSetEvaluator {
     private readonly m_styleConditionClassifier = new StyleConditionClassifier();
     private m_layer: string | undefined;
     private m_geometryType: string | undefined;
+    private m_definitions?: Definitions;
+    private m_definitionExprCache?: Map<string, Expr>;
 
-    constructor(styleSet: StyleSet) {
+    constructor(styleSet: StyleSet, definitions?: Definitions) {
         let techniqueRenderOrder = 0;
         let styleSetIndex = 0;
+        this.m_definitions = definitions;
+        if (definitions !== undefined) {
+            this.m_definitionExprCache = new Map();
+        }
 
-        const cloneStyle = (style: StyleDeclaration): StyleDeclaration | undefined => {
+        const resolveStyleReferences = (style: StyleDeclaration): StyleDeclaration | undefined => {
             if (isJsonExpr(style)) {
-                return undefined;
+                if (!isJsonExprReference(style)) {
+                    throw new Error(
+                        "invalid expression in this context, only 'ref's are supported"
+                    );
+                }
+                // expand and instantiate references to style definitions.
+                const definitionName = style[1];
+                const def = definitions && definitions[definitionName];
+
+                if (!def) {
+                    throw new Error(`invalid reference '${definitionName}' - not found`);
+                }
+                if (!isActualSelectorDefinition(def)) {
+                    throw new Error(
+                        `invalid reference '${definitionName}' - expected style definition`
+                    );
+                }
+
+                // instantiate the style
+                return resolveStyleReferences(def);
             }
             return {
                 ...style,
                 styles:
                     style.styles !== undefined
                         ? (style.styles
-                              .map(subStyle => cloneStyle(subStyle))
+                              .map(subStyle => resolveStyleReferences(subStyle))
                               .filter(subStyle => subStyle !== undefined) as StyleSet)
                         : undefined
             };
         };
-        styleSet = styleSet.map(style => cloneStyle(style) as StyleDeclaration);
+        styleSet = styleSet.map(style => resolveStyleReferences(style) as StyleDeclaration);
         const computeDefaultRenderOrder = (style: InternalStyle): void => {
             if (style.renderOrderBiasGroup !== undefined) {
                 const renderOrderBiasGroupOrder = style.renderOrderBiasGroup
@@ -425,7 +460,7 @@ export class StyleSetEvaluator {
         if (style.when !== undefined) {
             try {
                 style._whenExpr = Array.isArray(style.when)
-                    ? Expr.fromJSON(style.when)
+                    ? Expr.fromJSON(style.when, this.m_definitions, this.m_definitionExprCache)
                     : Expr.parse(style.when);
 
                 // search for usages of '$layer' and any other
@@ -584,7 +619,17 @@ export class StyleSetEvaluator {
             ];
 
             if (isJsonExpr(attrValue)) {
-                const expr = Expr.fromJSON(attrValue).intern(this.m_exprPool);
+                const expr = Expr.fromJSON(
+                    attrValue,
+                    this.m_definitions,
+                    this.m_definitionExprCache
+                ).intern(this.m_exprPool);
+                if (expr instanceof LiteralExpr) {
+                    // Shortcut for literal expressions, so they are not taken into account when
+                    // trying to instantiate technique variants.
+                    targetStaticAttributes.push([attrName, expr.value]);
+                    return;
+                }
                 switch (attrScope) {
                     case AttrScope.FeatureGeometry:
                         dynamicFeatureAttributes.push([attrName, expr]);

@here/harp-datasource-protocol/lib/WorkerDecoderProtocol.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { StyleSet } from "./Theme";
+import { Definitions, StyleSet } from "./Theme";
 import { WorkerServiceProtocol } from "./WorkerServiceProtocol";
 
 /**
@@ -72,6 +72,7 @@ export namespace WorkerDecoderProtocol {
     export interface ConfigurationMessage extends DecoderMessage {
         type: DecoderMessageName.Configuration;
         styleSet?: StyleSet;
+        definitions?: Definitions;
         options?: OptionsMap;
         languages?: string[];
     }

@here/harp-datasource-protocol/lib/operators/MathOperators.ts
@@ -29,7 +29,6 @@ const operators = {
             const a = context.evaluate(args[0]);
             const b = context.evaluate(args[1]);
             if (typeof a !== "number" || typeof b !== "number") {
-                // tslint:disable-next-line: max-line-length
                 throw new Error(
                     `invalid operands '${typeof a}' and '${typeof b}' for operator '-'`
                 );

@here/harp-datasource-protocol/test/StyleSetEvaluatorTest.ts
@@ -10,8 +10,8 @@
 import { assert } from "chai";
 import { MapEnv } from "../lib/Expr";
 import { StyleSetEvaluator } from "../lib/StyleSetEvaluator";
-import { isSolidLineTechnique, SolidLineTechnique } from "../lib/Techniques";
-import { StyleSet } from "../lib/Theme";
+import { FillTechnique, isSolidLineTechnique, SolidLineTechnique } from "../lib/Techniques";
+import { Definitions, StyleDeclaration, StyleSet } from "../lib/Theme";
 
 describe("StyleSetEvaluator", function() {
     const basicStyleSetAutoOrder: StyleSet = [
@@ -164,6 +164,7 @@ describe("StyleSetEvaluator", function() {
             {
                 technique: "solid-line",
                 when: "kind == 'park'",
+                renderOrder: ["get", "area"],
                 attr: {
                     lineWidth: ["get", "area"],
                     color: "#00aa00",
@@ -189,11 +190,12 @@ describe("StyleSetEvaluator", function() {
             assert.isTrue(isSolidLineTechnique(r1[0]));
             assert.equal((r1[0] as SolidLineTechnique).clipping, false);
             assert.equal((r1[0] as SolidLineTechnique).lineWidth, 2);
-            assert.equal(ev.techniques.length, 2);
+            assert.equal(r1[0].renderOrder, 2);
 
             assert.isTrue(isSolidLineTechnique(r2[0]));
             assert.equal((r2[0] as SolidLineTechnique).clipping, true);
             assert.equal((r2[0] as SolidLineTechnique).lineWidth, 3);
+            assert.equal(r2[0].renderOrder, 3);
         });
 
         it("generates stable technique cache key", function() {
@@ -234,6 +236,59 @@ describe("StyleSetEvaluator", function() {
             assert.deepEqual(techniquesTileA[1], techniquesTileC[0]);
             assert.deepEqual(techniquesTileA[2], techniquesTileB[0]);
             assert.deepEqual(techniquesTileA[0], techniquesTileC[1]);
+        });
+    });
+    describe('definitions / "ref" operator support', function() {
+        const sampleStyleDeclaration: StyleDeclaration = {
+            technique: "fill",
+            when: ["ref", "expr"],
+            attr: { lineWidth: ["ref", "number"] }
+        };
+        const sampleDefinitions: Definitions = {
+            expr: { type: "selector", value: ["==", ["get", "kind"], "park"] },
+            number: { type: "number", value: 123 }
+        };
+        it("resolves references in style declaration attributes", function() {
+            const sse = new StyleSetEvaluator([sampleStyleDeclaration], sampleDefinitions);
+            const techniques = sse.getMatchingTechniques(new MapEnv({ kind: "park" }));
+
+            assert.equal(techniques.length, 1);
+            assert.deepNestedInclude(techniques[0], {
+                name: "fill",
+                lineWidth: 123,
+                renderOrder: 0
+            });
+        });
+        it("resolves style declaration references", function() {
+            const sse = new StyleSetEvaluator([["ref", "style"]], {
+                ...sampleDefinitions,
+                style: sampleStyleDeclaration
+            });
+            const techniques = sse.getMatchingTechniques(new MapEnv({ kind: "park" }));
+
+            assert.equal(techniques.length, 1);
+            assert.deepNestedInclude(techniques[0], {
+                name: "fill",
+                lineWidth: 123,
+                renderOrder: 0
+            });
+        });
+        it("reuses very same instances of objects from definition table", function() {
+            const styleReferencingObject: StyleDeclaration = {
+                technique: "fill",
+                when: ["ref", "expr"],
+                attr: { lineWidth: ["ref", "number"], color: ["ref", "bigObject"] }
+            };
+            const bigObject = {};
+            const definitions: Definitions = {
+                ...sampleDefinitions,
+                bigObject: ["literal", bigObject]
+            };
+            const sse = new StyleSetEvaluator([styleReferencingObject], definitions);
+
+            const techniques = sse.getMatchingTechniques(new MapEnv({ kind: "park" }));
+            assert.equal(techniques.length, 1);
+            assert.strictEqual((techniques[0] as FillTechnique).color as any, bigObject);
         });
     });
 

commit 5a4240ff6a24fc9fadfa4842bdee332a1965e583

Fix theme handling in TileDataSource.
TileDataSource shouldn't update styleset in `setTheme` if theme doesn't
define styleset with `styleSetName` of datasource.

This fixes backwards compatibility bug introduced in #796.

Among others fixes `datasource_features_polygons` demo.

Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

@here/harp-mapview-decoder/lib/TileDataSource.ts
@@ -199,12 +199,21 @@ export class TileDataSource<TileType extends Tile> extends DataSource {
         this.mapView.markTilesDirty(this);
     }
 
+    /**
+     * Apply the [[Theme]] to this data source.
+     *
+     * Applies new [[StyleSet]] and definitions from theme only if matching styleset (see
+     * `styleSetName` property) is found in `theme`.
+     */
     setTheme(theme: Theme, languages?: string[]): void {
         const styleSet =
-            (this.styleSetName !== undefined && theme.styles && theme.styles[this.styleSetName]) ||
-            [];
+            this.styleSetName !== undefined && theme.styles
+                ? theme.styles[this.styleSetName]
+                : undefined;
 
-        this.setStyleSet(styleSet, theme.definitions, languages);
+        if (styleSet !== undefined) {
+            this.setStyleSet(styleSet, theme.definitions, languages);
+        }
     }
 
     clearCache() {

@here/harp-mapview/lib/DataSource.ts
@@ -218,9 +218,10 @@ export abstract class DataSource extends THREE.EventDispatcher {
     }
 
     /**
-     * Invoked by [[MapView]] to apply new [[Theme]].
+     * Apply the [[Theme]] to this data source.
      *
-     * If `DataSource` depends on a `styleSet` or `languages`, it must update its tiles' geometry.
+     * If `DataSource` depends on a `styleSet` defined by this theme or `languages`, it must update
+     * its tiles' geometry.
      *
      * @param languages
      */