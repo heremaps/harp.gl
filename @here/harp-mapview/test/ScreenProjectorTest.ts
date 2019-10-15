935a6bb17e12043d5c0edbf4be2f68c58ae14369
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2019-10-15 10:52:31 +0200

    HARP-7419 Fix missing header
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- @here/harp-mapview/test/ScreenProjectorTest.ts
+++ @here/harp-mapview/test/ScreenProjectorTest.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ScreenProjector } from "../lib/ScreenProjector";
 
 import { assert } from "chai";
