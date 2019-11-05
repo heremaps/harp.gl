a0e989e67fdc0ed3461aaa9369335fc481b981f2
Nino Kettlitz <1396039+ninok@users.noreply.github.com>
2019-11-05 09:47:26 +0100

    Updated and fixed yeoman generator

--- @here/generator-harp.gl/generators/app/templates/index.ts
+++ @here/generator-harp.gl/generators/app/templates/index.ts
@@ -22,7 +22,7 @@ window.addEventListener("resize", () => {
 });
 
 // center the camera to New York
-mapView.lookAt(new GeoCoordinates(40.70398928, -74.01319808), 1500, 40, 190);
+mapView.lookAt(new GeoCoordinates(40.70398928, -74.01319808), 1500, 40, 0);
 
 // make sure the map is rendered
 mapView.update();
