heremaps/harp.gl

commit 9383825cada068b6639556674d58ced1af92c865

HARP-7161: Support for UrlResolvers in MapView.
* Adds `uriResolver` option to `MapView` that provides means to
  use resolve application specific URIs in theme.

* `MapView` and `ThemeLoader` resolves all provided `URI`s using
  provided `UriResolver`

* URL->URI in places when we support `URI`s in API

* Added, basic, prefix-map based resolver - `PrefixMapUriResolver`.

Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

@here/harp.gl/lib/BundleMain.ts
@@ -6,12 +6,12 @@
 
 import { ConcurrentDecoderFacade, ConcurrentTilerFacade } from "@here/harp-mapview";
 import { WorkerLoader } from "@here/harp-mapview/lib/workers/WorkerLoader";
-import { baseUrl, setDefaultUrlResolver } from "@here/harp-utils";
+import { baseUrl, UriResolver } from "@here/harp-utils";
 
 /**
  * Default decoder url for bundled map component.
  */
-export const DEFAULT_DECODER_SCRIPT_URL = "harp.gl:harp-decoders.js";
+export const DEFAULT_DECODER_SCRIPT_URL = "harp.js-bundle://harp-decoders.js";
 
 /**
  * Basename of map bundle script - used by [[getBundleScriptUrl]] as fallback, when
@@ -77,7 +77,7 @@ export function getScriptUrl(name: string): string | undefined | null {
     }
 }
 
-const HARP_GL_BUNDLED_ASSETS_PREFIX = "harp.gl:";
+const HARP_GL_BUNDLED_ASSETS_PREFIX = "harp.js-bundle://";
 
 /**
  * Resolve URLs with support for `harp.gl` bundle specific URLs.
@@ -86,23 +86,26 @@ const HARP_GL_BUNDLED_ASSETS_PREFIX = "harp.gl:";
  *
  * @hidden
  */
-export function resolveBundledResourceUrl(url: string) {
-    if (url.startsWith(HARP_GL_BUNDLED_ASSETS_PREFIX)) {
-        const bundleSriptUrl = getBundleScriptUrl();
-        if (bundleSriptUrl === null || bundleSriptUrl === undefined) {
-            throw new Error(
-                `harp.js: cannot resolve ${url} because 'harp.gl' base url is not set.`
-            );
-        } else {
-            url = url.substring(HARP_GL_BUNDLED_ASSETS_PREFIX.length);
-            if (url.startsWith("/")) {
-                url = url.substring(1);
+export class BundledUriResolver implements UriResolver {
+    resolveUri(uri: string) {
+        if (uri.startsWith(HARP_GL_BUNDLED_ASSETS_PREFIX)) {
+            const bundleSriptUrl = getBundleScriptUrl();
+            if (bundleSriptUrl === null || bundleSriptUrl === undefined) {
+                throw new Error(
+                    `harp.js: cannot resolve ${uri} because 'harp.gl' base url is not set.`
+                );
+            } else {
+                uri = uri.substring(HARP_GL_BUNDLED_ASSETS_PREFIX.length);
+                if (uri.startsWith("/")) {
+                    uri = uri.substring(1);
+                }
+                return baseUrl(bundleSriptUrl) + uri;
             }
-            return baseUrl(bundleSriptUrl) + url;
         }
+        return uri;
     }
-    return url;
 }
+const bundledUriResolver = new BundledUriResolver();
 
 const getActualDecoderScriptUrl = () => {
     const baseScriptUrl = getBundleScriptUrl();
@@ -124,7 +127,7 @@ const getActualDecoderScriptUrl = () => {
     const decoderScriptName = !isMinified
         ? DEFAULT_DECODER_SCRIPT_URL
         : DEFAULT_DECODER_SCRIPT_URL.replace(".js$", ".min.js");
-    return resolveBundledResourceUrl(decoderScriptName);
+    return bundledUriResolver.resolveUri(decoderScriptName);
 };
 
 /**
@@ -174,9 +177,5 @@ function installDefaultDecoderUrlHook() {
  * @hidden
  */
 export function mapBundleMain() {
-    const mapBundleUrl = getBundleScriptUrl();
-    if (mapBundleUrl !== null && mapBundleUrl !== undefined) {
-        setDefaultUrlResolver(resolveBundledResourceUrl);
-    }
     installDefaultDecoderUrlHook();
 }

@here/harp-mapview/lib/MapView.ts
@@ -24,7 +24,8 @@ import {
     getOptionValue,
     LoggerManager,
     LogLevel,
-    PerformanceTimer
+    PerformanceTimer,
+    UriResolver
 } from "@here/harp-utils";
 import * as THREE from "three";
 
@@ -298,7 +299,7 @@ export interface MapViewOptions {
      * The URL of the script that the decoder worker runs. The default URL is
      * `./decoder.bundle.js`.
      *
-     * Relative URLs are resolved to full URL using the document's base URL
+     * Relative URIs are resolved to full URL using the document's base URL
      * (see: https://www.w3.org/TR/WD-html40-970917/htmlweb.html#h-5.1.2).
      */
     decoderUrl?: string;
@@ -313,7 +314,7 @@ export interface MapViewOptions {
      * The [[Theme]] used by Mapview.
      *
      * This Theme can be one of the following:
-     *  - `string` : the URL of the theme file used to style this map
+     *  - `string` : the URI of the theme file used to style this map
      *  - `Theme` : the `Theme` object already loaded
      *  - `Promise<Theme>` : the future `Theme` object
      *  - `undefined` : the theme is not yet set up, but can be set later. Rendering waits until
@@ -321,10 +322,38 @@ export interface MapViewOptions {
      *
      * **Note:** Layers that use a theme do not render any content until that theme is available.
      *
-     * Relative URLs are resolved to full URL using the document's base URL
+     * Relative URIs are resolved to full URL using the document's base URL
      * (see: https://www.w3.org/TR/WD-html40-970917/htmlweb.html#h-5.1.2).
+     *
+     * Custom URIs (of theme itself and of resources referenced by theme) may be resolved with help
+     * of [[uriResolver]].
+     *
+     * @see [[ThemeLoader.load]] for details how theme is loaded
      */
     theme?: string | Theme | Promise<Theme>;
+
+    /**
+     * Resolve `URI` referenced in `MapView` assets using this resolver.
+     *
+     * Use, to support application/deployment specific `URI`s into actual `URLs` that can be loaded
+     * with `fetch`.
+     *
+     * Example:
+     * ```
+     * uriResolver: new PrefixMapUriResolver({
+     *     "local://poiMasterList": "/assets/poiMasterList.json",
+     *        // will match only 'local//:poiMasterList' and
+     *        // resolve to `/assets/poiMasterList.json`
+     *     "local://icons/": "/assets/icons/"
+     *        // will match only 'local//:icons/ANYPATH' (and similar) and
+     *        // resolve to `/assets/icons/ANYPATH`
+     * })
+     * ```
+     *
+     * @see [[UriResolver]]
+     * @See [[PrefixMapUriResolver]]
+     */
+    uriResolver?: UriResolver;
 
     /**
      * The minimum zoom level; default is `1`.
@@ -750,6 +779,7 @@ export class MapView extends THREE.EventDispatcher {
     private readonly m_visibleTileSetOptions: VisibleTileSetOptions;
 
     private m_theme: Theme = {};
+    private m_uriResolver?: UriResolver;
     private m_themeIsLoading: boolean = false;
 
     private m_previousFrameTimeStamp?: number;
@@ -794,6 +824,8 @@ export class MapView extends THREE.EventDispatcher {
         // make a copy to avoid unwanted changes to the original options.
         this.m_options = { ...options };
 
+        this.m_uriResolver = this.m_options.uriResolver;
+
         if (this.m_options.minZoomLevel !== undefined) {
             this.m_minZoomLevel = this.m_options.minZoomLevel;
         }
@@ -811,7 +843,9 @@ export class MapView extends THREE.EventDispatcher {
         }
 
         if (this.m_options.decoderUrl !== undefined) {
-            ConcurrentDecoderFacade.defaultScriptUrl = this.m_options.decoderUrl;
+            ConcurrentDecoderFacade.defaultScriptUrl = this.m_uriResolver
+                ? this.m_uriResolver.resolveUri(this.m_options.decoderUrl)
+                : this.m_options.decoderUrl;
         }
 
         if (this.m_options.decoderCount !== undefined) {
@@ -1214,7 +1248,7 @@ export class MapView extends THREE.EventDispatcher {
         if (!ThemeLoader.isThemeLoaded(theme)) {
             this.m_themeIsLoading = true;
             // If theme is not yet loaded, let's set theme asynchronously
-            ThemeLoader.load(theme)
+            ThemeLoader.load(theme, { uriResolver: this.m_uriResolver })
                 .then(loadedTheme => {
                     this.m_themeIsLoading = false;
                     this.theme = loadedTheme;
@@ -1271,6 +1305,14 @@ export class MapView extends THREE.EventDispatcher {
         }
         this.dispatchEvent(THEME_LOADED_EVENT);
         this.update();
+    }
+
+    /**
+     * [[UriResolver]] used to resolve application/deployment specific `URI`s into actual `URLs`
+     * that can be loaded with `fetch`.
+     */
+    get uriResolver(): UriResolver | undefined {
+        return this.m_uriResolver;
     }
 
     /**
@@ -2934,7 +2976,7 @@ export class MapView extends THREE.EventDispatcher {
 
         this.m_themeIsLoading = true;
         Promise.resolve<string | Theme>(this.m_options.theme)
-            .then(theme => ThemeLoader.load(theme))
+            .then(theme => ThemeLoader.load(theme, { uriResolver: this.m_uriResolver }))
             .then(theme => {
                 this.m_themeIsLoading = false;
                 this.theme = theme;

@here/harp-mapview/lib/ThemeLoader.ts
@@ -17,14 +17,15 @@ import {
 } from "@here/harp-datasource-protocol/lib/Theme";
 import {
     cloneDeep,
-    composeUrlResolvers,
+    composeUriResolvers,
     ContextLogger,
-    defaultUrlResolver,
     getAppBaseUrl,
     getOptionValue,
     IContextLogger,
     ISimpleChannel,
-    resolveReferenceUrl
+    RelativeUriResolver,
+    resolveReferenceUri,
+    UriResolver
 } from "@here/harp-utils";
 import { SKY_CUBEMAP_FACE_COUNT, SkyCubemapFaceId } from "./SkyCubemapTexture";
 
@@ -69,6 +70,11 @@ export interface ThemeLoadOptions {
      * If not specified, [[ThemeLoader.load]] will log to `console`.
      */
     logger?: ISimpleChannel;
+
+    /**
+     * Resolve asset `URI`s referenced in `Theme` assets using this resolver.
+     */
+    uriResolver?: UriResolver;
 }
 
 /**
@@ -85,8 +91,11 @@ export class ThemeLoader {
      *  -  `ref` - resolves all `ref` instances to their values defined in `definitions` section
      *     of theme (see [[resolveThemeReferences]])
      *
-     * Relative URLs of reference resources are resolved to full URL using the document's base URL
+     * Relative URIs of reference resources are resolved to full URL using the document's base URL
      * (see [[resolveUrls]]).
+     *
+     * Custom URIs (of theme itself and of resources referenced by theme) may be resolved with by
+     * providing [[UriResolver]] using [[ThemeLoadOptions.uriResolver]] option.
      *
      * @param theme [[Theme]] instance or theme URL to the theme.
      * @param options Optional, a [[ThemeLoadOptions]] objects containing any custom settings for
@@ -95,19 +104,20 @@ export class ThemeLoader {
     static async load(theme: string | Theme, options?: ThemeLoadOptions): Promise<Theme> {
         options = options || {};
         if (typeof theme === "string") {
-            const themeUrl = defaultUrlResolver(theme);
+            const uriResolver = options.uriResolver;
+            const themeUrl = uriResolver !== undefined ? uriResolver.resolveUri(theme) : theme;
 
             const response = await fetch(themeUrl, { signal: options.signal });
             if (!response.ok) {
                 throw new Error(`ThemeLoader#load: cannot load theme: ${response.statusText}`);
             }
             theme = (await response.json()) as Theme;
-            theme.url = resolveReferenceUrl(getAppBaseUrl(), themeUrl);
-            theme = this.resolveUrls(theme);
+            theme.url = resolveReferenceUri(getAppBaseUrl(), themeUrl);
+            theme = this.resolveUrls(theme, uriResolver);
         } else if (theme.url === undefined) {
             // assume that theme url is same as baseUrl
             theme.url = getAppBaseUrl();
-            theme = this.resolveUrls(theme);
+            theme = this.resolveUrls(theme, options.uriResolver);
         }
 
         if (theme === null || theme === undefined) {
@@ -158,25 +168,25 @@ export class ThemeLoader {
      *
      * @param theme The [[Theme]] to resolve.
      */
-    static resolveUrls(theme: Theme): Theme {
-        // Ensure that all resources referenced in theme by relative URLs are in fact relative to
+    static resolveUrls(theme: Theme, uriResolver?: UriResolver): Theme {
+        // Ensure that all resources referenced in theme by relative URIs are in fact relative to
         // theme.
         if (theme.url === undefined) {
             return theme;
         }
 
-        const childUrlResolver = composeUrlResolvers(
-            (childUrl: string) => resolveReferenceUrl(theme.url, childUrl),
-            defaultUrlResolver
+        const childUrlResolver = composeUriResolvers(
+            new RelativeUriResolver(theme.url),
+            uriResolver
         );
 
         if (theme.extends) {
             if (typeof theme.extends === "string") {
-                theme.extends = childUrlResolver(theme.extends);
+                theme.extends = childUrlResolver.resolveUri(theme.extends);
             } else {
                 if (theme.extends.url === undefined) {
                     theme.extends.url = theme.url;
-                    theme.extends = this.resolveUrls(theme.extends);
+                    theme.extends = this.resolveUrls(theme.extends, uriResolver);
                 }
             }
         }
@@ -185,28 +195,28 @@ export class ThemeLoader {
             for (let i = 0; i < SKY_CUBEMAP_FACE_COUNT; ++i) {
                 const faceUrl: string | undefined = (theme.sky as any)[SkyCubemapFaceId[i]];
                 if (faceUrl !== undefined) {
-                    (theme.sky as any)[SkyCubemapFaceId[i]] = childUrlResolver(faceUrl);
+                    (theme.sky as any)[SkyCubemapFaceId[i]] = childUrlResolver.resolveUri(faceUrl);
                 }
             }
         }
         if (theme.images) {
             for (const name of Object.keys(theme.images)) {
                 const image = theme.images[name];
-                image.url = childUrlResolver(image.url);
+                image.url = childUrlResolver.resolveUri(image.url);
 
                 if (image.atlas !== undefined) {
-                    image.atlas = childUrlResolver(image.atlas);
+                    image.atlas = childUrlResolver.resolveUri(image.atlas);
                 }
             }
         }
         if (theme.fontCatalogs) {
             for (const font of theme.fontCatalogs) {
-                font.url = childUrlResolver(font.url);
+                font.url = childUrlResolver.resolveUri(font.url);
             }
         }
         if (theme.poiTables) {
             for (const poiTable of theme.poiTables) {
-                poiTable.url = childUrlResolver(poiTable.url);
+                poiTable.url = childUrlResolver.resolveUri(poiTable.url);
             }
         }
 
@@ -224,9 +234,9 @@ export class ThemeLoader {
                         texturePropertyName => {
                             const textureProperty = (style.attr! as any)[texturePropertyName];
                             if (textureProperty && typeof textureProperty === "string") {
-                                (style.attr! as any)[texturePropertyName] = childUrlResolver(
-                                    textureProperty
-                                );
+                                (style.attr! as any)[
+                                    texturePropertyName
+                                ] = childUrlResolver.resolveUri(textureProperty);
                             }
                         }
                     );

@here/harp-mapview/lib/poi/PoiTableManager.ts
@@ -10,12 +10,7 @@ import {
     PoiTableRef,
     Theme
 } from "@here/harp-datasource-protocol";
-import {
-    composeUrlResolvers,
-    defaultUrlResolver,
-    LoggerManager,
-    resolveReferenceUrl
-} from "@here/harp-utils";
+import { LoggerManager } from "@here/harp-utils";
 
 import { MapView } from "../MapView";
 
@@ -327,17 +322,6 @@ export class PoiTableManager {
                 // Gather promises to signal the success of having loaded them all
                 const loadPromises: Array<Promise<boolean>> = new Array();
 
-                // Ensure that all resources referenced in theme by relative URLs are in fact
-                // relative to theme.
-                const themeUrl = theme.url;
-                const childUrlResolver =
-                    themeUrl === undefined
-                        ? undefined
-                        : composeUrlResolvers(
-                              (childUrl: string) => resolveReferenceUrl(themeUrl, childUrl),
-                              defaultUrlResolver
-                          );
-
                 theme.poiTables.forEach((poiTableRef: PoiTableRef) => {
                     if (
                         poiTableRef !== undefined &&
@@ -350,11 +334,7 @@ export class PoiTableManager {
                         );
                         if (poiTableRef.url !== undefined && typeof poiTableRef.url === "string") {
                             this.addTable(poiTable);
-                            const tableUrl =
-                                childUrlResolver === undefined
-                                    ? poiTableRef.url
-                                    : childUrlResolver(poiTableRef.url);
-                            loadPromises.push(poiTable.load(tableUrl));
+                            loadPromises.push(poiTable.load(poiTableRef.url));
                         } else {
                             logger.error(`POI table definition has no valid url: ${poiTableRef}`);
                         }

@here/harp-mapview/test/ThemeLoaderTest.ts
@@ -24,7 +24,13 @@ import {
     Theme
 } from "@here/harp-datasource-protocol";
 import { getTestResourceUrl } from "@here/harp-test-utils";
-import { cloneDeep, ContextLogger, getAppBaseUrl, resolveReferenceUrl } from "@here/harp-utils";
+import {
+    cloneDeep,
+    ContextLogger,
+    getAppBaseUrl,
+    resolveReferenceUri,
+    UriResolver
+} from "@here/harp-utils";
 import { ThemeLoader } from "../lib/ThemeLoader";
 
 function makeUrlRelative(baseUrl: string, url: string) {
@@ -58,7 +64,7 @@ describe("ThemeLoader", function() {
             const relativeToAppUrl = makeUrlRelative(appBaseUrl, sampleThemeUrl);
 
             const result = await ThemeLoader.load(relativeToAppUrl);
-            assert.equal(result.url, resolveReferenceUrl(appBaseUrl, relativeToAppUrl));
+            assert.equal(result.url, resolveReferenceUri(appBaseUrl, relativeToAppUrl));
         });
 
         it("resolves absolute url of theme loaded from relative url", async function() {
@@ -66,16 +72,16 @@ describe("ThemeLoader", function() {
             const relativeToAppUrl = makeUrlRelative(appBaseUrl, sampleThemeUrl);
 
             const result = await ThemeLoader.load(relativeToAppUrl);
-            assert.equal(result.url, resolveReferenceUrl(appBaseUrl, relativeToAppUrl));
+            assert.equal(result.url, resolveReferenceUri(appBaseUrl, relativeToAppUrl));
         });
 
         it("resolves urls of resources embedded in theme", async function() {
-            const absoluteSampleThemeUrl = resolveReferenceUrl(appBaseUrl, sampleThemeUrl);
+            const absoluteSampleThemeUrl = resolveReferenceUri(appBaseUrl, sampleThemeUrl);
 
             const result = await ThemeLoader.load(absoluteSampleThemeUrl);
             assert.equal(result.url, absoluteSampleThemeUrl);
 
-            const expectedFontCatalogUrl = resolveReferenceUrl(
+            const expectedFontCatalogUrl = resolveReferenceUri(
                 absoluteSampleThemeUrl,
                 "fonts/Default_FontCatalog.json"
             );
@@ -92,6 +98,42 @@ describe("ThemeLoader", function() {
             const secondLoadResult = await ThemeLoader.load(firstLoadResult);
 
             assert.deepEqual(firstResultCopy, secondLoadResult);
+        });
+
+        it("obeys `uriResolver` option", async function() {
+            // new PrefixMapUriResolver({
+            //     "fonts://fira": "ACTUAL_FONT_LOCATION",
+            //     "icons://": "ACTUAL_ICONS_LOCATION"
+            // });
+            const uriResolver: UriResolver = {
+                resolveUri(uri) {
+                    return "resolved!" + uri;
+                }
+            };
+            const r = await ThemeLoader.load(
+                {
+                    fontCatalogs: [
+                        {
+                            url: "fonts://fira",
+                            name: "fira"
+                        }
+                    ],
+                    images: {
+                        icons_day_maki: {
+                            url: "icons://maki_icons.png",
+                            preload: true,
+                            atlas: "icons://icons/maki_icons.json"
+                        }
+                    }
+                },
+                {
+                    uriResolver
+                }
+            );
+
+            assert.equal(r.fontCatalogs![0].url, "resolved!fonts://fira");
+            assert.equal(r.images!.icons_day_maki.url, "resolved!icons://maki_icons.png");
+            assert.equal(r.images!.icons_day_maki.atlas, "resolved!icons://icons/maki_icons.json");
         });
     });
     describe("#resolveStyleSet", function() {

@here/harp-utils/index.ts
@@ -15,7 +15,7 @@ export * from "./lib/ContextLogger";
 export * from "./lib/PerformanceTimer";
 export * from "./lib/ObjectUtils";
 export * from "./lib/OptionsUtils";
-export * from "./lib/UrlResolver";
+export * from "./lib/UriResolver";
 export * from "./lib/UrlUtils";
 export * from "./lib/UrlPlatformUtils";
 export * from "./lib/Functions";

@here/harp-utils/lib/UriResolver.ts
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { resolveReferenceUri } from "./UrlUtils";
+
+/**
+ * Custom, app-specific URI resolver interface.
+ */
+export interface UriResolver {
+    /**
+     * Attempt to resolve `URI` to `URL`.
+     *
+     * If given resolver doesn't know about this specific kind of `URI`, it should return string as
+     * received.
+     *
+     * @param input `URI`
+     * @returns actual `URL` if this handler knows how locate given `uri` or original `uri`
+     */
+    resolveUri(uri: string): string;
+}
+
+export interface PrefixUriResolverDefinition {
+    [prefix: string]: string;
+}
+
+/**
+ * Basic, import-map like [[UriResolver]].
+ *
+ * Resolves `uris` basing on exact or prefix match of `key` from `definitions`.
+ *
+ * In definitions, `key` is matched against input uri with following strategy:
+ *  - `key` without trailing `/` -> `key` and input `uri` must be identical
+ *  - `key` with trailing `/`, -> `key` is treated as "package prefix", so `uri` must start with
+ *    `key`
+ *
+ * Example:
+ * ```
+ * {
+ *     "local://poiMasterList": "/assets/poiMasterList.json"
+ *        // will match only 'local://poiMasterList' and resolve `/assets/poiMasterList.json`
+ *     "local://icons/": "/assets/icons/"
+ *        // will match only 'local://icons/ANYPATH' (and similar) and resolve to
+ *        // `/assets/icons/ANYPATH
+ * }
+ * ```
+ * Inspired by [`WICG` import maps proposal](https://github.com/WICG/import-maps#the-import-map).
+ */
+export class PrefixMapUriResolver implements UriResolver {
+    constructor(readonly definitions: PrefixUriResolverDefinition) {}
+
+    resolveUri(uri: string) {
+        return Object.keys(this.definitions).reduce((r, key) => {
+            if (key.endsWith("/") && r.startsWith(key)) {
+                const newPrefix = this.definitions[key];
+                return newPrefix + r.substr(key.length);
+            } else if (r === key) {
+                return this.definitions[key];
+            }
+            return r;
+        }, uri);
+    }
+}
+
+/**
+ * [UriResolver] that resolve relative `uri`s against to parent resource `uri`.
+ */
+export class RelativeUriResolver implements UriResolver {
+    constructor(readonly parentUri: string) {}
+
+    resolveUri(uri: string) {
+        return resolveReferenceUri(this.parentUri, uri);
+    }
+}
+
+/**
+ * Compose URI resolvers.
+ *
+ * Creates new [[UriResolver]] that applies resolvers in orders or arguments.
+ *
+ * Example:
+ *
+ *     const themeUrl = ...; // url of parent object
+ *     const childUrlResolver = composeUrlResolvers(
+ *           new RelativeUriResolver(themeUrl),
+ *           defaultUrlResolver
+ *     );
+ */
+export function composeUriResolvers(...resolvers: Array<UriResolver | undefined>): UriResolver {
+    return {
+        resolveUri(originalUrl: string) {
+            return resolvers.reduce((url, resolver) => {
+                if (resolver !== undefined) {
+                    return resolver.resolveUri(url);
+                } else {
+                    return url;
+                }
+            }, originalUrl);
+        }
+    };
+}

@here/harp-utils/lib/UrlResolver.ts
@@ -1,62 +1,0 @@
-/*
- * Copyright (C) 2017-2019 HERE Europe B.V.
- * Licensed under Apache 2.0, see full license in LICENSE
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * Convenience type definition for URL resolvers.
- */
-export type UrlResolver = (url: string) => string;
-
-/**
- * Resolves URL using default URL resolver.
- *
- * By default URL resolver is just identity function, it can be changed using
- * [[setDefaultUrlResolver].
- */
-export function defaultUrlResolver(url: string): string {
-    if (customDefaultUrlResolver !== undefined) {
-        return customDefaultUrlResolver(url);
-    } else {
-        return url;
-    }
-}
-
-/**
- * Change resolver used by [[defaultUrlResolver]].
- *
- * `undefined` resets default resolver to identity function.
- *
- * @param resolver
- */
-export function setDefaultUrlResolver(resolver: UrlResolver | undefined) {
-    customDefaultUrlResolver = resolver;
-}
-
-let customDefaultUrlResolver: UrlResolver | undefined;
-
-/**
- * Compose URL resolvers.
- *
- * Creates new `UrlResolver` that applies resolvers in orders or arguments.
- *
- * Example:
- *
- *     const themeUrl = ...; // url of parent object
- *     const childUrlResolver = composeUrlResolvers(
- *           (childUrl: string) => resolveReferenceUrl(themeUrl, childUrl),
- *           defaultUrlResolver
- *     );
- */
-export function composeUrlResolvers(...resolvers: UrlResolver[]): UrlResolver {
-    return (originalUrl: string) => {
-        return resolvers.reduce((url, resolver) => {
-            if (resolver !== undefined) {
-                return resolver(url);
-            } else {
-                return url;
-            }
-        }, originalUrl);
-    };
-}

commit d96577c7b2b5f9db0bbcc88b40a4d5a2f6e2e80b

Fix resolving of decoder script in harp.js
Ensure that `getBundleScriptUrl` is called during main script load and
that it loaded `harp(.min).js`.

Fixes regression introduced in #901.

Related-to: HARP-7933

Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

@here/harp.gl/lib/BundleMain.ts
@@ -37,7 +37,13 @@ export function getBundleScriptUrl(): string | undefined | null {
     }
 
     const currentScript = document.currentScript as HTMLScriptElement | null;
-    const baseScriptUrl = currentScript ? currentScript.src : getScriptUrl(BUNDLE_SCRIPT_BASENAME);
+    const baseScriptUrl =
+        currentScript !== null &&
+        typeof currentScript.src === "string" &&
+        (currentScript.src.endsWith(BUNDLE_SCRIPT_BASENAME + ".js") ||
+            currentScript.src.endsWith(BUNDLE_SCRIPT_BASENAME + ".min.js"))
+            ? currentScript.src
+            : getScriptUrl(BUNDLE_SCRIPT_BASENAME);
 
     if (baseScriptUrl) {
         bundleScriptUrl = baseScriptUrl;
@@ -177,5 +183,6 @@ function installDefaultDecoderUrlHook() {
  * @hidden
  */
 export function mapBundleMain() {
+    getBundleScriptUrl();
     installDefaultDecoderUrlHook();
 }