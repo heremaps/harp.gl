1591cb50a68c9e439432c93da3ecf7f550f8e3db
Andres Mandado <andres.mandado-almajano@here.com>
2020-01-08 11:03:50 +0100

    HARP-8356: Fix label intersections.
    When a label was replaced, the glyphs of its predecessor where
    always used, even if the replacement already had its glyphs loaded.
    
    Signed-off-by: Andres Mandado <andres.mandado-almajano@here.com>

--- @here/harp-mapview/lib/text/TextElementState.ts
+++ @here/harp-mapview/lib/text/TextElementState.ts
@@ -95,10 +95,12 @@ export class TextElementState {
         predecessor.m_textRenderState = undefined;
         predecessor.m_iconRenderStates = undefined;
 
-        // Use the predecessor glyphs, bounds and case array until proper ones are computed.
-        this.element.glyphs = predecessor.element.glyphs;
-        this.element.bounds = predecessor.element.bounds;
-        this.element.glyphCaseArray = predecessor.element.glyphCaseArray;
+        if (this.element.glyphs === undefined) {
+            // Use the predecessor glyphs, bounds and case array until proper ones are computed.
+            this.element.glyphs = predecessor.element.glyphs;
+            this.element.bounds = predecessor.element.bounds;
+            this.element.glyphCaseArray = predecessor.element.glyphCaseArray;
+        }
     }
 
     /**
