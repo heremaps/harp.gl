86a452779478f46351efd226ef660e0a845a62a7
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2020-01-17 16:21:08 +0100

    Improve resolving @here/harp-utils symbols in web target.
    `@here/harp-utils` contains platform-dependent module - UrlPlatformUtils
    that has to be resolved differently in node and web context.
    
    Follow strategy from `@here/harp-fetch` and use `package.json`s `browser`
    field, so `.web` versions are default in `web` target.
    
    Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

---
+++ @here/harp-utils/index-common.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export * from "./lib/GroupedPriorityList";
+export * from "./lib/Logger";
+export * from "./lib/Math2D";
+export * from "./lib/MathUtils";
+export * from "./lib/Mixins";
+export * from "./lib/assert";
+export * from "./lib/CachedResource";
+export * from "./lib/ContextLogger";
+export * from "./lib/PerformanceTimer";
+export * from "./lib/ObjectUtils";
+export * from "./lib/OptionsUtils";
+export * from "./lib/UriResolver";
+export * from "./lib/UrlUtils";
+export * from "./lib/Functions";
