b10f499206b89794cd05c651202867b92dcf5c03
Harald Fernengel <harald.fernengel@here.com>
2020-01-28 13:39:40 +0100

    Remove typestring dependency
    Typestring is unmaintained and brings along a super old TypeScript
    version. Use the TypeScript Compiler API instead.

--- @here/harp-datasource-protocol/test/ThemeTypingsTest.ts
+++ @here/harp-datasource-protocol/test/ThemeTypingsTest.ts
@@ -9,33 +9,65 @@
 
 import { LoggerManager } from "@here/harp-utils";
 import { assert } from "chai";
-
-declare const require: any;
+import * as ts from "typescript";
 
 const logger = LoggerManager.instance.create("ThemeTypingsTest");
 
 const isNode = typeof window === "undefined";
 const describeOnlyNode = isNode ? describe : xdescribe;
 
-function validate(type: string, response: any, validationExpectedToFail: boolean = false) {
-    try {
-        const ts = require("typestring");
-        const repl = ts.compile(
-            `import { ${type} } from "./lib/Theme";\n` +
-                `const reply: ${type} = ` +
-                JSON.stringify(response) +
-                ";", // {String} input - TypeScript to compile
-            {}, // {Object} [refs] - map of referenced filenames to content
-            {}, // {Object} [opts] - Options to TypeScript compiler
-            false // {Boolean} [semantic] - if true, throw semantic errors
-        );
-        assert.isDefined(repl);
-    } catch (err) {
-        logger.log(err);
-        if (!validationExpectedToFail) {
-            assert.fail();
+function tryTranspile(source: string, silent = false): boolean {
+    const sourceFileName = __dirname + "/.test.generated.ts";
+    const sourceFile = ts.createSourceFile(sourceFileName, source, ts.ScriptTarget.Latest);
+
+    const compilerHost = ts.createCompilerHost({});
+
+    // override getSourceFile in order to "inject" a string as file
+    const oldGetSourceFile = compilerHost.getSourceFile;
+    const getSourceFile = (
+        fileName: string,
+        languageVersion: ts.ScriptTarget,
+        onError?: (message: string) => void,
+        shouldCreateNewSourceFile?: boolean
+    ): ts.SourceFile | undefined => {
+        if (fileName === sourceFileName) {
+            return sourceFile;
         }
+        return oldGetSourceFile(fileName, languageVersion, onError, shouldCreateNewSourceFile);
+    };
+    compilerHost.getSourceFile = getSourceFile;
+    compilerHost.writeFile = () => {
+        /* do not write output to file */
+    };
+
+    const compilerOptions: ts.CompilerOptions = {
+        noEmitOnError: true,
+        noImplicitAny: true,
+        target: ts.ScriptTarget.Latest,
+        module: ts.ModuleKind.CommonJS
+    };
+
+    const program = ts.createProgram([sourceFileName], compilerOptions, compilerHost);
+    const result = program.emit();
+
+    // force transpilation without warnings
+    if (result.diagnostics.length !== 0) {
+        if (!silent) {
+            logger.warn("diagnostig messages:", result.diagnostics);
+        }
+        return false;
     }
+
+    return !result.emitSkipped;
+}
+
+function validate(type: string, response: any, validationExpectedToFail: boolean = false) {
+    const source =
+        `import { ${type} } from "../lib/Theme";\n` +
+        `const reply: ${type} = ${JSON.stringify(response)};`;
+
+    const result = tryTranspile(source, validationExpectedToFail);
+    assert.strictEqual(!result, validationExpectedToFail);
 }
 
 describeOnlyNode("Theme typings test", function() {
