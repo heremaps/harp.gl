3950f7eda32003d8a462c856b40e3635ba18fae7
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2020-02-07 09:20:31 +0100

    Fixes #1270. Construct correct Raycaster
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- @here/harp-mapview/lib/MapView.ts
+++ @here/harp-mapview/lib/MapView.ts
@@ -51,6 +51,7 @@ import { SimpleTileGeometryManager, TileGeometryManager } from "./geometry/TileG
 import { MapViewImageCache } from "./image/MapViewImageCache";
 import { MapViewFog } from "./MapViewFog";
 import { PickHandler, PickResult } from "./PickHandler";
+import { PickingRaycaster } from "./PickingRaycaster";
 import { PoiManager } from "./poi/PoiManager";
 import { PoiRendererFactory } from "./poi/PoiRendererFactory";
 import { PoiTableManager } from "./poi/PoiTableManager";
@@ -756,7 +757,7 @@ export class MapView extends THREE.EventDispatcher {
     private m_enablePolarDataSource: boolean = true;
 
     // gestures
-    private readonly m_raycaster = new THREE.Raycaster();
+    private readonly m_raycaster = new PickingRaycaster(this);
     private readonly m_plane = new THREE.Plane(new THREE.Vector3(0, 0, 1));
     private readonly m_sphere = new THREE.Sphere(undefined, EarthConstants.EQUATORIAL_RADIUS);
 
