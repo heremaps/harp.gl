heremaps/harp.gl

commit db6090118b428ea8c44e08053b14db34e99042fe

HARP-9037 Refactored DataSource zoom level limits(minDataLevel, maxDataLevel, minDisplayLevel, maxDisplayLevel). (#1375)
Signed-off-by: Marcel Pursche <marcel.pursche@here.com>

@here/harp-omv-datasource/lib/OmvDataSource.ts
@@ -15,7 +15,7 @@ import {
 } from "@here/harp-datasource-protocol";
 import { EarthConstants, TileKey, webMercatorTilingScheme } from "@here/harp-geoutils";
 import { LineGroup } from "@here/harp-lines";
-import { CopyrightInfo, CopyrightProvider } from "@here/harp-mapview";
+import { CopyrightInfo, CopyrightProvider, DataSourceOptions } from "@here/harp-mapview";
 import { DataProvider, TileDataSource, TileFactory } from "@here/harp-mapview-decoder";
 import { getOptionValue, LoggerManager } from "@here/harp-utils";
 import {
@@ -51,19 +51,7 @@ export interface OmvTileFactory {
     createTile(dataSource: OmvDataSource, tileKey: TileKey): OmvTile;
 }
 
-export interface OmvDataSourceParameters {
-    /**
-     * The unique name of the [[OmvDataSource]].
-     */
-    name?: string;
-
-    /**
-     * The name of the [[StyleSet]] that this [[OmvDataSource]] should use for decoding.
-     *
-     *  @default "omv"
-     */
-    styleSetName?: string;
-
+export interface OmvDataSourceParameters extends DataSourceOptions {
     /**
      * If set to `true`, features that have no technique in the theme will be printed to the console
      * (can be excessive!).
@@ -147,16 +135,6 @@ export interface OmvDataSourceParameters {
     copyrightProvider?: CopyrightProvider;
 
     /**
-     * Optional minimum zoom level (storage level) for [[Tile]]s. Default is 1.
-     */
-    minZoomLevel?: number;
-
-    /**
-     * Optional maximum zoom level (storage level) for [[Tile]]s. Default is 17.
-     */
-    maxZoomLevel?: number;
-
-    /**
      * Maximum geometry height above groud level this `OmvDataSource` can produce.
      *
      * Used in first stage of frustum culling before [[Tile.maxGeometryHeight]] data is available.
@@ -164,11 +142,6 @@ export interface OmvDataSourceParameters {
      * @default [[EarthConstants.MAX_BUILDING_HEIGHT]].
      */
     maxGeometryHeight?: number;
-
-    /**
-     * Optional storage level offset for [[Tile]]s. Default is -1.
-     */
-    storageLevelOffset?: number;
 
     /**
      * Indicates whether overlay on elevation is enabled. Defaults to `false`.
@@ -220,8 +193,10 @@ export class OmvDataSource extends TileDataSource<OmvTile> {
             concurrentDecoderScriptUrl: m_params.concurrentDecoderScriptUrl,
             copyrightInfo: m_params.copyrightInfo,
             copyrightProvider: m_params.copyrightProvider,
-            minZoomLevel: getOptionValue(m_params.minZoomLevel, 1),
-            maxZoomLevel: getOptionValue(m_params.maxZoomLevel, 17),
+            minDataLevel: getOptionValue(m_params.minDataLevel, 1),
+            maxDataLevel: getOptionValue(m_params.maxDataLevel, 17),
+            minDisplayLevel: m_params.minDisplayLevel,
+            maxDisplayLevel: m_params.maxDisplayLevel,
             storageLevelOffset: getOptionValue(m_params.storageLevelOffset, -1)
         });
 
@@ -298,34 +273,11 @@ export class OmvDataSource extends TileDataSource<OmvTile> {
         return true;
     }
 
-    /**
-     * Check if a data source should be rendered or not depending on the zoom level.
-     *
-     * @param zoomLevel Zoom level.
-     * @param tileKey Level of the tile.
-     * @returns `true` if the data source should be rendered.
-     * @override
-     */
-    canGetTile(zoomLevel: number, tileKey: TileKey): boolean {
-        if (tileKey.level > this.maxZoomLevel) {
-            return false;
-        }
-        if (tileKey.level <= this.maxZoomLevel && zoomLevel >= this.maxZoomLevel) {
-            return true;
-        }
-        return super.canGetTile(zoomLevel, tileKey);
-    }
-
     /** @override */
     setLanguages(languages?: string[]): void {
         if (languages !== undefined) {
             this.configureDecoder(undefined, undefined, languages, undefined);
         }
-    }
-
-    /** @override */
-    get storageLevelOffset() {
-        return super.storageLevelOffset;
     }
 
     /** @override */

@here/harp-debug-datasource/lib/DebugTileDataSource.ts
@@ -122,7 +122,7 @@ export class DebugTileDataSource extends DataSource {
         name = "debug",
         public maxDbgZoomLevel: number = 20
     ) {
-        super(name, undefined, 1, 20, -1);
+        super({ name, minDataLevel: 1, maxDataLevel: 20, storageLevelOffset: -1 });
 
         this.cacheable = true;
     }

@here/harp-mapview-decoder/lib/TileDataSource.ts
@@ -17,6 +17,7 @@ import {
     CopyrightInfo,
     CopyrightProvider,
     DataSource,
+    DataSourceOptions,
     Tile,
     TileLoaderState
 } from "@here/harp-mapview";
@@ -27,17 +28,7 @@ import { TileInfoLoader, TileLoader } from "./TileLoader";
 /**
  * Set of common options for all [[TileDataSource]]s.
  */
-export interface TileDataSourceOptions {
-    /**
-     * Name of [[TileDataSource]], must be unique.
-     */
-    name?: string;
-
-    /**
-     * The name of the [[StyleSet]] to evaluate for the decoding.
-     */
-    styleSetName: string;
-
+export interface TileDataSourceOptions extends DataSourceOptions {
     /**
      * The [[TilingScheme]] the data source is using.
      */
@@ -81,21 +72,6 @@ export interface TileDataSourceOptions {
      * provider are concatenated with default ones from `copyrightInfo`.
      */
     copyrightProvider?: CopyrightProvider;
-
-    /**
-     * Optional minimum zoom level (storage level) for [[Tile]]s. Default is 1.
-     */
-    minZoomLevel?: number;
-
-    /**
-     * Optional maximum zoom level (storage level) for [[Tile]]s. Default is 20.
-     */
-    maxZoomLevel?: number;
-
-    /**
-     * Optional storage level offset for [[Tile]]s. Default is 0.
-     */
-    storageLevelOffset?: number;
 }
 
 /**
@@ -144,13 +120,15 @@ export class TileDataSource<TileType extends Tile> extends DataSource {
         private readonly m_tileFactory: TileFactory<TileType>,
         private readonly m_options: TileDataSourceOptions
     ) {
-        super(
-            m_options.name,
-            m_options.styleSetName,
-            m_options.minZoomLevel,
-            m_options.maxZoomLevel,
-            m_options.storageLevelOffset
-        );
+        super({
+            name: m_options.name,
+            styleSetName: m_options.styleSetName,
+            minDataLevel: m_options.minDataLevel,
+            maxDataLevel: m_options.maxDataLevel,
+            minDisplayLevel: m_options.minDisplayLevel,
+            maxDisplayLevel: m_options.maxDisplayLevel,
+            storageLevelOffset: m_options.storageLevelOffset
+        });
         if (m_options.decoder) {
             this.m_decoder = m_options.decoder;
         } else if (m_options.concurrentDecoderServiceName) {

@here/harp-mapview/lib/BackgroundDataSource.ts
@@ -18,7 +18,7 @@ export class BackgroundDataSource extends DataSource {
     private m_tilingScheme: TilingScheme = BackgroundDataSource.DEFAULT_TILING_SCHEME;
 
     constructor() {
-        super("background");
+        super({ name: "background" });
         this.cacheable = true;
         this.addGroundPlane = true;
     }

@here/harp-mapview/lib/DataSource.ts
@@ -5,12 +5,59 @@
  */
 import { Definitions, StyleSet, Theme, ValueMap } from "@here/harp-datasource-protocol";
 import { Projection, TileKey, TilingScheme } from "@here/harp-geoutils";
-import { assert } from "@here/harp-utils";
+import { assert, LoggerManager } from "@here/harp-utils";
 import * as THREE from "three";
 import { MapView } from "./MapView";
 import { Tile } from "./Tile";
 
+const logger = LoggerManager.instance.create("DataSource");
 const UPDATE_EVENT = { type: "update" };
+
+/**
+ * Options for a [[DataSource]].
+ */
+export interface DataSourceOptions {
+    /**
+     * The unique name of a [[DataSource]] instance.
+     */
+    name?: string;
+    /**
+     * The name of the [[StyleSet]] to evaluate for the decoding.
+     */
+    styleSetName?: string;
+    /**
+     * The minimum zoom level at which data is available or displayed at
+     * (depending on [[DataSource]] subclass).
+     * @deprecated Use [[minDataLevel]] and [[minDisplayLevel]] instead.
+     */
+    minZoomLevel?: number;
+    /**
+     * The maximum zoom level at which data is available or displayed at
+     * (depending on [[DataSource]] subclass).
+     * @deprecated Use [[maxDataLevel]] and [[maxDisplayLevel]] instead.
+     */
+    maxZoomLevel?: number;
+    /**
+     * The minimum zoom level at which data is available.
+     */
+    minDataLevel?: number;
+    /**
+     * The maximum zoom level at which data is available.
+     */
+    maxDataLevel?: number;
+    /**
+     * The minimum zoom level at which [[DataSource]] is displayed.
+     */
+    minDisplayLevel?: number;
+    /**
+     * The maximum zoom level at which [[DataSource]] is displayed.
+     */
+    maxDisplayLevel?: number;
+    /**
+     * Storage level offset applied to this `DataSource`.
+     */
+    storageLevelOffset?: number;
+}
 
 /**
  * Derive a class from `DataSource` to contribute data and geometries to the [[MapView]].
@@ -51,6 +98,26 @@ export abstract class DataSource extends THREE.EventDispatcher {
     addGroundPlane: boolean = false;
 
     /**
+     * The minimum zoom level at which data is available.
+     */
+    minDataLevel: number = 1;
+
+    /**
+     * The maximum zoom level at which data is available.
+     */
+    maxDataLevel: number = 20;
+
+    /**
+     * The minimum zoom level at which [[DataSource]] is displayed.
+     */
+    minDisplayLevel: number = 1;
+
+    /**
+     * The maximum zoom level at which [[DataSource]] is displayed.
+     */
+    maxDisplayLevel: number = 20;
+
+    /**
      * The [[MapView]] instance holding a reference to this `DataSource`.
      */
     private m_mapView?: MapView;
@@ -59,16 +126,6 @@ export abstract class DataSource extends THREE.EventDispatcher {
      * The name of the [[StyleSet]] to evaluate for the decoding.
      */
     private m_styleSetName?: string;
-
-    /**
-     * Minimum zoom level this `DataSource` can be displayed in.
-     */
-    private m_minZoomLevel: number = 1;
-
-    /**
-     * Maximum zoom level this `DataSource` can be displayed in.
-     */
-    private m_maxZoomLevel: number = 20;
 
     /**
      * Current value of [[maxGeometryHeight]] property.
@@ -85,33 +142,49 @@ export abstract class DataSource extends THREE.EventDispatcher {
     /**
      * Constructs a new `DataSource`.
      *
-     * @param uniqueName A unique name that represents this `DataSource`.
-     * @param styleSetName The name of the [[StyleSet]] to refer to in a [[Theme]], to decode vector
-     * tiles.
-     * @param minZoomLevel Minimum zoom level this `DataSource` can be displayed in.
-     * @param maxZoomLevel Maximum zoom level this `DataSource` can be displayed in.
-     * @param storageLevelOffset Storage level offset applied to this `DataSource`.
+     * @param options The options to create the data source.
      */
-    constructor(
-        uniqueName?: string,
-        styleSetName?: string,
-        minZoomLevel?: number,
-        maxZoomLevel?: number,
-        storageLevelOffset?: number
-    ) {
+    constructor(options: DataSourceOptions = {}) {
         super();
-        if (uniqueName === undefined || uniqueName.length === 0) {
-            uniqueName = `anonymous-datasource#${++DataSource.uniqueNameCounter}`;
+        let { name } = options;
+        const {
+            styleSetName,
+            // tslint:disable-next-line: deprecation
+            minZoomLevel,
+            // tslint:disable-next-line: deprecation
+            maxZoomLevel,
+            minDataLevel,
+            maxDataLevel,
+            minDisplayLevel,
+            maxDisplayLevel,
+            storageLevelOffset
+        } = options;
+        if (name === undefined || name.length === 0) {
+            name = `anonymous-datasource#${++DataSource.uniqueNameCounter}`;
         }
-        this.name = uniqueName;
+        this.name = name;
 
         this.styleSetName = styleSetName;
 
         if (minZoomLevel !== undefined) {
-            this.m_minZoomLevel = minZoomLevel;
+            // tslint:disable-next-line: deprecation
+            this.minZoomLevel = minZoomLevel;
         }
         if (maxZoomLevel !== undefined) {
-            this.m_maxZoomLevel = maxZoomLevel;
+            // tslint:disable-next-line: deprecation
+            this.maxZoomLevel = maxZoomLevel;
+        }
+        if (minDataLevel !== undefined) {
+            this.minDataLevel = minDataLevel;
+        }
+        if (maxDataLevel !== undefined) {
+            this.maxDataLevel = maxDataLevel;
+        }
+        if (minDisplayLevel !== undefined) {
+            this.minDisplayLevel = minDisplayLevel;
+        }
+        if (maxDisplayLevel !== undefined) {
+            this.maxDisplayLevel = maxDisplayLevel;
         }
         if (storageLevelOffset !== undefined) {
             this.m_storageLevelOffset = storageLevelOffset;
@@ -333,27 +406,41 @@ export abstract class DataSource extends THREE.EventDispatcher {
     }
 
     /**
-     * The minimum zoom level to use for display.
-     *
-     * @returns The minimum zoom level to use for display.
+     * The minimum zoom level at which data is available or displayed at
+     * (depending on [[DataSource]] subclass).
+     * @deprecated Use [[minDataLevel]] and [[minDisplayLevel]] instead.
      */
     get minZoomLevel(): number {
-        return this.m_minZoomLevel;
+        logger.warn(
+            "DataSource.minZoomLevel is deprecated. Use minDataLevel and maxDataLevel instead."
+        );
+        return this.minDataLevel;
     }
 
     set minZoomLevel(level: number) {
-        this.m_minZoomLevel = level;
+        logger.warn(
+            "DataSource.minZoomLevel is deprecated. Use minDataLevel and minDisplayLevel instead."
+        );
+        this.minDataLevel = level;
     }
 
     /**
-     * The maximum zoom level to use for display.
+     * The maximum zoom level at which data is available or displayed at
+     * (depending on [[DataSource]] subclass).
+     * @deprecated Use [[maxDataLevel]] and [[maxDisplayLevel]] instead.
      */
     get maxZoomLevel(): number {
-        return this.m_maxZoomLevel;
+        logger.warn(
+            "DataSource.maxZoomLevel is deprecated. Use maxDataLevel and maxDisplayLevel instead."
+        );
+        return this.maxDataLevel;
     }
 
     set maxZoomLevel(level: number) {
-        this.m_maxZoomLevel = level;
+        logger.warn(
+            "DataSource.maxZoomLevel is deprecated. Use maxDataLevel and maxDisplayLevel instead."
+        );
+        this.maxDataLevel = level;
     }
 
     /**
@@ -402,17 +489,25 @@ export abstract class DataSource extends THREE.EventDispatcher {
     }
 
     /**
-     * Computes the zoom level to use for display.
+     * Computes the data zoom level to use.
      *
      * @param zoomLevel The zoom level of the [[MapView]].
-     * @returns The zoom level to use for display.
+     * @returns The data zoom level to use.
      */
-    getDisplayZoomLevel(zoomLevel: number): number {
+    getDataZoomLevel(zoomLevel: number): number {
         return THREE.MathUtils.clamp(
             zoomLevel + this.m_storageLevelOffset,
-            this.m_minZoomLevel,
-            this.m_maxZoomLevel
+            this.minDataLevel,
+            this.maxDataLevel
         );
+    }
+
+    /**
+     * Returns `true` if [[DataSource]] should be displayed for the zoom level.
+     * @param zoomLevel The zoom level of the [[MapView]].
+     */
+    isVisible(zoomLevel: number): boolean {
+        return zoomLevel >= this.minDisplayLevel && zoomLevel <= this.maxDisplayLevel;
     }
 
     /**

@here/harp-mapview/lib/MapView.ts
@@ -1802,7 +1802,8 @@ export class MapView extends THREE.EventDispatcher {
         return (
             dataSource.enabled &&
             dataSource.ready() &&
-            this.m_connectedDataSources.has(dataSource.name)
+            this.m_connectedDataSources.has(dataSource.name) &&
+            dataSource.isVisible(this.zoomLevel)
         );
     }
 

@here/harp-mapview/lib/PolarTileDataSource.ts
@@ -17,36 +17,11 @@ import {
     TransverseMercatorUtils
 } from "@here/harp-geoutils";
 
-import { DataSource } from "./DataSource";
+import { DataSource, DataSourceOptions } from "./DataSource";
 import { createMaterial } from "./DecodedTileHelpers";
 import { Tile } from "./Tile";
 
-export interface PolarTileDataSourceOptions {
-    /**
-     * Name of [[TileDataSource]], must be unique.
-     */
-    name?: string;
-
-    /**
-     * The name of the [[StyleSet]] to evaluate for the decoding.
-     */
-    styleSetName?: string;
-
-    /**
-     * Optional minimum zoom level (storage level) for [[Tile]]s. Default is 1.
-     */
-    minZoomLevel?: number;
-
-    /**
-     * Optional maximum zoom level (storage level) for [[Tile]]s. Default is 20.
-     */
-    maxZoomLevel?: number;
-
-    /**
-     * Optional storage level offset for [[Tile]]s. Default is -1.
-     */
-    storageLevelOffset?: number;
-
+export interface PolarTileDataSourceOptions extends DataSourceOptions {
     /**
      * Optional level offset of regular tiles from reference datasource to align tiles to.
      * Default is -1.
@@ -76,13 +51,23 @@ export class PolarTileDataSource extends DataSource {
     constructor({
         name = "polar",
         styleSetName,
-        minZoomLevel,
-        maxZoomLevel,
+        minDataLevel,
+        maxDataLevel,
+        minDisplayLevel,
+        maxDisplayLevel,
         storageLevelOffset = -2,
-        geometryLevelOffset = -1,
+        geometryLevelOffset = 1,
         debugTiles = false
     }: PolarTileDataSourceOptions) {
-        super(name, styleSetName, minZoomLevel, maxZoomLevel, storageLevelOffset);
+        super({
+            name,
+            styleSetName,
+            minDataLevel,
+            maxDataLevel,
+            minDisplayLevel,
+            maxDisplayLevel,
+            storageLevelOffset
+        });
 
         this.m_geometryLevelOffset = geometryLevelOffset;
         this.m_debugTiles = debugTiles;

@here/harp-mapview/lib/VisibleTileSet.ts
@@ -519,7 +519,7 @@ export class VisibleTileSet {
             let allDataSourceTilesLoaded = true;
             let numTilesLoading = 0;
             // Create actual tiles only for the allowed number of visible tiles
-            const displayZoomLevel = dataSource.getDisplayZoomLevel(zoomLevel);
+            const dataZoomLevel = dataSource.getDataZoomLevel(zoomLevel);
             for (
                 let i = 0;
                 i < visibleTileKeys.length &&
@@ -563,7 +563,7 @@ export class VisibleTileSet {
             this.dataSourceTileList.push({
                 dataSource,
                 storageLevel,
-                zoomLevel: displayZoomLevel,
+                zoomLevel: dataZoomLevel,
                 allVisibleTileLoaded: allDataSourceTilesLoaded,
                 numTilesLoading,
                 visibleTiles: actuallyVisibleTiles,
@@ -883,11 +883,11 @@ export class VisibleTileSet {
     ): { searchLevelsUp: number; searchLevelsDown: number } {
         const searchLevelsUp = Math.min(
             this.options.quadTreeSearchDistanceUp,
-            Math.max(0, visibleLevel - dataSource.minZoomLevel)
+            Math.max(0, visibleLevel - dataSource.minDataLevel)
         );
         const searchLevelsDown = Math.min(
             this.options.quadTreeSearchDistanceDown,
-            Math.max(0, dataSource.maxZoomLevel - visibleLevel)
+            Math.max(0, dataSource.maxDataLevel - visibleLevel)
         );
 
         return { searchLevelsUp, searchLevelsDown };
@@ -903,7 +903,7 @@ export class VisibleTileSet {
     private fillMissingTilesFromCache() {
         this.dataSourceTileList.forEach(renderListEntry => {
             const dataSource = renderListEntry.dataSource;
-            const displayZoomLevel = renderListEntry.zoomLevel;
+            const dataZoomLevel = renderListEntry.zoomLevel;
             const renderedTiles = renderListEntry.renderedTiles;
 
             // Direction in quad tree to search: up -> shallower levels, down -> deeper levels.
@@ -917,7 +917,7 @@ export class VisibleTileSet {
 
             const { searchLevelsUp, searchLevelsDown } = this.getCacheSearchLevels(
                 dataSource,
-                displayZoomLevel
+                dataZoomLevel
             );
 
             defaultSearchDirection =
@@ -966,7 +966,7 @@ export class VisibleTileSet {
                     if (
                         this.findUp(
                             tileKeyCode,
-                            displayZoomLevel,
+                            dataZoomLevel,
                             renderedTiles,
                             checkedTiles,
                             dataSource
@@ -981,7 +981,7 @@ export class VisibleTileSet {
                     searchDirection === SearchDirection.BOTH ||
                     searchDirection === SearchDirection.DOWN
                 ) {
-                    this.findDown(tileKeyCode, displayZoomLevel, renderedTiles, dataSource);
+                    this.findDown(tileKeyCode, dataZoomLevel, renderedTiles, dataSource);
                 }
             }
         });
@@ -989,7 +989,7 @@ export class VisibleTileSet {
 
     private findDown(
         tileKeyCode: number,
-        displayZoomLevel: number,
+        dataZoomLevel: number,
         renderedTiles: Map<number, Tile>,
         dataSource: DataSource
     ) {
@@ -1007,7 +1007,7 @@ export class VisibleTileSet {
                 dataSource
             );
 
-            const nextLevelDiff = Math.abs(childTileKey.level - displayZoomLevel);
+            const nextLevelDiff = Math.abs(childTileKey.level - dataZoomLevel);
             if (childTile !== undefined && childTile.hasGeometry) {
                 // childTile has geometry, so can be reused as fallback
                 renderedTiles.set(childTileCode, childTile);
@@ -1017,7 +1017,7 @@ export class VisibleTileSet {
 
             // Recurse down until the max distance is reached.
             if (nextLevelDiff < this.options.quadTreeSearchDistanceDown) {
-                this.findDown(childTileCode, displayZoomLevel, renderedTiles, dataSource);
+                this.findDown(childTileCode, dataZoomLevel, renderedTiles, dataSource);
             }
         }
     }
@@ -1025,7 +1025,7 @@ export class VisibleTileSet {
     /**
      * Returns true if a tile was found in the cache which is a parent
      * @param tileKeyCode Morton code of the current tile that should be searched for.
-     * @param displayZoomLevel The current zoom level of tiles that are to be displayed.
+     * @param dataZoomLevel The current data zoom level of tiles that are to be displayed.
      * @param renderedTiles The list of tiles that are shown to the user.
      * @param checkedTiles Used to map a given code to a boolean which tells us if an ancestor is
      * displayed or not.
@@ -1034,7 +1034,7 @@ export class VisibleTileSet {
      */
     private findUp(
         tileKeyCode: number,
-        displayZoomLevel: number,
+        dataZoomLevel: number,
         renderedTiles: Map<number, Tile>,
         checkedTiles: Map<number, boolean>,
         dataSource: DataSource
@@ -1052,7 +1052,7 @@ export class VisibleTileSet {
         const { offset, mortonCode } = TileOffsetUtils.extractOffsetAndMortonKeyFromKey(parentCode);
         const parentTile = this.m_dataSourceCache.get(mortonCode, offset, dataSource);
         const parentTileKey = parentTile ? parentTile.tileKey : TileKey.fromMortonCode(mortonCode);
-        const nextLevelDiff = Math.abs(displayZoomLevel - parentTileKey.level);
+        const nextLevelDiff = Math.abs(dataZoomLevel - parentTileKey.level);
         if (parentTile !== undefined && parentTile.hasGeometry) {
             checkedTiles.set(parentCode, true);
             // parentTile has geometry, so can be reused as fallback
@@ -1070,7 +1070,7 @@ export class VisibleTileSet {
         if (nextLevelDiff < this.options.quadTreeSearchDistanceUp && parentTileKey.level !== 0) {
             const foundUp = this.findUp(
                 parentCode,
-                displayZoomLevel,
+                dataZoomLevel,
                 renderedTiles,
                 checkedTiles,
                 dataSource
@@ -1212,7 +1212,7 @@ export class VisibleTileSet {
         // For each bucket of data sources with same tiling scheme, calculate frustum intersection
         // once using the maximum display level.
         for (const [tilingScheme, bucket] of dataSourceBuckets) {
-            const zoomLevels = bucket.map(dataSource => dataSource.getDisplayZoomLevel(zoomLevel));
+            const zoomLevels = bucket.map(dataSource => dataSource.getDataZoomLevel(zoomLevel));
             const result = this.m_frustumIntersection.compute(
                 tilingScheme,
                 elevationRangeSource,
@@ -1226,9 +1226,9 @@ export class VisibleTileSet {
                 // For each data source check what tiles from the intersection should be rendered
                 // at this zoom level.
                 const visibleTileKeys: TileKeyEntry[] = [];
-                const displayZoomLevel = dataSource.getDisplayZoomLevel(zoomLevel);
-                for (const tileKeyEntry of result.tileKeyEntries.get(displayZoomLevel)!.values()) {
-                    if (dataSource.canGetTile(displayZoomLevel, tileKeyEntry.tileKey)) {
+                const dataZoomLevel = dataSource.getDataZoomLevel(zoomLevel);
+                for (const tileKeyEntry of result.tileKeyEntries.get(dataZoomLevel)!.values()) {
+                    if (dataSource.canGetTile(dataZoomLevel, tileKeyEntry.tileKey)) {
                         visibleTileKeys.push(tileKeyEntry);
                     }
                 }

commit 489886a109cc0a750265696c27ae3a1aff1a1023

Fixed bug in OmvDataSource introduced by PR #1375 (HARP-9037).
Signed-off-by: Marcel Pursche <marcel.pursche@here.com>

@here/harp-omv-datasource/lib/OmvDataSource.ts
@@ -281,6 +281,11 @@ export class OmvDataSource extends TileDataSource<OmvTile> {
     }
 
     /** @override */
+    get storageLevelOffset() {
+        return super.storageLevelOffset;
+    }
+
+    /** @override */
     set storageLevelOffset(levelOffset: number) {
         super.storageLevelOffset = levelOffset;
         this.m_decoderOptions.storageLevelOffset = this.storageLevelOffset;

@here/harp-omv-datasource/test/OmvDataSourceTest.ts
@@ -79,4 +79,28 @@ describe("DataProviders", function() {
         });
         assert.isTrue(omvDataSource.dataProvider() instanceof MockDataProvider);
     });
+
+    describe("storageLevelOffset", function() {
+        it("updates storageLevelOffset in decoder options", function() {
+            const mapView = {
+                markTilesDirty() {
+                    /* noop */
+                }
+            } as any;
+            const mockDataProvider = new MockDataProvider();
+            const omvDataSource = new OmvDataSource({
+                decoder: new OmvTileDecoder(),
+                baseUrl: "https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7",
+                apiFormat: APIFormat.MapboxV4,
+                authenticationCode: "123",
+                dataProvider: mockDataProvider
+            });
+            omvDataSource.attach(mapView);
+            omvDataSource.storageLevelOffset = 2;
+            assert.equal(omvDataSource.storageLevelOffset, 2);
+            // tslint:disable-next-line: no-string-literal
+            assert.equal(omvDataSource["m_decoderOptions"].storageLevelOffset, 2);
+            assert.equal((omvDataSource.decoder as any).m_storageLevelOffset, 2);
+        });
+    });
 });