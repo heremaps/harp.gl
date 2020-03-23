ba9b25bdedd9bc19f5de08e101f663572a65bd75
Andres Mandado <andres.mandado-almajano@here.com>
2020-03-23 12:17:00 +0100

    HARP-9275: DisplacedBufferGeometry/Attribute classes to displace meshes on the fly.
    Mesh displacement for terrain overlay is done in the vertex shader. For picking,
    mesh displacement is done on demand in the CPU.
    
    Signed-off-by: Andres Mandado <andres.mandado-almajano@here.com>

---
+++ @here/harp-mapview/lib/geometry/DisplacedBufferAttribute.ts
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Vector3Like } from "@here/harp-geoutils";
+import { sampleBilinear } from "@here/harp-utils";
+import * as THREE from "three";
+import { VertexCache } from "./VertexCache";
+
+/**
+ * @internal
+ * BufferAttribute decorator that displaces on the fly the coordinates in a given attribute using a
+ * specified displacement map.
+ */
+export class DisplacedBufferAttribute extends THREE.BufferAttribute {
+    private static MAX_CACHE_SIZE = 6;
+    private m_texture?: Float32Array;
+    private m_textureWidth: number = 0;
+    private m_textureHeight: number = 0;
+    private m_cache = new VertexCache(DisplacedBufferAttribute.MAX_CACHE_SIZE);
+    private m_lastBufferIndex?: number;
+    private m_lastPos = new THREE.Vector3();
+    private m_tmpNormal = new THREE.Vector3();
+
+    /**
+     * Creates an instance of displaced buffer attribute.
+     * @param originalAttribute The buffer attribute to be displaced (e.g. the position attribute).
+     * @param m_normals The normals along which the coordinates will be displaced.
+     * @param m_uvs The uv coordinates to be used to sample the displacement map.
+     * @param displacementMap A texture with the displacement values in 32bit floats.
+     */
+    constructor(
+        public originalAttribute: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        private m_normals: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        private m_uvs: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        displacementMap: THREE.DataTexture
+    ) {
+        super(originalAttribute.array, originalAttribute.itemSize, originalAttribute.normalized);
+        this.resetTexture(displacementMap);
+    }
+
+    /**
+     * Resets the displaced buffer attribute to use new buffer attributes or displacement map.
+     * @param originalAttribute The buffer attribute to be displaced (e.g. the position attribute).
+     * @param normals The normals along which the coordinates will be displaced.
+     * @param uvs  The uv coordinates to be used to sample the displacement map.
+     * @param displacementMap A texture with the displacement values in 32bit floats.
+     */
+    reset(
+        originalAttribute: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        normals: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        uvs: THREE.BufferAttribute | THREE.InterleavedBufferAttribute,
+        displacementMap: THREE.DataTexture
+    ) {
+        this.array = originalAttribute.array;
+        this.itemSize = originalAttribute.itemSize;
+        this.count = this.array.length / this.itemSize;
+        this.normalized = originalAttribute.normalized;
+        this.originalAttribute = originalAttribute;
+        this.m_normals = normals;
+        this.m_uvs = uvs;
+        this.m_cache.clear();
+        this.m_lastBufferIndex = undefined;
+        this.resetTexture(displacementMap);
+    }
+
+    // HARP-9585: These getters are overrides of the base class ones, however tslint doesn't
+    // recognize them as such.
+    getX(index: number): number {
+        return this.getDisplacedCoordinate(index).x;
+    }
+    getY(index: number): number {
+        return this.getDisplacedCoordinate(index).y;
+    }
+    getZ(index: number): number {
+        return this.getDisplacedCoordinate(index).z;
+    }
+
+    private resetTexture(displacementMap: THREE.DataTexture) {
+        this.m_texture = new Float32Array(displacementMap.image.data.buffer);
+        this.m_textureWidth = displacementMap.image.width;
+        this.m_textureHeight = displacementMap.image.height;
+    }
+    private getDisplacedCoordinate(bufferIndex: number): Vector3Like {
+        if (bufferIndex === this.m_lastBufferIndex) {
+            return this.m_lastPos;
+        }
+        this.m_lastBufferIndex = bufferIndex;
+        if (this.m_cache.get(bufferIndex, this.m_lastPos)) {
+            return this.m_lastPos;
+        }
+        this.displacePosition(bufferIndex);
+        this.m_cache.set(bufferIndex, this.m_lastPos);
+        return this.m_lastPos;
+    }
+    private displacePosition(bufferIndex: number) {
+        this.m_lastPos.set(
+            super.getX(bufferIndex),
+            super.getY(bufferIndex),
+            super.getZ(bufferIndex)
+        );
+        const normals = this.m_normals as THREE.BufferAttribute;
+        this.m_tmpNormal.fromBufferAttribute(normals, bufferIndex);
+        const uvs = this.m_uvs;
+        const u = THREE.MathUtils.clamp(uvs.getX(bufferIndex), 0, 1);
+        const v = THREE.MathUtils.clamp(uvs.getY(bufferIndex), 0, 1);
+        const displacement = sampleBilinear(
+            this.m_texture!,
+            this.m_textureWidth,
+            this.m_textureHeight,
+            u,
+            v
+        );
+        this.m_lastPos.add(this.m_tmpNormal.multiplyScalar(displacement));
+    }
+}
---
+++ @here/harp-mapview/lib/geometry/DisplacedBufferGeometry.ts
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as THREE from "three";
+import { DisplacedBufferAttribute } from "./DisplacedBufferAttribute";
+
+const tmpNormalMin = new THREE.Vector3();
+const tmpNormalMax = new THREE.Vector3();
+const tmpBBox = new THREE.Box3();
+
+/**
+ * @internal
+ * Range of displacement values found in a given displacement map.
+ */
+export interface DisplacementRange {
+    min: number;
+    max: number;
+}
+
+/**
+ * @internal
+ * BufferGeometry decorator that displaces on the fly the position attribute using a specified
+ * displacement map.
+ */
+export class DisplacedBufferGeometry extends THREE.BufferGeometry {
+    private m_displacedPositions: DisplacedBufferAttribute;
+    private m_originalBoundingBox = new THREE.Box3();
+
+    /**
+     * Creates an instance of displaced buffer geometry.
+     * @param originalGeometry The goeometry to be displaced.
+     * @param displacementMap A texture with the displacement values.
+     * @param m_displacementRange The displacement value range found in the displacement map.
+     * @param displacedPositions Buffer attribute that will be used for displaced positions if
+     * provided, otherwise a new buffer attribute will be created.
+     */
+    constructor(
+        public originalGeometry: THREE.BufferGeometry,
+        displacementMap: THREE.DataTexture,
+        private m_displacementRange: DisplacementRange,
+        displacedPositions?: DisplacedBufferAttribute
+    ) {
+        super();
+        if (!displacedPositions) {
+            this.m_displacedPositions = new DisplacedBufferAttribute(
+                originalGeometry.attributes.position,
+                originalGeometry.attributes.normal,
+                originalGeometry.attributes.uv,
+                displacementMap
+            );
+        } else {
+            this.m_displacedPositions = displacedPositions;
+        }
+        this.resetAttributes();
+    }
+
+    /**
+     * Resets the displaced buffer geometry to use new geometry or displacement map.
+     * @param geometry The goeometry to be displaced.
+     * @param displacementMap A texture with the displacement values.
+     * @param displacementRange The displacement value range found in the displacement map.
+     */
+    reset(
+        geometry: THREE.BufferGeometry,
+        displacementMap: THREE.DataTexture,
+        displacementRange: DisplacementRange
+    ) {
+        this.originalGeometry = geometry;
+        const positions = geometry.attributes.position;
+        const normals = geometry.attributes.normal;
+        const uvs = geometry.attributes.uv;
+        this.m_displacedPositions.reset(positions, normals, uvs, displacementMap);
+        const displacementRangeChanged =
+            this.m_displacementRange.min !== displacementRange.min ||
+            this.m_displacementRange.max !== displacementRange.max;
+        this.m_displacementRange = displacementRange;
+        this.resetAttributes();
+        this.resetBoundingVolumes(displacementRangeChanged);
+    }
+
+    // HARP-9585: Override of base class method, however tslint doesn't recognize it as such.
+    computeBoundingBox(): void {
+        // Calculate a coarse approximation of the displaced geometry bbox by displacing the
+        // original bbox and enlarging it to cover the whole displacement range.
+        // This approximation is used to avoid having to displace the whole geometry, which will
+        // be done only if the bbox test passes.
+        if (this.originalGeometry.boundingBox === null) {
+            this.originalGeometry.computeBoundingBox();
+        }
+        const origBBox = this.m_originalBoundingBox.copy(this.originalGeometry.boundingBox);
+        if (this.boundingBox === null) {
+            this.boundingBox = origBBox.clone();
+        } else {
+            this.boundingBox.copy(origBBox);
+        }
+        tmpBBox.copy(origBBox);
+        tmpNormalMin.fromBufferAttribute(this.attributes.normal as THREE.BufferAttribute, 0);
+        tmpNormalMax.copy(tmpNormalMin);
+        this.boundingBox
+            .translate(tmpNormalMin.multiplyScalar(this.m_displacementRange.min))
+            .union(tmpBBox.translate(tmpNormalMax.multiplyScalar(this.m_displacementRange.max)));
+    }
+
+    // HARP-9585: Override of base class method, however tslint doesn't recognize it as such.
+    computeBoundingSphere(): void {
+        // Use as coarse approximation the sphere bounding the bbox.
+        if (this.boundingBox === null) {
+            this.computeBoundingBox();
+        }
+        if (this.boundingSphere === null) {
+            this.boundingSphere = new THREE.Sphere();
+        }
+        this.boundingBox.getBoundingSphere(this.boundingSphere);
+    }
+
+    private needsBoundingBoxUpdate(displacementRangeChanged: boolean): boolean {
+        return (
+            displacementRangeChanged ||
+            (this.boundingBox &&
+                (!this.originalGeometry.boundingBox ||
+                    !this.m_originalBoundingBox.equals(this.originalGeometry.boundingBox)))
+        );
+    }
+
+    private resetBoundingVolumes(displacementRangeChanged: boolean) {
+        if (this.needsBoundingBoxUpdate(displacementRangeChanged)) {
+            this.computeBoundingBox();
+            if (this.boundingSphere) {
+                this.computeBoundingSphere();
+            }
+        }
+    }
+
+    private resetAttributes() {
+        this.index = this.originalGeometry.index;
+        this.groups = this.originalGeometry.groups;
+        this.drawRange = this.originalGeometry.drawRange;
+        this.attributes = { ...this.originalGeometry.attributes };
+        this.attributes.position = this.m_displacedPositions;
+    }
+}
