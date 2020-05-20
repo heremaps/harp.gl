36ec9bbf849ea85158125838042bacc864a3b9d1
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2020-05-20 15:35:17 +0200

    HARP-10450 MINOR Remove unused file (#1548)
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- @here/harp-datasource-protocol/index-decoder.ts
+++ @here/harp-datasource-protocol/index-decoder.ts
@@ -1,11 +1,10 @@
 /*
- * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Copyright (C) 2017-2020 HERE Europe B.V.
  * Licensed under Apache 2.0, see full license in LICENSE
  * SPDX-License-Identifier: Apache-2.0
  */
 
 export * from "./lib/StyleSetEvaluator";
-export * from "./lib/Extruder";
 export * from "./lib/IMeshBuffers";
 export * from "./lib/Outliner";
 export * from "./lib/Expr";
