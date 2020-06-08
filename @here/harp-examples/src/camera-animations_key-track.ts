dc699a8f1ddf210ec6eec068ec69a4d71d03a636
Frauke Fritz <42568257+FraukeF@users.noreply.github.com>
2020-06-08 15:55:54 +0200

    (HARP-10278) Adds CameraKeyTrackAnimations and Utilities (#1500)
    Signed-off-by: Frauke Fritz <frauke.fritz@here.com>

---
+++ @here/harp-examples/src/camera-animations_key-track.ts
@@ -0,0 +1,261 @@
+/*
+ * Copyright (C) 2017-2020 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { GeoCoordinates, mercatorProjection, sphereProjection } from "@here/harp-geoutils";
+import {
+    CameraAnimationBuilder,
+    CameraKeyTrackAnimation,
+    ControlPoint
+} from "@here/harp-map-controls";
+import { CopyrightElementHandler, MapView } from "@here/harp-mapview";
+import { APIFormat, AuthenticationMethod, OmvDataSource } from "@here/harp-omv-datasource";
+import { GUI } from "dat.gui";
+import THREE = require("three");
+
+import { apikey, copyrightInfo } from "../config";
+
+interface GeoLocations {
+    [key: string]: GeoCoordinates;
+}
+/**
+ * In this example we use the [[CameraKeyTrackAnimation]] and the [[CameraAnimationBuilder]] to
+ * generate animations.
+ *
+ * First we create a map
+ * Then we generate different camera animations.
+ *
+ *
+ * Flying a bow to a next location using a maximum altitude for the bow. As a starting
+ * [[ControlPoint]], the current Camera properties are used and converted to a [[ControlPoint]].
+ * ```typescript
+ * [[include:harp_gl_camera_animation_1.ts]]
+ * ```
+ * An orbit animation around the current target of the map.
+ * ```typescript
+ * [[include:harp_gl_camera_animation_2.ts]]
+ * ```
+ *
+ * A [[CameraKeyTrackAnimation]] using some geoLocations to generate the [[ControlPoint]]s
+ *
+ * The simple [[CameraKeyTrackAnimationOptions]] contain a list of [[ControlPoint]]s
+ * ```typescript
+ * [[include:harp_gl_camera_animation_3.ts]]
+ * ```
+ *
+ *
+ */
+export namespace CameraAnimationExample {
+    // snippet:harp_gl_camera_animation_0.ts
+    const map = createBaseMap();
+    // end:harp_gl_camera_animation_0.ts
+
+    const geoLocations: GeoLocations = {
+        Dubai: new GeoCoordinates(25.19705, 55.27419),
+        BerlinStation: new GeoCoordinates(52.5250871, 13.367208),
+        BerlinReichstag: new GeoCoordinates(52.5186234, 13.373993),
+        BerlinTower: new GeoCoordinates(52.52081829, 13.407225)
+    };
+
+    map.lookAt({
+        target: geoLocations.Dubai,
+        distance: 1000,
+        tilt: 45
+    });
+
+    const options = {
+        globe: true,
+        orbit: false,
+        flyTo: "Dubai",
+        flyOver: false
+    };
+    const animationOptions = {
+        interpolation: THREE.InterpolateSmooth,
+        loop: THREE.LoopOnce,
+        repetitions: 1,
+        rotateOnlyClockWise: true
+    };
+    let cameraAnimation: CameraKeyTrackAnimation | undefined;
+
+    // snippet:harp_gl_camera_animation_3.ts
+    const flyOverAnimationOptions = {
+        controlPoints: [
+            new ControlPoint({
+                target: geoLocations.BerlinReichstag,
+                timestamp: 0,
+                heading: 300,
+                tilt: 45,
+                distance: 500
+            }),
+            new ControlPoint({
+                target: geoLocations.BerlinStation,
+                timestamp: 10,
+                heading: 20,
+                tilt: 45,
+                distance: 3000
+            }),
+            new ControlPoint({
+                target: geoLocations.BerlinTower,
+                timestamp: 15,
+                heading: 180,
+                tilt: 35,
+                distance: 200
+            })
+        ]
+    };
+    // end:harp_gl_camera_animation_3.ts
+
+    const gui = new GUI({ width: 300 });
+    gui.add(options, "globe").onChange(() => {
+        map.projection = options.globe ? sphereProjection : mercatorProjection;
+    });
+    gui.add(options, "orbit")
+        .onChange(enableOrbit)
+        .listen();
+    gui.add(options, "flyTo", [...Object.keys(geoLocations)])
+        .onChange(flyTo)
+        .listen();
+    gui.add(options, "flyOver")
+        .onChange(enableFlyOver)
+        .listen();
+
+    gui.add(animationOptions, "interpolation", {
+        smooth: THREE.InterpolateSmooth,
+        linear: THREE.InterpolateLinear,
+        discrete: THREE.InterpolateDiscrete
+    })
+        .onChange(value => {
+            animationOptions.interpolation = parseInt(value, 10);
+            alert("This will only take effect for the next animation created");
+        })
+        .listen();
+
+    gui.add(animationOptions, "repetitions", [1, 2, 3, 5, 10, Infinity])
+        .onChange(value => {
+            if (cameraAnimation) {
+                cameraAnimation.repetitions = value;
+            }
+        })
+        .listen();
+
+    gui.add(animationOptions, "loop", {
+        once: THREE.LoopOnce,
+        pingpong: THREE.LoopPingPong,
+        repeat: THREE.LoopRepeat
+    }).onChange(value => {
+        animationOptions.loop = parseInt(value, 10);
+        if (cameraAnimation) {
+            cameraAnimation.loop = parseInt(value, 10);
+        }
+    });
+
+    gui.add(animationOptions, "rotateOnlyClockWise")
+        .onChange(value => {
+            if (cameraAnimation) {
+                cameraAnimation.rotateOnlyClockwise = value;
+            }
+        })
+        .listen();
+
+    function stopAnimation() {
+        if (cameraAnimation) {
+            cameraAnimation.stop();
+            cameraAnimation = undefined;
+            options.flyOver = false;
+            options.orbit = false;
+        }
+    }
+    function enableOrbit(enable: boolean) {
+        stopAnimation();
+        options.orbit = enable;
+        if (enable) {
+            cameraAnimation = createOrbitAnimation();
+            cameraAnimation.start();
+        }
+    }
+
+    function flyTo(location: string) {
+        stopAnimation();
+        options.flyTo = location;
+        if (location !== "") {
+            // snippet:harp_gl_camera_animation_1.ts
+            const target = new ControlPoint({
+                target: geoLocations[location] as GeoCoordinates,
+                distance: 800,
+                tilt: 25,
+                heading: Math.random() * 360,
+                timestamp: 10
+            });
+            const flyToOpts = CameraAnimationBuilder.createBowFlyToOptions(
+                map,
+                new ControlPoint({
+                    ...CameraAnimationBuilder.getLookAtFromView(map),
+                    timestamp: 0
+                }),
+                target
+            );
+            Object.assign(flyToOpts, animationOptions);
+            cameraAnimation = new CameraKeyTrackAnimation(map, flyToOpts);
+            // end:harp_gl_camera_animation_1.ts
+            cameraAnimation.start();
+        }
+    }
+
+    function enableFlyOver(enable: boolean) {
+        stopAnimation();
+        options.flyOver = enable;
+        if (enable) {
+            Object.assign(flyOverAnimationOptions, animationOptions);
+            cameraAnimation = new CameraKeyTrackAnimation(map, flyOverAnimationOptions);
+            cameraAnimation.start();
+        }
+    }
+
+    function createOrbitAnimation(): CameraKeyTrackAnimation {
+        // snippet:harp_gl_camera_animation_2.ts
+        const orbitOpts = CameraAnimationBuilder.createOrbitOptions(
+            new ControlPoint({
+                ...CameraAnimationBuilder.getLookAtFromView(map),
+                timestamp: 0
+            }),
+            3
+        );
+        Object.assign(orbitOpts, animationOptions);
+        return new CameraKeyTrackAnimation(map, orbitOpts);
+        // end:harp_gl_camera_animation_2.ts
+    }
+
+    function createBaseMap(): MapView {
+        const canvas = document.getElementById("mapCanvas") as HTMLCanvasElement;
+        const mapView = new MapView({
+            canvas,
+            projection: sphereProjection,
+            theme: "resources/berlin_tilezen_base_globe.json"
+        });
+        canvas.addEventListener("contextmenu", e => e.preventDefault());
+
+        CopyrightElementHandler.install("copyrightNotice", mapView);
+
+        mapView.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener("resize", () => {
+            mapView.resize(window.innerWidth, window.innerHeight);
+        });
+
+        const omvDataSource = new OmvDataSource({
+            baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+            apiFormat: APIFormat.XYZOMV,
+            styleSetName: "tilezen",
+            authenticationCode: apikey,
+            authenticationMethod: {
+                method: AuthenticationMethod.QueryString,
+                name: "apikey"
+            },
+            copyrightInfo
+        });
+        mapView.addDataSource(omvDataSource);
+
+        return mapView;
+    }
+}
---
+++ @here/harp-map-controls/test/CameraAnimationBuilderTest.ts
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { GeoCoordinates, mercatorProjection } from "@here/harp-geoutils";
+import { MapView } from "@here/harp-mapview";
+import { assert } from "chai";
+import * as sinon from "sinon";
+import * as THREE from "three";
+
+import { CameraAnimationBuilder } from "../lib/CameraAnimationBuilder";
+import { CameraKeyTrackAnimationOptions, ControlPoint } from "../lib/CameraKeyTrackAnimation";
+
+// tslint:disable:only-arrow-functions
+//    Mocha discourages using arrow functions, see https://mochajs.org/#arrow-functions
+
+describe("MapControls", function() {
+    let animationOptions: CameraKeyTrackAnimationOptions;
+    let controlPoint_0: ControlPoint;
+    let controlPoint_1: ControlPoint;
+    let controlPoint_2: ControlPoint;
+    let sandbox: sinon.SinonSandbox;
+    let mapView: MapView;
+    let camera: THREE.Camera;
+    beforeEach(function() {
+        controlPoint_0 = new ControlPoint({
+            target: new GeoCoordinates(0, 0),
+            timestamp: 0
+        });
+        controlPoint_1 = new ControlPoint({
+            target: new GeoCoordinates(1, 1),
+            timestamp: 100
+        });
+        controlPoint_2 = new ControlPoint({
+            target: new GeoCoordinates(2, 2),
+            timestamp: 200
+        });
+        animationOptions = {
+            controlPoints: [controlPoint_0, controlPoint_1]
+        };
+        sandbox = sinon.createSandbox();
+        mapView = sandbox.createStubInstance(MapView) as any;
+        camera = new THREE.PerspectiveCamera(40);
+        sandbox.stub(mapView, "camera").get(() => camera);
+        sandbox.stub(mapView, "projection").get(() => mercatorProjection);
+    });
+    describe("CameraAnimationBuilder", function() {
+        it("prepends a control point with default time", function() {
+            CameraAnimationBuilder.prependControlPoint(animationOptions, controlPoint_2);
+
+            assert.equal(animationOptions.controlPoints.length, 3);
+            assert.equal(animationOptions.controlPoints[0].timestamp, 0);
+            assert.equal(animationOptions.controlPoints[0].target.latitude, 2);
+            assert.equal(animationOptions.controlPoints[1].target.latitude, 0);
+            assert.notEqual(animationOptions.controlPoints[1].timestamp, 0);
+        });
+
+        it("prepends a control point with specified time", function() {
+            CameraAnimationBuilder.prependControlPoint(animationOptions, controlPoint_2, 20);
+
+            assert.equal(animationOptions.controlPoints.length, 3);
+            assert.equal(animationOptions.controlPoints[0].timestamp, 0);
+            assert.equal(animationOptions.controlPoints[0].target.latitude, 2);
+            assert.equal(animationOptions.controlPoints[1].target.latitude, 0);
+            assert.equal(animationOptions.controlPoints[1].timestamp, 20);
+        });
+
+        it("append a control point with fitting time", function() {
+            CameraAnimationBuilder.appendControlPoint(animationOptions, controlPoint_2);
+
+            assert.equal(animationOptions.controlPoints.length, 3);
+            assert.equal(animationOptions.controlPoints[2].target.latitude, 2);
+            assert.equal(animationOptions.controlPoints[2].timestamp, 200);
+        });
+
+        it("appends a control point with timestamp smaller then the preceding one ", function() {
+            controlPoint_2.timestamp = 0;
+            CameraAnimationBuilder.appendControlPoint(animationOptions, controlPoint_2);
+
+            assert.equal(animationOptions.controlPoints.length, 3);
+            assert.equal(animationOptions.controlPoints[2].target.latitude, 2);
+            assert.equal(animationOptions.controlPoints[2].timestamp, 110);
+        });
+
+        it(
+            "appends a control point with timestamp smaller then the preceding one, and set" +
+                "appendTime",
+            function() {
+                controlPoint_2.timestamp = 0;
+                CameraAnimationBuilder.appendControlPoint(animationOptions, controlPoint_2, 20);
+
+                assert.equal(animationOptions.controlPoints.length, 3);
+                assert.equal(animationOptions.controlPoints[2].target.latitude, 2);
+                assert.equal(animationOptions.controlPoints[2].timestamp, 120);
+            }
+        );
+
+        it("appends a control point to an empty control point list", function() {
+            const options: CameraKeyTrackAnimationOptions = { controlPoints: [] };
+            CameraAnimationBuilder.appendControlPoint(options, controlPoint_2);
+
+            assert.equal(options.controlPoints.length, 1);
+            assert.equal(options.controlPoints[0].target.latitude, 2);
+            assert.equal(options.controlPoints[0].timestamp, 200);
+        });
+
+        it("creates Bow Fly To options with defaults", function() {
+            const bowOptions = CameraAnimationBuilder.createBowFlyToOptions(
+                mapView,
+                controlPoint_0,
+                controlPoint_1
+            );
+
+            assert.equal(bowOptions.controlPoints.length, 4);
+            assert.equal(bowOptions.controlPoints[1].target.latitude, 0.25);
+            assert.equal(bowOptions.controlPoints[2].target.latitude, 0.75);
+            assert.equal(bowOptions.controlPoints[0].distance, 0);
+            assert.isAbove(bowOptions.controlPoints[1].distance, 0);
+            assert.isAbove(bowOptions.controlPoints[2].distance, 0);
+            assert.equal(bowOptions.controlPoints[0].timestamp, 0);
+            assert.equal(bowOptions.controlPoints[1].timestamp, 10 / 3);
+            assert.equal(bowOptions.controlPoints[2].timestamp, 20 / 3);
+            assert.equal(bowOptions.controlPoints[3].timestamp, 10);
+        });
+
+        it("creates Bow Fly To options with specified altitude and duration", function() {
+            const bowOptions = CameraAnimationBuilder.createBowFlyToOptions(
+                mapView,
+                controlPoint_0,
+                controlPoint_1,
+                1000,
+                15
+            );
+
+            assert.equal(bowOptions.controlPoints.length, 4);
+            assert.equal(bowOptions.controlPoints[1].target.latitude, 0.25);
+            assert.equal(bowOptions.controlPoints[2].target.latitude, 0.75);
+            assert.equal(bowOptions.controlPoints[0].distance, 0);
+            assert.equal(bowOptions.controlPoints[1].distance, 1000);
+            assert.equal(bowOptions.controlPoints[2].distance, 1000);
+            assert.equal(bowOptions.controlPoints[0].timestamp, 0);
+            assert.equal(bowOptions.controlPoints[1].timestamp, 5);
+            assert.equal(bowOptions.controlPoints[2].timestamp, 10);
+            assert.equal(bowOptions.controlPoints[3].timestamp, 15);
+        });
+        it("creates orbit options", function() {
+            const orbitOptions = CameraAnimationBuilder.createOrbitOptions(controlPoint_0);
+
+            assert.equal(orbitOptions.controlPoints.length, 4);
+            assert.equal(orbitOptions.controlPoints[0].heading, 0);
+            assert.equal(orbitOptions.controlPoints[1].heading, -120);
+            assert.equal(orbitOptions.controlPoints[2].heading, -240);
+            assert.equal(orbitOptions.controlPoints[3].heading, -360);
+            assert.equal(orbitOptions.controlPoints[0].timestamp, 0);
+            assert.equal(orbitOptions.controlPoints[1].timestamp, 10 / 3);
+            assert.equal(orbitOptions.controlPoints[2].timestamp, 20 / 3);
+            assert.equal(orbitOptions.controlPoints[3].timestamp, 10);
+        });
+    });
+});
