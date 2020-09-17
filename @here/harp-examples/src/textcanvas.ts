cf27e7338194f34379c240fd5675c2acaf68fe8a
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2020-09-17 13:25:19 +0200

    HARP-11671 Adding mb usage and heading speed (#1832)
    to monitor memory usage without the need for the profiler
    
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- @here/harp-examples/src/textcanvas.ts
+++ @here/harp-examples/src/textcanvas.ts
@@ -16,9 +16,8 @@ import {
     TextRenderStyle
 } from "@here/harp-text-canvas";
 import { GUI } from "dat.gui";
+import * as Stats from "stats.js";
 import * as THREE from "three";
-const Stats = require("stats.js");
-
 /**
  * This example showcases how [[TextCanvas]] can handle dynamic loading of multiple [[FontCatalog]]
  * assets, as well as dynamic real-time text styling.
