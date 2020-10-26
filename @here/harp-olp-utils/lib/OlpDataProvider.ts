086c65e0d24229adc74e9b1f0ddcb2a8e7b3f065
Oleksii Zubko <ext-oleksii.zubko@here.com>
2020-10-26 18:15:21 +0200

    Stopping use deprecates.
    The Data SDK for Typescript will have removed in the next release the
    deprecated constructor for the `VersionedLayerClient` and also
    methods `getVersion()` and `withVersion()` for the `DataRequest`.
    
    This CR adapts the code according to future changes.
    
    Signed-off-by: Oleksii Zubko <ext-oleksii.zubko@here.com>

--- @here/harp-olp-utils/lib/OlpDataProvider.ts
+++ @here/harp-olp-utils/lib/OlpDataProvider.ts
@@ -75,11 +75,12 @@ export class OlpDataProvider extends DataProvider {
 
             this.m_catalogVersion = latestVersion;
         }
-        this.m_versionLayerClient = new VersionedLayerClient(
-            HRN.fromString(this.params.hrn),
-            this.params.layerId,
+        this.m_versionLayerClient = new VersionedLayerClient({
+            catalogHrn: HRN.fromString(this.params.hrn),
+            layerId: this.params.layerId,
+            version: this.m_catalogVersion,
             settings
-        );
+        });
     }
 
     /**
@@ -103,7 +104,7 @@ export class OlpDataProvider extends DataProvider {
 
         try {
             const response = await this.m_versionLayerClient.getData(
-                new DataRequest().withQuadKey(tileKey).withVersion(this.m_catalogVersion),
+                new DataRequest().withQuadKey(tileKey),
                 abortSignal
             );
             if (abortSignal && abortSignal.aborted) {
