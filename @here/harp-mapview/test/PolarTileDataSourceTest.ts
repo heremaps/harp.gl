5d938608cd3fdcf5657f6a4bcc5499709906da97
Andres Mandado <andres.mandado-almajano@here.com>
2020-11-20 19:33:04 +0100

    MINOR: Check with polar data sources test fails with timeout.

--- @here/harp-mapview/test/PolarTileDataSourceTest.ts
+++ @here/harp-mapview/test/PolarTileDataSourceTest.ts
@@ -239,6 +239,7 @@ describe("PolarTileDataSource", function() {
         });
 
         it("Geometry should not exceed pole radius", function() {
+            this.timeout(5000);
             const EPSILON = 1e-5;
             const minLevel = 1; // at zoomLevel 0 there's no hole at the poles
             const maxLevel = 8;
