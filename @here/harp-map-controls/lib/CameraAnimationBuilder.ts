b50847c457a771fa4699417a20c79c129b700083
Fischer, Thomas <Thomas.Fischer@HERE.com>
2021-01-18 23:16:45 +0100

    HARP-13770: Update the copyright header
    Signed-off-by: German Zargaryan <2526045+germanz@users.noreply.github.com>

--- @here/harp-map-controls/lib/CameraAnimationBuilder.ts
+++ @here/harp-map-controls/lib/CameraAnimationBuilder.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2020 HERE Europe B.V.
+ * Copyright (C) 2020-2021 HERE Europe B.V.
  * Licensed under Apache 2.0, see full license in LICENSE
  * SPDX-License-Identifier: Apache-2.0
  */
