12748bf25e12ab49b275f79517faff25f36a6a57
Andres Mandado <andres.mandado-almajano@here.com>
2021-02-22 15:05:25 +0100

    HARP-14257: GeoJsonTiler generates feature ids if needed.
    GeoJsonTiler will generate feature ids if input data doesn't have them.
    They're useful for picking, text element deduplication and replacement.

--- @here/harp-mapview-decoder/test/GeoJsonTilerTest.ts
+++ @here/harp-mapview-decoder/test/GeoJsonTilerTest.ts
@@ -10,7 +10,7 @@ import { expect } from "chai";
 
 import { GeoJsonTiler } from "../lib/GeoJsonTiler";
 
-const featureCollection: FeatureCollection = {
+const featureCollectionWithIds: FeatureCollection = {
     type: "FeatureCollection",
     features: [
         {
@@ -55,6 +55,48 @@ const featureCollection: FeatureCollection = {
     ]
 };
 
+const featureCollectionWithoutIds: FeatureCollection = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            properties: {},
+
+            geometry: {
+                type: "Point",
+                coordinates: [1, 2]
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [1, 2],
+                    [3, 4]
+                ]
+            }
+        },
+        {
+            type: "Feature",
+            properties: {},
+
+            geometry: {
+                type: "Polygon",
+                coordinates: [
+                    [
+                        [1, 2],
+                        [3, 4],
+                        [5, 6]
+                    ]
+                ]
+            }
+        }
+    ]
+};
+
 describe("GeoJsonTiler", function () {
     let tiler: GeoJsonTiler;
 
@@ -64,15 +106,28 @@ describe("GeoJsonTiler", function () {
 
     it("returns features with their original geojson ids", async function () {
         const indexId = "dummy";
-        await tiler.registerIndex(indexId, featureCollection);
+        await tiler.registerIndex(indexId, featureCollectionWithIds);
 
         const tile = (await tiler.getTile(indexId, new TileKey(0, 0, 1))) as any;
 
         expect(tile.features).has.lengthOf(3);
-        const expectedFeatureIds = featureCollection.features.map(feature => feature.id);
+        const expectedFeatureIds = featureCollectionWithIds.features.map(feature => feature.id);
         const actualFeatureIds: string[] = tile.features.map(
             (feature: { id: string }) => feature.id
         );
         expect(actualFeatureIds).has.members(expectedFeatureIds);
+    });
+
+    it("generates feature ids if input geojson doesn't have them", async function () {
+        const indexId = "dummy";
+        await tiler.registerIndex(indexId, featureCollectionWithoutIds);
+
+        const tile = (await tiler.getTile(indexId, new TileKey(0, 0, 1))) as any;
+
+        expect(tile.features).has.lengthOf(3);
+        const actualFeatureIds: Array<number | undefined> = tile.features.map(
+            (feature: { id: number }) => feature.id
+        );
+        expect(actualFeatureIds).to.not.include(undefined);
     });
 });
