bccab08e7defadcdd320fa2e15d09dd44b44dc82
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2021-08-23 16:43:05 +0200

    MAPSJS-2983 Write to the console if we need too many attempts getting… (#2284)
    Some failed attempts are ok, because there are cases where the DataSource is attached and ready
    and the map has started to render, but the theme isn't fully set.
    
    There are other ways to fix the problem, but they are generally more invasive and could introduce
    regressions / performance issues, some options are:
    - In the TileGeometryCreator::createAllGeometries method, await on the getTheme() Promise
    - Don't allow new TextElements to be added while the theme is updating
    - Investigate and remove all calls to update the mapview while the method `addDataSource` is called
      and see if this can be removed, because no update calls means we don't render and request new tiles
      which may contain references to POI's that don't yet exist on the cache.
    
    For the sake of simplicity, this is the preferred method.
    
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- @here/harp-mapview/test/PoiRendererTest.ts
+++ @here/harp-mapview/test/PoiRendererTest.ts
@@ -13,9 +13,15 @@
 import { Env } from "@here/harp-datasource-protocol";
 import { Math2D } from "@here/harp-utils";
 import { expect } from "chai";
-import * as THREE from "three";
 
+import { MapViewImageCache } from "../lib/image/MapViewImageCache";
+import { MapView } from "../lib/MapView";
+import { PoiManager } from "../lib/poi/PoiManager";
 import { PoiBuffer, PoiRenderer } from "../lib/poi/PoiRenderer";
+import { TextElement } from "../lib/text/TextElement";
+
+import sinon = require("sinon");
+import * as THREE from "three";
 
 describe("PoiRenderer", function () {
     describe("computeIconScreenBox", function () {
@@ -95,6 +101,92 @@ describe("PoiRenderer", function () {
             expect(screenBox.y).to.equal(-16);
             expect(screenBox.w).to.equal(16);
             expect(screenBox.h).to.equal(16);
+        });
+    });
+
+    describe("search for cached images", function () {
+        const webGLRenderer = {
+            capabilities: {
+                isWebGL2: false
+            }
+        } as THREE.WebGLRenderer;
+        const mapViewStub = sinon.createStubInstance(MapView);
+        const testImageName = "testImage";
+        const createPointLabel = (name: string) => {
+            return {
+                poiInfo: {
+                    imageTextureName: name,
+                    isValid: true,
+                    buffer: undefined,
+                    technique: {}
+                },
+                visible: true
+            } as TextElement;
+        };
+
+        const addRandomImageToCache = (
+            testCache: MapViewImageCache,
+            name: string,
+            load: boolean
+        ) => {
+            return testCache.addImage(
+                name,
+                `/@here/harp-mapview/test/resources/headshot.jpg`,
+                load
+            );
+        };
+        let poiManager: PoiManager;
+        let mapEnv: Env;
+        let testCache: MapViewImageCache;
+        beforeEach(() => {
+            poiManager = new PoiManager((mapViewStub as any) as MapView);
+            mapEnv = new Env();
+            testCache = new MapViewImageCache();
+        });
+
+        it("poi is valid when in cache and loading started", async function () {
+            this.timeout(5000);
+
+            const testImageItem = addRandomImageToCache(testCache, testImageName, true);
+            const caches = [testCache];
+            const poiRenderer = new PoiRenderer(webGLRenderer, poiManager, caches);
+            const pointLabel = createPointLabel(testImageName);
+            const waitingForLoad = poiRenderer.prepareRender(pointLabel, mapEnv);
+            // This is false because the image is still being loaded in the background, notice the
+            // true flag passed to the call to testCache.addImage.
+            expect(waitingForLoad).false;
+
+            // Promise.resolve used to convert the type `ImageItem | Promise<ImageItem>` to
+            // `Promise<ImageItem>`.
+            await Promise.resolve(testImageItem);
+
+            const imageLoaded = poiRenderer.prepareRender(pointLabel, mapEnv);
+            expect(imageLoaded).true;
+
+            // Check that a second attempt to prepareRender succeeds.
+            const imageLoadedAgain = poiRenderer.prepareRender(pointLabel, mapEnv);
+            expect(imageLoadedAgain).true;
+        });
+
+        it("poi is invalid when not in cache, adding to cache means it will load", async function () {
+            this.timeout(5000);
+
+            // Empty cache for now
+            const caches = [testCache];
+            const poiRenderer = new PoiRenderer(webGLRenderer, poiManager, caches);
+            const pointLabel = createPointLabel(testImageName);
+            const waitingForLoad = poiRenderer.prepareRender(pointLabel, mapEnv);
+            expect(waitingForLoad).false;
+
+            const testImageItem = addRandomImageToCache(testCache, testImageName, true);
+
+            // Promise.resolve used to convert the type `ImageItem | Promise<ImageItem>` to
+            // `Promise<ImageItem>`.
+            await Promise.resolve(testImageItem);
+
+            const imageLoaded = poiRenderer.prepareRender(pointLabel, mapEnv);
+            // Check that adding to the cache means it will now load.
+            expect(imageLoaded).true;
         });
     });
 });
