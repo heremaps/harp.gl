bd9d86e794919c21fbba1a4def1a85db5640d2e8
Matthias Viranyi <mviranyi@users.noreply.github.com>
2021-10-14 16:22:12 +0200

    Change URL caching inside ThemeLoader. (#2318)
    Signed-off-by: Matthias Viranyi <matthias.viranyi@here.com>
    
    Co-authored-by: Matthias Viranyi <matthias.viranyi@here.com>

--- @here/harp-mapview/lib/ThemeLoader.ts
+++ @here/harp-mapview/lib/ThemeLoader.ts
@@ -25,7 +25,6 @@ import {
     IContextLogger,
     ISimpleChannel,
     RelativeUriResolver,
-    resolveReferenceUri,
     UriResolver
 } from "@here/harp-utils";
 
@@ -136,7 +135,7 @@ export class ThemeLoader {
                 throw new Error(`ThemeLoader#load: cannot load theme: ${response.statusText}`);
             }
             theme = (await response.json()) as Theme;
-            theme.url = resolveReferenceUri(getAppBaseUrl(), themeUrl);
+            theme.url = themeUrl;
             theme = this.resolveUrls(theme, options);
         } else if (theme.url === undefined) {
             // assume that theme url is same as baseUrl
